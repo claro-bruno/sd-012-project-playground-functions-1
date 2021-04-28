d25e275bc60851a5f0e6d1ca22f214dc06515bcc
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-04-28 13:34:14 -0300

    Resoluçao do cha1

---
+++ challenges.js
@@ -0,0 +1,7 @@
+function compareTrue(booleano1, booleano2) {
+  if(booleano1===true && booleano2===true)
+  return console.log('true');
+  else return console.log('false');
+
+};
+compareTrue(true, true);
