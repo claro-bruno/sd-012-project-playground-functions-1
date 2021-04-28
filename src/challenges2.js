d0db33ec67db1a45aba7f2f66772123fcf542020
Paulo Michael <paulo.michael@gmail.com>
2021-04-28 18:50:31 -0300

    desafio 11 - generatePhoneNumber(numberArray)

--- src/challenges2.js
+++ src/challenges2.js
@@ -32,9 +32,67 @@ function techList(technologies, name) {
 // console.log(techList(array,name));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function mostRepeatedCount(array){
+  let maxElement = array[0];
+  let maxCount = 1;
+  for(let index = 0; index < array.length - 1; index += 1){
+      let count = 0;
+      for(let pointer = index + 1; pointer < array.length; pointer += 1){
+          if(array[index] === array[pointer]){
+              count += 1;
+          }
+      }
+      if(count > maxCount){
+          maxCount = count;
+          maxElement = array[index];
+      }
+  }
+  return maxCount;
 }
+
+function containsInvalidDigit(array){
+  let isInvalid = 0;
+  for(let number of array){
+    if(number < 0 || number > 9 || mostRepeatedCount(array) >= 3){
+      isInvalid += 1;
+    }
+  }
+  if(isInvalid > 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+// console.log(containsInvalidDigit(array));
+
+function generatePhoneNumber(numberArray) {
+  let formatedNumber = '';
+  let areaCode = firstHalf = secondHalf = '';
+  if(numberArray.length != 11){
+    return 'Array com tamanho incorreto.';
+  }
+  else if(containsInvalidDigit(numberArray)){
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  else{
+    for(let index = 0; index < 2; index += 1){
+      areaCode += numberArray[index];
+    }
+    for(let index = 2; index < 7; index += 1){
+      firstHalf += numberArray[index];
+    }
+    for(let index = 7; index < 11; index += 1){
+      secondHalf += numberArray[index];
+    }
+  }
+  formatedNumber = '('+areaCode+') '+firstHalf+'-'+secondHalf;
+  return formatedNumber;
+}
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+// console.log(generatePhoneNumber(array));
 
 // Desafio 12
 function triangleCheck() {
