e3cd1300ef2423236eb26fc173b145173d6d4a6a
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-28 18:50:36 -0300

    add funçao highestCount

--- src/challenges.js
+++ src/challenges.js
@@ -7,9 +7,9 @@ function compareTrue(valor1, valor2) {
     return('false');
   }
 }
-console.log(compareTrue(89,56));
-console.log(compareTrue(73,11));
-console.log(compareTrue(98,42));
+//console.log(compareTrue(89,56));
+//console.log(compareTrue(73,11));
+//console.log(compareTrue(98,42));
 
 
 
@@ -17,9 +17,9 @@ console.log(compareTrue(98,42));
 function calcArea(base, height) {
  return ((base * height) / 2);
 }
-console.log(calcArea(10,50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1));
+//console.log(calcArea(10,50));
+//console.log(calcArea(5,2));
+//console.log(calcArea(51,1));
 
 
 
@@ -35,28 +35,42 @@ console.log(splitSentence('go Trybe'))
 
 
 // Desafio 4  
-let array = [ ];
+let nome = [ ];
 
-function concatName (array) {
-  return array[array.length - 1] + ', ' + array[0];  
+function concatName (nome) {
+  return nome[nome.length - 1] + ', ' + nome[0];  
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
-console.log(concatName(['foguete', 'não', 'tem', 'ré']));
-console.log(concatName(['captain', 'my', 'captain']));
+//console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+//console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+//console.log(concatName(['captain', 'my', 'captain']));
 
 
 // Desafio 5
 function footballPoints(wins,ties) {
   return (wins * 3) + ties; 
 }
-console.log(footballPoints(14,8));
-console.log(footballPoints(1,2));
-console.log(footballPoints(0,0));
+//console.log(footballPoints(14,8));
+//console.log(footballPoints(1,2));
+//console.log(footballPoints(0,0));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+let repeticao = [ ];
+let resultado = 0;
+
+function highestCount(repeticao) {
+  let a = repeticao.sort();
+  let b = a[a.length - 1]
+for(index = 0; index < a.length; index += 1)
+    if (a[index] === b){
+    resultado = resultado + 1;
+    } else { 
+    }
+  return resultado;
 }
+
+//console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+//console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+//console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse() {
