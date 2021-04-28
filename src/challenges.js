359b0750f91991850322fbb335dc25c162b1ba8d
julio11lp <julio11lp@hotmail.com>
2021-04-28 19:21:32 -0300

    Adiciona criterio 7

--- src/challenges.js
+++ src/challenges.js
@@ -40,8 +40,7 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numeros) {
-  // seu código aqui
-  
+  // seu código aqui  
   let maiorNumero = 0;
   let contador = 0;
 
@@ -60,8 +59,21 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  // let gato1 = Math.abs(cat1 - mouse);
+  // let gato2 = Math.abs(cat2 - mouse);
+
+  let gato1 = cat1 - mouse;
+  let gato2 = cat2 - mouse;
+
+  if(gato1 < gato2){
+    return "cat1";
+  } else if (gato2 < gato1){
+    return "cat2";
+  } else{
+    return "os gatos trombam e o rato foge";
+  };
 }
 
 // Desafio 8
