b9b3a075ccdda3b0a2b82c4dbb11ed5361914291
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-04-28 19:02:57 -0300

    Adiciona requisito 11 - funcao generatePhone Number

--- src/challenges2.js
+++ src/challenges2.js
@@ -27,9 +27,63 @@ function techList(array, name) {
 };
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+
+function generatePhoneNumber(array) {
+  let result;
+  let verificacao = false;
+
+  for (let index = 0; index < array.length; index += 1) {
+
+    switch (true) {
+      case (array[index] < 0 || array[index] > 9):
+        verificacao = true;
+        break;
+    };
+
+    let qtdeDeRepeticoes = 0;
+
+    for (let index2 = 0; index2 < array.length; index2 += 1){
+
+      if (array[index] === array[index2]){
+        qtdeDeRepeticoes += 1;
+      };
+
+      if(qtdeDeRepeticoes >=3) {
+        verificacao = true;
+      };
+    };
+  };
+
+  if (array.length !== 11) {
+    result = "Array com tamanho incorreto."
+  } else if (verificacao === true) {
+    result = "não é possível gerar um número de telefone com esses valores"
+  } else {
+    let phoneNumber = "(";
+
+    for (let index = 0; index < 2; index += 1) {
+      phoneNumber += array[index];
+    };
+  
+    phoneNumber += ") ";
+  
+    for (let index = 2; index < 7; index += 1) {
+      phoneNumber += array[index];
+    };
+  
+    phoneNumber += "-";
+  
+    for (let index = 7; index < 11; index += 1) {
+      phoneNumber += array[index];
+    };
+  
+    result = phoneNumber;
+  };
+
+  return result;
+};
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 1, 1, 1]))
 
 // Desafio 12
 function triangleCheck() {
