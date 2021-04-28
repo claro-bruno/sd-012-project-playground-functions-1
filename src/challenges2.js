0213b936c833af1f45db5cd16fdf8a68161b995f
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-04-28 17:59:19 -0300

    Resolução do requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let techs = [];
+  let returnTechs = 0;
+  let order = array.sort();
+  if (order.length !== 0) {
+    for (let index = 0; index < order.length; index += 1) {
+      techs[index] = {
+        tech: order[index],
+        name,
+      };
+    }
+    returnTechs = techs;
+  } else {
+    returnTechs = 'Vazio!';
+  }
+  return returnTechs;
 }
+
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'],
+  'Matheus'));
 
 // Desafio 11
 function generatePhoneNumber() {
