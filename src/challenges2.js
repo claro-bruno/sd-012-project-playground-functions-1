34a08a803c34fec910410ff95b87108b19341cc2
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-04-28 17:36:57 -0300

    desafio 10 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,30 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+
+function techList(arr, name) {
+  if(arr.length === 0){
+    return 'Vazio!';
+  };
+  let techList = [];
+  for(let index = 0; index< arr.length; index+=1){
+    let obj = {};
+    obj.tech = arr[index];
+    obj.name = name;
+    techList.push(obj);
+  };
+
+  return techList;
+};
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+
+function generatePhoneNumber(arr) {
+  let phoneNumber = '';
+  for(let index = 0; index < arr.length ; index += 0){
+    if(arr[index]===0){
+      phoneNumber += ('(' + arr[index]);
+    }
+  }
 }
 
 // Desafio 12
