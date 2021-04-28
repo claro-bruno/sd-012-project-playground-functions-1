abde67f732ec72198311be422d3c0a2832ce4ea3
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-28 19:13:06 -0300

    correcao de lit try 9

--- src/challenges.js
+++ src/challenges.js
@@ -58,20 +58,22 @@ function catAndMouse(mouse, cat1, cat2) {
   }
   if (distanciaCat2 < distanciaCat1) {
     return 'cat2';
-  } 
+  }
   return 'os gatos trombam e o rato foge';
 }
+
 // Desafio 8.1
-function checkBuzz(num){
-  if (num % 3 == 0) return 'fizz';
-  if (num % 5 == 0) return 'buzz';
-  if (num % 3 == 0 && num % 5 == 0) return 'fizzBuzz';
+function checkBuzz(num) {
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  if ((num % 3 === 0) && (num % 5 === 0)) return 'fizzBuzz';
   return 'bug!';
 }
+
 // Desafio 8
 function fizzBuzz(numbers) {
-  let newArray=[];
-  for (let i in numbers){
+  let newArray = [];
+  for (let i in numbers) {
     newArray.push(checkBuzz(numbers[i]));
   }
   return newArray;
@@ -79,20 +81,20 @@ function fizzBuzz(numbers) {
 
 // Desafio 9
 function encode(string) {
-  string = string.replace(/a/g,"1");
-  string = string.replace(/e/g,"2");
-  string = string.replace(/i/g,"3");
-  string = string.replace(/o/g,"4");
-  string = string.replace(/u/g,"5");
+  string = string.replace(/a/g, '1');
+  string = string.replace(/e/g, '2');
+  string = string.replace(/i/g, '3');
+  string = string.replace(/o/g, '4');
+  string = string.replace(/u/g, '5');
   return string;
 }
 
 function decode(string) {
-  string = string.replace(/1/g,"a");
-  string = string.replace(/2/g,"e");
-  string = string.replace(/3/g,"i");
-  string = string.replace(/4/g,"o");
-  string = string.replace(/5/g,"u");
+  string = string.replace(/1/g, 'a');
+  string = string.replace(/2/g, 'e');
+  string = string.replace(/3/g, 'i');
+  string = string.replace(/4/g, 'o');
+  string = string.replace(/5/g, 'u');
   return string;
 }
 
