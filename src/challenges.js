3c253c8b0dc2881ef1c37cbc796259e29eed1219
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-04-28 18:00:53 -0300

    descomentei funcoes de 4 a 9 para teste de corretor

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,10 @@
 // Desafio 1
 function compareTrue(value1, value2) {
   if (value1 === true && value2 === true) {
-    return true;
-    
+    return true   
   }
 }
- 
+compareTrue()
 
 //  console.log(compareTrue(true, true));
 
@@ -15,7 +14,7 @@ function compareTrue(value1, value2) {
 function calcArea(base, height){
   return (base * height) / 2;
 }
-calcArea()
+
 // console.log(calcArea(10, 50));
 // console.log(calcArea(5, 2));
 // console.log(calcArea(51, 1));
@@ -26,48 +25,40 @@ calcArea()
 function splitSentence(string){
   return string.split(' ');
 }
-// console.log(splitSentence('go trybe'))
 
-// for (index = 0; index < string.length; index += 1)
-//   string = [ string[index] + ',  ']
-  
-// }
-// console.log(splitSentence('go trybe'))
+// Desafio 4
+function concatName(string1, string2) {
+  for (index = 0; index <) 
+  teste 
+}
 
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
 
-// // Desafio 4
-// function concatName(string1, string2) {
-//   for (index = 0; index <) 
-//   teste 
-// }
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
 
-// // Desafio 5
-// function footballPoints() {
-//   // seu código aqui
-// }
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
 
-// // Desafio 6
-// function highestCount() {
-//   // seu código aqui
-// }
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
 
-// // Desafio 7
-// function catAndMouse() {
-//   // seu código aqui
-// }
-
-// // Desafio 8
-// function fizzBuzz() {
-//   // seu código aqui
-// }
-
-// // Desafio 9
-// function encode() {
-//   // seu código aqui
-// }
-// function decode() {
-//   // seu código aqui
-// }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
 module.exports = {
   calcArea,
