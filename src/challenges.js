1f37a204e1347122acecae85ead8d1347d1f8e7d
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-04-28 17:25:17 -0300

    feito o desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -43,9 +43,19 @@ function highestCount(arr) {
 console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = Math.abs(cat1 - mouse);
+  let distCat2 = Math.abs(cat2 - mouse);
+  let closer = '';
+
+  if (distCat1 == distCat2) {
+    closer = 'os gatos trombam e o rato foge';
+  } else {
+    closer = distCat1 < distCat2 ? 'cat1' : 'cat2';
+  }
+  return closer;
 }
+
 
 // Desafio 8
 function fizzBuzz() {
