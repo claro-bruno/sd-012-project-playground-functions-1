5c3ff2f8aca20590b1ed63afb61fd8a4683a7361
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-28 17:26:20 -0300

    (14) Resolve o requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,9 +17,28 @@ function techList(tech, name) {
 };
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(PhoneNumber) {
   // seu código aqui
-}
+  if (PhoneNumber.length != 11){
+    return "Array com tamanho incorreto."
+  } else {
+    for (let number in PhoneNumber) {
+      if (PhoneNumber[number] < 0 || PhoneNumber[number] > 9) {
+        return "não é possível gerar um número de telefone com esses valores"
+      };
+      let contador = 0;
+      for (let number2 in PhoneNumber){
+        if (PhoneNumber[number] === PhoneNumber[number2]){
+          contador += 1
+        }
+        if (contador > 2){
+          return "não é possível gerar um número de telefone com esses valores"
+        };
+      };
+    };
+    return '(' + PhoneNumber[0] + PhoneNumber[1] + ') ' + PhoneNumber[2] + PhoneNumber[3] + PhoneNumber[4] + PhoneNumber[5] + PhoneNumber[6] + '-' + PhoneNumber[7] + PhoneNumber[8] + PhoneNumber[9] + PhoneNumber[10]
+  };
+};
 
 // Desafio 12
 function triangleCheck() {
