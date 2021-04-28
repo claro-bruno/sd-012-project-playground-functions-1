084e6b99518a4cfb5bdd54da8b259bb17eac9e26
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-04-28 16:55:34 -0300

    Resolve o requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -55,9 +55,13 @@ console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  return (3 * wins) + ties
 }
+
+console.log(footballPoints(14,8));
+console.log(footballPoints(1,2));
+console.log(footballPoints(0,0));
 
 // Desafio 6
 function highestCount() {
