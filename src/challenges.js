4be6053fa0e74afe4d49ece3c08a639cdb55c119
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-04-28 18:54:20 -0300

    Exercicio de 1 a 6 sem console.log

--- src/challenges.js
+++ src/challenges.js
@@ -8,38 +8,17 @@ function compareTrue(firstValue,secondValue) {
   }
 };
 
-console.log(compareTrue(false,true));
-console.log(compareTrue(false,false));
-console.log(compareTrue(true,true));
-
-
-
 // Desafio 2
-let base;
-let height;
-
 function calcArea(base,height) {
   return (base * height) / 2;
   
 }
 
-console.log(calcArea(10,50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1));
-
-
 // Desafio 3
-let string;
-
 function splitSentence(string) {
   let array = string.split(' ');
   return array
 }
-
-console.log(splitSentence('go Trybe'));
-console.log(splitSentence('vamo que vamo'));
-console.log(splitSentence('foguete'));
-
 
 
 // Desafio 4
@@ -49,18 +28,12 @@ function concatName(names) {
   return names[names.length-1] + ", " + names[0]
 }
 
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
-console.log(concatName(['foguete', 'não', 'tem', 'ré']));
-console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
 function footballPoints(wins,ties) {
   return (3 * wins) + ties
 }
 
-console.log(footballPoints(14,8));
-console.log(footballPoints(1,2));
-console.log(footballPoints(0,0));
 
 // Desafio 6
 function highestCount(numbers) {
@@ -79,10 +52,6 @@ function highestCount(numbers) {
   return counter
 }
 
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
-console.log(highestCount([0, 0, 0]));
-
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   if(cat1 - mouse < cat2 - mouse) {
@@ -95,9 +64,8 @@ function catAndMouse(mouse,cat1,cat2) {
   
 }
 
-console.log(catAndMouse(0,3,2));
-console.log(catAndMouse(0,6,12));
-console.log(catAndMouse(0,0,0));
+console.log(catAndMouse(1,0,2));
+
 
 
 // Desafio 8
