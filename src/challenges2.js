7c32aa2749b17c71e6536f2605b602f0308d28f3
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-28 18:05:46 -0300

    adding challenge bonus 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -45,8 +45,12 @@ function generatePhoneNumber(array) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let value = true;
+  if (lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineA + lineB || lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineA-lineB)){
+    value = false;
+  }
+  return value;
 }
 
 // Desafio 13
