82620b0978bc065518dceedce8e0e0a7b9bc40d1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-04-28 16:20:45 -0300

    conclusão do Desafio 5, onde escrevo uma função para informar a pontuação alcançada por um determinado time de futebol

--- src/challenges.js
+++ src/challenges.js
@@ -23,9 +23,9 @@ function concatName() {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
+};
 
 // Desafio 6
 function highestCount() {
