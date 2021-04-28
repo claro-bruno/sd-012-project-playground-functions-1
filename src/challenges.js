246364b6bde96fcb33a948b8224c0dc3b6867e8d
Marcus Santos <smarcuslemos@gmail.com>
2021-04-28 15:05:35 -0300

    Add os exercicios 1 e 2.

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,42 @@
 // Desafio 1
-function compareTrue() {
-  xablau
-  // seu código aqui
-}
+/*
+let value = true;
+let value2 = true;
+
+function compareTrue(value, value2) {
+  if(value === true && value2 === false || value === false && value2 === false){
+    return false;
+  } else {
+    return true;
+  };
+};
+
+console.log(compareTrue(value, value2));
+
+
+
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+let base = 51;
+let height = 1;
+
+function calcArea(base, height) {
+  let operationCalcArea = (base * height) / 2
+  return operationCalcArea
 }
+
+console.log(calcArea(base, height));
+
+
+*/
+
 
 // Desafio 3
 function splitSentence() {
   // seu código aqui
 }
+
+/*
 
 // Desafio 4
 function concatName() {
@@ -59,3 +83,5 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+*/
