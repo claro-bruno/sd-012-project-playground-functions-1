def8b94550567118f32f27bf49322482359d57c5
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-28 20:30:16 -0300

    Pretende resolver requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,9 +4,18 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return "Array com tamanho incorreto.";
+  } else {
+    return '(' + numbers[0] + numbers[1] + ') '
+    + numbers[2] + numbers[3] + numbers[4]
+    + numbers[5] + numbers[6] + '-'
+    + numbers[7] + numbers[8]
+    + numbers[9] + numbers[10];
+  }
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
