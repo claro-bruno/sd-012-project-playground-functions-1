47e0810d638de8dd9f542c0d400970304495979c
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-04-28 20:42:19 -0300

    adiciona teste de correcao ao 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,13 @@
 // Desafio 1
 
 function compareTrue(value1, value2) {
-  if (value1 === true && value2 === true) {
-    return true   
+  if (value1 === true && value2 === true){
+  return true
+  } else {
+    return false
   }
 }
-
-
-//  console.log(compareTrue(true, true));
+console.log(compareTrue(false, false));
 
 
 // Desafio 2
