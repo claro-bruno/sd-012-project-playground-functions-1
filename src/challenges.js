e31f8f7be883cee13bd6c7a6cff3f4e92c4e0510
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-04-28 19:09:38 -0300

    resolve desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,48 +9,52 @@ function compareTrue(bloco1,bloco2) {
 console.log(compareTrue(true,false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
-
-// Desafio 3
-function splitSentence() {
+let base = 10;
+let height = 50;
+function calcArea(base,height) {
   
-  // seu código aqui
-}
+  return (base * height) / 2;
+};
+console.log (area);
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+// // Desafio 3
+// function splitSentence() {
+  
+//   // seu código aqui
+// }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
+
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
 module.exports = {
   calcArea,
