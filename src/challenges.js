2effb2faaea5e73eac494a269b2a9c3dfd34dfab
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-04-28 15:06:14 -0300

    Resolve o requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,11 @@ function compareTrue(aluno, turma) {
 
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+  let resultado = (base * height)/2 
+  return resultado;
+};
+
 
 // Desafio 3
 function splitSentence() {
