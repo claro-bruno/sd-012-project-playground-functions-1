81bfc948f7724450a1977c978054ced7acebab24
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-04-28 19:16:16 -0300

    refatorando desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,16 +3,23 @@ function techList(arr, name) {
 
   let result = []
   arr.forEach(element => {
-    const obj = {
-      tech: element,
-      name: name
-    };
+    function CreateObj(element, name) {
+      return {
+        tech: element,
+        name: name
+      }
+    }
+
+    const obj = CreateObj(element, name);
 
     result.push(obj);
   });
 
   return result;
 }
+
+const tecnologias = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+console.log(techList(tecnologias, 'Lucas'));
 
 
 // Desafio 11
