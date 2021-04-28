bc419c0c7f13593cce04fb698234de4525cce045
Euller Braz <euller.braz99@gmail.com>
2021-04-28 18:11:43 -0300

    Resolvido requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -98,19 +98,51 @@ function fizzBuzz(numbersArray) {
     } else if (numbersArray[index] % 5 === 0) {
       result.push("buzz");
     } else {
-      result.push("bug");
+      result.push("bug!");
     }
   }
-
   return result;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let result = "";
+  for (let index in string) {
+    if (string[index] === "a") {
+      result += "1";
+    } else if (string[index] === "e") {
+      result += "2";
+    } else if (string[index] === "i") {
+      result += "3";
+    } else if (string[index] === "o") {
+      result += "4";
+    } else if (string[index] === "u") {
+      result += "5";
+    } else {
+      result += string[index];
+    }
+  }
+  return result;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let result = "";
+  for (let index in string) {
+    if (string[index] === "1") {
+      result += "a";
+    } else if (string[index] === "2") {
+      result += "e";
+    } else if (string[index] === "3") {
+      result += "i";
+    } else if (string[index] === "4") {
+      result += "o";
+    } else if (string[index] === "5") {
+      result += "u";
+    } else {
+      result += string[index];
+    }
+  }
+  return result;
 }
 
 module.exports = {
