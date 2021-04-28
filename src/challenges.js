31d68c7c3717ba8f1053a817140862fb69357361
Sarah <sarahgsb@hotmail.com>
2021-04-28 15:30:26 -0300

    3- splitsetence v1

--- src/challenges.js
+++ src/challenges.js
@@ -13,7 +13,9 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  let string = ('go Trybe');
+  let goTrybe = string.split(' ', 2);
+  return goTrybe;
 }
 
 // Desafio 4
