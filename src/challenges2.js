93e47bc969c3f26cf7df1414edc1af81cf2abb59
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-04-28 18:38:45 -0300

    Requisito 10 - Cria funçao de lista de tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,26 +1,39 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techArray, name) {
+    let objectArray = [];
+    for (let tech of techArray) {
+        let techObject = {
+            tech: tech,
+            name: name
+        }
+        objectArray.push(techObject);
+    }
+    if (objectArray.length != 0) {
+        return objectArray;
+    } else {
+        return 'Vazio!';
+    }
+
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+    // seu código aqui
 }
 
 module.exports = {
-  generatePhoneNumber,
-  techList,
-  hydrate,
-  triangleCheck,
+    generatePhoneNumber,
+    techList,
+    hydrate,
+    triangleCheck,
 };
