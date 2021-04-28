f658248d7cd9e745c6ff99953ed62151712aad4a
Erick Silva <erick.silva6653@gmail.com>
2021-04-28 18:25:02 -0300

    exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,19 +2,21 @@
 function techList(tech, name) {
   let techSort = tech.sort()
   let list = [];
-  let objects = {
-    name: name,
-    tech: techSort[0]
-  }
-  for (index = 1; index < techSort.length; index += 1) {
+  for (index = 0; index < techSort.length; index += 1) {
+    let objects = {
+      name: name,
+      tech: ''
+    }
     objects.name = name;
     objects.tech = techSort[index];
     list.push(objects)
   }
+  if (list.length === 0){
+    return "Vazio!"
+  } else {
   return list;
+  }
 }
-
-// console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
