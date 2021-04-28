3623a1217da7f586c8de9bf3ffa9bae365bc1336
Martin <escorpmartin97@gmail.com>
2021-04-28 18:44:41 -0300

    exercise 4

--- src/challenges.js
+++ src/challenges.js
@@ -22,48 +22,50 @@ function splitSentence(string) {
 function concatName(array) {
   let primeiro = [];
   let ultimo = [];
+  for(let index in array){
   primeiro = array[0];
-  ultimo = array[array.length-1];
+  ultimo = array[array.length -1];
   return [ultimo, primeiro];
+  }
 }
 
 // Desafio 5
-// function footballPoints() {
-//   // seu código aqui
-// }
+function footballPoints() {
+  // seu código aqui
+}
 
-// // Desafio 6
-// function highestCount() {
-//   // seu código aqui
-// }
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
 
-// // Desafio 7
-// function catAndMouse() {
-//   // seu código aqui
-// }
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
 
-// // Desafio 8
-// function fizzBuzz() {
-//   // seu código aqui
-// }
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
 
-// // Desafio 9
-// function encode() {
-//   // seu código aqui
-// }
-// function decode() {
-//   // seu código aqui
-// }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
-// module.exports = {
-//   calcArea,
-//   catAndMouse,
-//   compareTrue,
-//   concatName,
-//   decode,
-//   encode,
-//   fizzBuzz,
-//   footballPoints,
-//   highestCount,
-//   splitSentence,
-// };
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  highestCount,
+  splitSentence,
+};
