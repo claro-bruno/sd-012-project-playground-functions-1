188eaaede9e267c83cd34c7277754b1aa5173b8d
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-04-28 17:52:04 -0300

    Correção requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -55,13 +55,13 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist1 = Math.abs(cat1 - mouse);
   let dist2 = Math.abs(cat2 - mouse);
 
-  if (cat1 === cat2) {
+  if (dist1 === dist2) {
     return 'os gatos trombam e o rato foge';
   }
-  if (dist1 > dist2) {
+  if (dist1 < dist2) {
     return 'cat1';
   }
-  if (dist1 < dist2) {
+  if (dist1 > dist2) {
     return 'cat2';
   }
 }
