9880f752d27c44e2b3cfa3015ef481d61582fe81
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-28 15:09:38 -0300

    Desafio 1 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(num1, num2) {
+  if (num1 === num2) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
+compareTrue(8,8);
 
 // Desafio 2
 function calcArea() {
