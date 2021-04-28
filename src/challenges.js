38b8314bdd4cef323849a250b9e9d48712489b20
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-04-28 18:16:30 -0300

    questao 3

--- src/challenges.js
+++ src/challenges.js
@@ -29,9 +29,15 @@ console.log(calcArea(51, 1))
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string, parts) {
+
+return string.split(' ')
+
 }
+console.log(splitSentence("go Trybe"))
+console.log(splitSentence("vamo que vamo"))
+console.log(splitSentence("foguete"))
+
 
 // Desafio 4
 function concatName() {
