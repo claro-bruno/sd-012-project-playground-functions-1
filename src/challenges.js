b4f300b3f59b9c617db1dd6d2b392ab67b37daff
Teika <shimworks.tk@gmail.com>
2021-04-28 14:51:21 -0300

    resolve desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  start
-}
+function compareTrue(valor1, valor2) {
+    if (valor1 === valor2 && valor2 === true){
+      return true;
+    } else {
+      return false;
+    };
+};
+
 
 // Desafio 2
 function calcArea() {
