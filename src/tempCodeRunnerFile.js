00a6e8f366e891aa386ee9d06b1a9e730e97f894
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-04-28 17:48:07 -0300

    Requisito 2 - calcArea

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,12 +1,6 @@
-// Desafio 1
-function compareTrue(valor1, valor2) {
+// Desafio 2
+function calcArea(base, height) {
   // seu código aqui
-  valor1 = true;
-  valor2 = true;
-
-  if((valor1 = true) && (valor2 = true)){
-    return true ;
-  } else{
-    return false;
-  }
+  let area = (base * height) / 2;
+  return area;
 } 
