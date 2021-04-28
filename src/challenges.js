0f41ba8398bf9de4ade3f9ff098044e9e9793100
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-04-28 18:22:35 -0300

    Terminando exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,23 +1,12 @@
 // Desafio 1
 function compareTrue() {
   // seu código aqui
-  let varTrue = true;
-  let vaarFalse = false;
-
-    if(varTrue === varFalse){
-      console.log(true)
-      if(varTrue =! vaarFalse && varFalse != varTrue){
-        console.log(false)
-
-      }
-
-    }
-    return(varTrue,varFalse)
 }
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base*height) / 2;
+
+  return area;
 }
 
 // Desafio 3
