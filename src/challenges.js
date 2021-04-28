59217970d275836adf8c712100ec00d29ec1dec0
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-28 16:28:21 -0300

    (11) Resolve o requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -102,13 +102,49 @@ function fizzBuzz(numeros) {
 };
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  let resposta = '';
+  let codigo = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5
+  }
+  for (index in string) {
+    if (string[index] in codigo){
+      resposta = resposta + codigo[string[index]]
+    } else {
+      resposta = resposta + string[index]
+    }
+  }
+  return resposta
+}
 
-}
-function decode() {
+
+
+
+function decode(string) {
   // seu código aqui
-}
+  let resposta = '';
+  let codigo = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u'
+  }
+  for (index in string) {
+    if (string[index] in codigo){
+      resposta = resposta + codigo[string[index]]
+    } else {
+      resposta = resposta + string[index]
+    };
+  };
+  return resposta;
+};
+
 
 module.exports = {
   calcArea,
