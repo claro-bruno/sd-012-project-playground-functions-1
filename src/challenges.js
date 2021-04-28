1f80d62b74aa251716e224113ce029ba5181ba70
Juliane Marques <mqs.juliane@gmail.com>
2021-04-28 15:58:06 -0300

    comenta console.log para testar

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,19 @@
 // Desafio 1
-let a = false;
-let b = true;
-console.log(compareTrue(a, b));
+let nun1 = true;
+let nun2 = true;
+console.log(compareTrue(nun1, nun2));
 
   // valor booleano 2 tipos (verdadeiro ou false)
   // possui o operador logico && que recebe 2 valores 
   // testar somente a condicao true e o resto retorna false <<<< 
+  function compareTrue(nun1, nun2) {
+    if (nun1 === true && nun2 === true){
+      return true;
+    } else {
+      return false;
+    }
+  }; 
+
 
 function compareTrue(a, b) {
   if(a == true && b == false) {
@@ -19,35 +27,34 @@ function compareTrue(a, b) {
   };
 }
 
+
 // Desafio 2
-let base = 10;
-let height = 50;
+//let base = 10;
+//let height = 50;
 
 function calcArea(base, height) {
   let area = base * height / 2;
   return area;
 } 
-console.log(calcArea(base, height));
+//console.log(calcArea(base, height));
 
-let base = 5;
-let height = 2;
+//let base = 5;
+//let height = 2;
 
-function calcArea(base, height) {
-  let area = base * height / 2;
-  return area;
-} 
-console.log(calcArea(base, height));
+//*/function calcArea(base, height) {
+//  let area = base * height / 2;
+//  return area;
+//} 
+//console.log(calcArea(base, height));
 
-let base = 51;
-let height = 1;
+//let base = 51;
+//let height = 1;
 
-function calcArea(base, height) {
-  let area = base * height / 2;
-  return area;
-} 
-console.log(calcArea(base, height));
-
-
+//function calcArea(base, height) {
+//  let area = base * height / 2;
+//  return area;
+//} 
+//console.log(calcArea(base, height));
 
 
 // Desafio 3
