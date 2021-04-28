07b3daae84eb2ba22e3d09f4a4aee7eb998dca02
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-28 18:54:49 -0300

    Pretende resolver requisito 7 (2)

--- src/challenges.js
+++ src/challenges.js
@@ -44,9 +44,9 @@ function catAndMouse(mouse, cat1, cat2) {
   if ((cat1-mouse) === (cat2-mouse)) {
     return "os gatos trombam e o rato foge";
   } else if ((cat1-mouse) > (cat2-mouse)){
-    return "cat1";
-  } else {
     return "cat2";
+  } else {
+    return "cat1";
   }
 }
 // Desafio 8
