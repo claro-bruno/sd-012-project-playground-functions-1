cf55b935a3551f2f570b5d0d1dea1b90d8f2b7dd
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-04-28 18:29:38 -0300

    Feito o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,48 @@
+// Desafio 11 - ESLint
+function menorZero(numeros) {
+  for (let num of numeros) {
+    if (num < 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function maiorNove(numeros) {
+  for (let num of numeros) {
+    if (num > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function validating(numeros, i) {
+  let count = 0;
+  for (let j = 0; j < numeros.length; j += 1) {
+    if (numeros[i] === numeros[j]) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function repeatedNumber(numeros) {
+  let counter = 0;
+  for (let i = 0; i < numeros.length; i += 1) {
+    counter = validating(numeros, i);
+    if (counter >= 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generatePhoneAuxiliar(numbers) {
+  numbers = numbers.join('');
+  return `(${numbers.substring(0, 2)}) ${numbers.substring(2, 7)}-${numbers.substring(7)}`;
+}
+
 // Desafio 10
 function techList(technology, person) {
   if (technology.length < 1) {
@@ -16,8 +61,14 @@ function techList(technology, person) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  if (number.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (menorZero(number) || maiorNove(number) || repeatedNumber(number)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return generatePhoneAuxiliar(number);
 }
 
 // Desafio 12
