75eb8befaff205e5e749aea1fb103ac8e69fb615
Nuwanda <morato_dev@protonmail.com>
2021-04-28 18:00:13 -0300

    tirei console.log do desafio 6 e desafio 7 feito

--- src/challenges.js
+++ src/challenges.js
@@ -46,11 +46,25 @@ function highestCount(mostRepeated) {
     }
     return count;
 }
-console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-    // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+    let cat1Wins = cat1 - mouse;
+    let cat2Wins = cat2 - mouse;
+    if (cat1Wins < 0) {
+        cat1Wins = cat1Wins * -1;
+    }
+    if (cat2Wins < 0) {
+        cat2Wins = cat2Wins * -1;
+    }
+
+    if (cat1Wins < cat2Wins) {
+        return 'cat1';
+    } else if (cat2Wins < cat1Wins) {
+        return 'cat2';
+    } else {
+        return 'os gatos trombam e o rato foge';
+    }
 }
 
 // Desafio 8
