3d6e659b2d6dbecae41653f0663397e16ba3b3ac
Ramon Paschoal <rpaschoal90@gmail.com>
2021-04-28 17:38:35 -0300

    Exercicio 6 tentativa 1

--- src/challenges.js
+++ src/challenges.js
@@ -40,8 +40,14 @@ function highestCount() {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if ((cat1 - mouse) > (cat2 - mouse)) {
+    return 'cat2'
+} else if ((cat1 - mouse) < (cat2 - mouse)) {
+  return 'cat1'
+} else {
+    return 'Os gatos trombam e o rato foge'
+}
 }
 
 // Desafio 8
