ac38ed02428db46f10a0d2dba4526a014b8f4c05
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-04-28 15:37:24 -0300

    adding the 6th challenge

--- src/challenges.js
+++ src/challenges.js
@@ -1,43 +1,57 @@
 // Desafio 1
-function compareTrue(boolean1,boolean2) {
-  if (boolean1 === true && boolean2 === true){
+function compareTrue(boolean1, boolean2) {
+  if (boolean1 === true && boolean2 === true) {
     return true;
-  }else {
+  } else {
     return false;
-  };
-};
+  }
+}
 
 // Desafio 2
-function calcArea(base,height) {
-  return (base*height/2);
-};
+function calcArea(base, height) {
+  return (base * height / 2);
+}
 
 // Desafio 3
 function splitSentence(sentence) {
-  return sentence.split(" ");
-};
+  return sentence.split(' ');
+}
+
+console.log(splitSentence('go Trybe'))
 
 // Desafio 4
 function concatName(array) {
   let phrase = '';
-  for(let index =0; index< array.length;index+=1){
-    if(index === 0){
-      phrase+=`${array[array.length-1]}, `
-    }else if(index === (array.length-1) ){
-      phrase+= array[0];
-    };
-  };
+  for (let index = 0; index < array.length; index += 1) {
+    if (index === 0) {
+      phrase += `${array[array.length-1]}, `;
+    } else if (index === (array.length-1)) {
+      phrase += array[0];
+    }
+  }
   return phrase;
-};
+}
 
 // Desafio 5
 function footballPoints(wins,ties) {
-  return (wins*3+ties);
-};
+  return (wins * 3 + ties);
+}
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let count = 0;
+  let hightestIndex = 0;
+  for (let index = 1; index < array.length; index +=1) {
+    if (array[index] > array[hightestIndex]) {
+      hightestIndex = index;
+    }
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[hightestIndex] === array[index]) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
