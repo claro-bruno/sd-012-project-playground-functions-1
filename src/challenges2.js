56be20211fd60bce7f14a2af048ce5dce50ab2d4
MGSiqueira <marcos.siqueira98@gmail.com>
2021-04-28 19:42:50 -0300

    Resolve o exxercício 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,14 @@
 // Desafio 10
-function techList() {
-  
+function techList(array, name) {
+    array.sort();
+    let respArray = [];
+    let technologies = {};
+    for (let objeto in array) {
+        respArray[objeto] = {tech: array[objeto], name: name} 
+    };
+    return respArray;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'))
 
 // Desafio 11
 function generatePhoneNumber() {
