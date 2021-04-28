71d8eb2c7d1b1b129f26cac58ac533bdc5cd1723
rachidb5 <jordan.rachid@gmail.com>
2021-04-28 17:43:18 -0300

    desafio1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,7 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a,b) {
   // seu código aqui
+  return a == true && b == true
 }
 
 // Desafio 2
---
+++ src/testes.js
@@ -0,0 +1,7 @@
+// Desafio 1
+function compareTrue(a,b) {
+    // seu código aqui
+    return a == true && b == true
+  }
+
+  console.log(compareTrue(true,false))
