dc9e7bf5312bd0dc8cb048fd24554bf977beac4b
Igor.Duarte <igorhdsouza@gmail.com>
2021-04-28 17:37:42 -0300

    Corrige erro do requisito 04 (espero que dessa vez funcione :p)

--- src/challenges.js
+++ src/challenges.js
@@ -24,7 +24,9 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(arrayDeString) {
-  let concatenada = (arrayDeString.slice(-1), arrayDeString[0]);
+  let string1 = arrayDeString[arrayDeString.length = 1];
+  let string2 = arrayDeString[0];
+  let concatenada = (string1 + ', ' + string2);
   
   return concatenada;
 }
