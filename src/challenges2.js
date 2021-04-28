195dd88709b2221a8097edf9740edf6ec384e514
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-28 16:30:28 -0300

    Completed requirement 10 | techList()

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+let stringArray1 = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+let emptyStringArray = [];
+let name1 = "Lucas";
+
+function techList(stringArray, name) {
+  if (stringArray.length === 0) {
+    return 'Vazio!';
+  }
+  let sortedArray = stringArray.sort();
+  let objectsList = [];
+  for (let index in sortedArray) {
+    let object = {tech: sortedArray[index], name: name};
+    objectsList.push(object);
+  }
+  return objectsList;
 }
+console.log(techList(stringArray1, name1));
+console.log(techList(emptyStringArray, name1));
 
 // Desafio 11
 function generatePhoneNumber() {
