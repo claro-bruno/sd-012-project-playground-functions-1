53ee0336795f99c0cffcbdb4dd27fc6912fa5ac5
Teika <shimworks.tk@gmail.com>
2021-04-28 15:04:03 -0300

    resolve desafio 03

--- src/challenges.js
+++ src/challenges.js
@@ -14,9 +14,10 @@ function calcArea(base, height) {
 };
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+function splitSentence(string) {
+ array = string.split(" ");
+ return array;
+};
 
 // Desafio 4
 function concatName() {
