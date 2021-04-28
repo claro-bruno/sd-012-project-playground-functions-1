88047cba13f11937b19a4bfa67026fe5025999bc
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-04-28 18:50:03 -0300

    Terminando exercicio 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+ let palavras = frase.split(" ")
+ return palavras
 }
+console.log(splitSentence("go trybe"))
 
 // Desafio 4
 function concatName() {
