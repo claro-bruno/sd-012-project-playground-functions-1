68f97dd3b7a1a140e6ee0e36a5b0b7bbea610493
martinsph <ph.mmartins@gmail.com>
2021-04-28 16:21:30 -0300

    adiciona requisitos 2 e 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,14 +14,24 @@ function compareTrue(valor1, valor2) {
 console.log(compareTrue(false, false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
+
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  let aux = str.split(" ");
+  return aux;
 }
+
+console.log(splitSentence('go Trybe'))
+console.log(splitSentence('vamo que vamo'))
+console.log(splitSentence('foguete'))
 
 // Desafio 4
 function concatName() {
