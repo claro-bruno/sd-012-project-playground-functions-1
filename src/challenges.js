556c5312dc2b92632c6a7dd72ec9e464e88b8c67
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-28 19:03:28 -0300

    requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -66,14 +66,14 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbers) {
   let outputArray = [];
-  for (let index in numbers){
-    if (((numbers[index] % 3) === 0) && ((numbers[index] % 5) === 0)){
+  for (let index in numbers) {
+    if (((numbers[index] % 3) === 0) && ((numbers[index] % 5) === 0)) {
       outputArray.push('fizzBuzz');
-    }else if ((numbers[index] % 3) === 0){
+    } else if ((numbers[index] % 3) === 0) {
       outputArray.push('fizz');
-    }else if ((numbers[index] % 5) === 0){
+    } else if ((numbers[index] % 5) === 0) {
       outputArray.push('buzz');
-    }else {
+    } else {
       outputArray.push('bug!');
     };
   };
@@ -82,12 +82,71 @@ function fizzBuzz(numbers) {
 // console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(inputString) {
+  let outputString = '';
+  for (let index in inputString) {
+    switch (inputString[index]) {
+      case 'a':
+        outputString += '1'
+        break;
+
+      case 'e':
+        outputString += '2'
+        break;
+
+      case 'i':
+        outputString += '3'
+        break;
+
+      case 'o':
+        outputString += '4'
+        break;
+
+      case 'u':
+        outputString += '5'
+        break;
+      
+      default:
+        outputString += inputString[index];
+    }
+  }
+  return outputString;
 }
-function decode() {
-  // seu código aqui
+// console.log(encode("Hey there, Delilah What's it like in New York City? I'm a thousand miles away But girl tonight you look so pretty Yes, you do Times Square can't shine as bright as you I swear it's true  Hey there, Delilah Don't you worry about the distance I'm right there if you get lonely Give this song another listen Close your eyes Listen to my voice it's my disguise I'm by your side  Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me What you do to me  Hey there, Delilah I know times are getting hard But just believe me girl Someday I'll pay the bills with this guitar We'll have it good We'll have the life we knew we would My word is good  Hey there, Delilah I've got so much left to say If every simple song I wrote to you Would take your breath away I'd write it all Even more in love with me you'd fall We'd have it all  Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me  A thousand miles seems pretty far But they've got planes and trains and cars I'd walk to you if I had no other way Our friends would all make fun of us And we'll just laugh along because we know That none of them have felt this way Delilah I can promise you That by the time that we get through The world will never ever be the same And you're the blame  Hey there, Delilah You be good and don't you miss me Two more years and you'll be done with school And I'll be making history like I do You know it's all because of you We can do whatever we want to Hey there Delilah heres to you This one's for you  Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me What you do to me Oh oh oh Oh oh oh"));
+// console.log('----------------------')
+
+function decode(inputString) {
+  let outputString = '';
+  for (let index in inputString) {
+    switch (inputString[index]) {
+      case '1':
+        outputString += 'a'
+        break;
+
+      case '2':
+        outputString += 'e'
+        break;
+
+      case '3':
+        outputString += 'i'
+        break;
+
+      case '4':
+        outputString += 'o'
+        break;
+
+      case '5':
+        outputString += 'u'
+        break;
+      
+      default:
+        outputString += inputString[index];
+    }
+  }
+  return outputString;
 }
+// console.log(decode("Hey there, Delilah What's it like in New York City? I'm a thousand miles away But girl tonight you look so pretty Yes, you do Times Square can't shine as bright as you I swear it's true  Hey there, Delilah Don't you worry about the distance I'm right there if you get lonely Give this song another listen Close your eyes Listen to my voice it's my disguise I'm by your side  Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me What you do to me  Hey there, Delilah I know times are getting hard But just believe me girl Someday I'll pay the bills with this guitar We'll have it good We'll have the life we knew we would My word is good  Hey there, Delilah I've got so much left to say If every simple song I wrote to you Would take your breath away I'd write it all Even more in love with me you'd fall We'd have it all  Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me  A thousand miles seems pretty far But they've got planes and trains and cars I'd walk to you if I had no other way Our friends would all make fun of us And we'll just laugh along because we know That none of them have felt this way Delilah I can promise you That by the time that we get through The world will never ever be the same And you're the blame  Hey there, Delilah You be good and don't you miss me Two more years and you'll be done with school And I'll be making history like I do You know it's all because of you We can do whatever we want to Hey there Delilah heres to you This one's for you  Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me Oh it's what you do to me What you do to me Oh oh oh Oh oh oh"));
+
 
 module.exports = {
   calcArea,
