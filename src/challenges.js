b9497a090b4aabb9f494b5315f5ea18e19ad2f5a
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-04-28 17:23:51 -0300

    7 - adicionado a funçao math.abs para tornar os numeros absolutos e a conta ficar mais precisa

--- src/challenges.js
+++ src/challenges.js
@@ -68,14 +68,16 @@ return count;
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-if((cat1-mouse)<(cat2-mouse)){
+
+if(Math.abs(cat1-mouse)<Math.abs(cat2-mouse)){
   return 'cat1';
-}else if((cat2-mouse)<(cat1-mouse)){
+}else if(Math.abs(cat2-mouse)<Math.abs(cat1-mouse)){
   return 'cat2';
 }else
 return 'os gatos tromam e o rato foge';
 
 }
+console.log(catAndMouse(1,0,2))
 
 
 // Desafio 8
