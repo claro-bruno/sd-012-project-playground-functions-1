4ad4d9ca65fa8fbe067379f440742b6a1a0ddd9b
camila-mp <ca.malvp@gmail.com>
2021-04-28 16:52:56 -0300

    voltando para o codigo que deu correto para desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -6,52 +6,49 @@ function compareTrue(valor1, valor2) {
     return false;
   }
 }
-
-console.log(compareTrue(false, false))
-
 // Desafio 2
 function calcArea(base, height) {
   let area = (base*height)/2
   return area
 }
 
-// // Desafio 3
-// function splitSentence(sentence) {
+// Desafio 3
+function splitSentence(sentence) {
   
-// }
+}
 
-// // Desafio 4
-// function concatName() {
-//   // seu código aqui
-// }
+// Desafio 4
+function concatName() {
+  // seu código aqui
+}
 
-// // Desafio 5
-// function footballPoints() {
-//   // seu código aqui
-// }
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
 
-// // Desafio 6
-// function highestCount() {
-//   // seu código aqui
-// }
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
 
-// // Desafio 7
-// function catAndMouse() {
-//   // seu código aqui
-// }
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
 
-// // Desafio 8
-// function fizzBuzz() {
-//   // seu código aqui
-// }
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
 
-// // Desafio 9
-// function encode() {
-//   // seu código aqui
-// }
-// function decode() {
-//   // seu código aqui
-// }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
 module.exports = {
   calcArea,
