e241388c0ac35759453d8a2152018cd08e6125a4
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-04-28 14:43:47 -0300

    Resolvendo requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(num1, num2) {
+    if(num1 === true && num2 === true){
+      return true;
+    }
+    else{
+      return false;
+    };
+};
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea() {
