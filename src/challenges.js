2b6c914708c15399ffa22a94c581dd8e74c9312f
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-28 18:37:27 -0300

    Pretende resolver requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -40,10 +40,15 @@ function highestCount(array) {
   return count;
 }
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if ((cat1-mouse) === (cat2-mouse)) {
+    return "os gatos trombam e o rato foge";
+  } else if ((cat1-mouse) > (cat2-mouse)){
+    return "cat1";
+  } else {
+    return "cat2";
+  }
 }
-
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
