8c071a25757a276e1b52e7622dbaaaf816b9f126
Laine Moura <lainermoura@gmail.com>
2021-04-28 17:51:42 -0300

    resolucao do exercicio 3

--- src/challenges.js
+++ src/challenges.js
@@ -4,13 +4,13 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea(base,altura) {
+function calcArea(base, altura) {
   return (base * altura) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split('');
 }
 
 // Desafio 4
