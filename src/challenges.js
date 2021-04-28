ed505d9b8860e680de19623f61ec228a55f14ddc
Martin <escorpmartin97@gmail.com>
2021-04-28 18:21:12 -0300

    corrige erros exercise anterior e adiciona exercise 4

--- src/challenges.js
+++ src/challenges.js
@@ -14,52 +14,56 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
-  let array = string.split(" ")
+  let array = string.split(' ');
   return array;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let primeiro = [];
+  let ultimo = [];
+  primeiro = array[0];
+  ultimo = array[array.length-1];
+  return [ultimo, primeiro];
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   highestCount,
+//   splitSentence,
+// };
