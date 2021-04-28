3418828a4f1a5a3e0685bd676c31869bdded77dc
Caio César <caiovg32@gmail.com>
2021-04-28 17:20:52 -0300

    Req.6

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,6 @@
 // Desafio 1
 function compareTrue(param1, param2) {
+
   if(param1 === true && param2 === true) {
     return true;
   }else{
@@ -9,6 +10,7 @@ function compareTrue(param1, param2) {
 
 // Desafio 2
 function calcArea(base, height) {
+  
   return ((base * height) / 2);
 }
 
@@ -25,14 +27,28 @@ function concatName(nomes) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  var points;
+  let points;
   points = (wins * 3) + ties;
   return points;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numeros) {
+  let highestNum = 0;
+  let repeticao = 0;
 
+  for(let index = 0; index < numeros.length; index += 1) {
+    if(highestNum < numeros[index]){
+      highestNum = numeros[index];
+    }
+  }
+
+  for(let index = 0; index < numeros.length; index += 1) {
+    if(highestNum === numeros[index]) {
+      repeticao += 1;
+    }
+  }
+   return repeticao;
 }
 
 // Desafio 7
