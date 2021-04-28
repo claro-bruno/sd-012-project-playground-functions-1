365db7cb35af53fc9c4aa5344479e2f7c97b7aad
Marcus Santos <smarcuslemos@gmail.com>
2021-04-28 16:45:39 -0300

    Add os exercicios 6 e 7.

--- src/challenges.js
+++ src/challenges.js
@@ -76,25 +76,67 @@ function footballPoints(wins, ties) {
 
 console.log(footballPoints(wins, ties));
 
-*/
+
 
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+let array = [0, 0, 0];
+
+function highestCount(array) {
+
+  let higherNumber = array[0];
+
+  for (let index = 0; index < array.length; index += 1){
+    if (array[index] > higherNumber) {
+      higherNumber = array[index];
+    } else {
+      higherNumber = higherNumber;
+    };
+  };
+  
+  let numberOfRepetitions = 0;
+  
+  for (let counter = 0; counter < array.length; counter += 1){
+    if (array[counter] === higherNumber) {
+      numberOfRepetitions += 1;
+    }; 
+  };
+  
+  return numberOfRepetitions;
+
 }
 
-/*
+console.log(highestCount(array));
+
+
+
+
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+
+let cat1 = 6;
+let cat2 = 6;
+
+function catAndMouse(cat1, cat2) {
+  if (cat1 < cat2) {
+    return 'cat1';
+  } else if (cat2 < cat1){
+    return 'cat2';
+  } else {return "os gatos trombam e o rato foge"; }; 
+};
+
+console.log(catAndMouse(cat1, cat2))
+
+*/
+
 
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
 }
+
+/*
 
 // Desafio 9
 function encode() {
