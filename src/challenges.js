21cc109f839f907c846887052f9ce835bda7c53c
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-04-28 20:11:48 -0300

    resolve desafio 6 + referencia codigo do colega que auxiliou

--- src/challenges.js
+++ src/challenges.js
@@ -43,21 +43,35 @@ function concatName(strings) {
 // console.log(concatName(strings));
 
 // Desafio 5
-function footballPoints(wins, ties) {
+function footballPoints(wins, ties){
   let points = (wins*3) + ties;
   return points;
 }
 
-console.log(footballPoints(14, 8));
-console.log(footballPoints(1, 2));
-console.log(footballPoints(0, 0));
-
+// console.log(footballPoints(14, 8));
+// console.log(footballPoints(1, 2));
+// console.log(footballPoints(0, 0));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(numbers) {
+    let highIndex = 0;
+    let repetitions = 0;
+    for(let index in numbers) {
+      if(numbers[index] > highIndex) {
+        highIndex = numbers[index];
+      }
+    }
+    for(let number of numbers){
+      if( number === highIndex){
+        repetitions += 1;
+      }
+    }
+    return repetitions;
+  }
 
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,8 +1,19 @@
-function footballPoints(wins, ties) {
-  let points = (wins*3) + ties;
-  return points;
-}
+function highestCount(numbers) {
+    let highIndex = 0;
+    let repetitions = 0;
+    for(let index in numbers) {
+      if(numbers[index] > highIndex) {
+        highIndex = numbers[index];
+      }
+    }
+    for(let number of numbers){
+      if( number === highIndex){
+        repetitions += 1;
+      }
+    }
+    return repetitions;
+  }
 
-console.log(footballPoints(14, 8));
-console.log(footballPoints(1, 2));
-console.log(footballPoints(0, 0));
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
