67e9a04771ee1f285fc478281fdc6e72427b166a
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-28 15:59:30 -0300

    Completed requirement 8 | fizzBuzz()

--- src/challenges.js
+++ src/challenges.js
@@ -86,9 +86,28 @@ function catAndMouse(mouse, cat1, cat2) {
 console.log(catAndMouse(mouse, cat1, cat2));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+let numberArray4 = [2, 15, 7, 9, 45];
+let numberArray5 = [7, 9];
+let numberArray6 = [9, 25];
+
+function fizzBuzz(numberArray) {
+  let stringArray = [];
+  for (let index in numberArray) {
+    if (numberArray[index] % 3 === 0 && numberArray[index] % 5 !== 0) {
+      stringArray.push("fizz");
+    } else if (numberArray[index] % 5 === 0 && numberArray[index] % 3 !== 0) {
+      stringArray.push("buzz");
+    } else if (numberArray[index] % 3 === 0 && numberArray[index] % 5 === 0) {
+      stringArray.push("fizzbuzz");
+    } else {
+      stringArray.push("bug!");
+    }
+  }
+  return stringArray;
 }
+console.log(fizzBuzz(numberArray4));
+console.log(fizzBuzz(numberArray5));
+console.log(fizzBuzz(numberArray6));
 
 // Desafio 9
 function encode() {
