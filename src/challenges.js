60ec500086ca0038dd190c4393eb011fcfca88f2
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-28 15:05:10 -0300

    completa Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(bool1, bool2) {
+
+  if ((bool1 && bool2) === true){
+    return true;
+  } else {
+    return false
+  }
+
 }
 
 // Desafio 2
