b26b588203ff77e1f52ec24a9fa38857066d3650
Rafael Passos <rafael.9347@hotmail.com>
2021-04-28 19:49:54 -0300

    Faltando Apenas a 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,12 @@
 // Desafio 1
+let index = 0;
 function compareTrue( bolleano1, bolleano2) {
 
   if (bolleano1 === true && bolleano2 === true) {
     return true;
   };
   return false;
-};
+}
 
 // Desafio 2
 function calcArea(base, height) {
@@ -14,18 +15,17 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(frase) {
-let marcador= 0;
-let palavras= [];
+let marcador = 0;
+let palavras = [];
 let ffinal= [];
-
   for (let index = 0; index < frase.length; index += 1){
     if (frase[index] === ' '){
         for (marcador; marcador < index; marcador += 1){
           palavras += frase[marcador];
         };
         ffinal.push(palavras);
-        palavras=[];
-        marcador=index+1;
+        palavras = [];
+        marcador = index + 1;
     };
   };
   for (marcador;marcador < frase.length;marcador += 1){
@@ -63,7 +63,7 @@ let contador = 0;
   return contador;
 }
 
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -79,20 +79,79 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'os gatos trombam e o rato foge';
   }
 }
-console.log(catAndMouse(10,8,8));
-
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(buzzer) {
+  let fizer = [];
+  for(index =0 ; index <= buzzer.length; index += 1){
+    if(buzzer[index] % 3 === 0 && buzzer[index] % 5 !== 0 ){
+      fizer.push('fizz');
+    }
+    else if(buzzer[index] % 5 === 0 && buzzer[index] % 3 !== 0){
+      fizer.push('buzz');
+    }
+    else if(buzzer[index] % 5 === 0 && buzzer[index] % 3 === 0){
+      fizer.push('fizzBuzz');
+    }
+    else{
+      fizer.push('bug!')
+    }
+  }
+return fizer;  
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let final = [];
+    for (index = 0; index < string.length; index += 1){
+      if(string[index] === 'a'){
+        final += '1';
+      }
+      else if(string[index] === 'e'){
+        final += '2';
+      }
+      else if(string[index] === 'i'){
+        final += '3';
+      }
+      else if(string[index] === 'o'){
+        final += '4';
+      }
+      else if(string[index] === 'u'){
+        final += '5';
+      }
+      else{
+        final +=string[index];
+      }
+    }
+return final
 }
-function decode() {
-  // seu código aqui
+
+
+function decode(string1) {
+  let final1 = [];
+  for (index = 0; index < string1.length; index += 1){
+    if(string1[index] === '1'){
+      final1 += 'a';
+    }
+    else if (string1[index] === '2'){
+      final1 += 'e';
+    }
+    else if (string1[index] === '3'){
+      final1 += 'i';
+    }
+    else if (string1[index] === '4'){
+      final1 += 'o';
+    }
+    else if (string1[index] === '5'){
+      final1 += 'u';
+    }
+    else{
+      final1 += string1[index];
+    }
+  }
+return final1
 }
+
+console.log(decode('h3 th2r2!'));
 
 module.exports = {
   calcArea,
