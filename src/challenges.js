8b2526400676118da653f1a6e54695968242955d
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-28 17:15:50 -0300

    Desafio 6 resolvido e 1 melhorado

--- src/challenges.js
+++ src/challenges.js
@@ -1,23 +1,21 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  if (valor1 === true && valor2 === false || valor1 == false && valor2 === true) {
-    return false;
-  } else if (valor1 === false && valor2 === false) {
-    return false;
-  } else {
+  if (valor1 === true && valor2 === true) {
     return true;
+  } else {
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base*height)/2;
+  let area = (base * height)/2;
   return area;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  let arrayDeStrings = string.split(" "); 
+  let arrayDeStrings = string.split(' '); 
   return arrayDeStrings;
 }
 
@@ -25,19 +23,35 @@ function splitSentence(string) {
 function concatName(array) {
   let primeiroItem = array[0];
   let segundoItem = array[array.length-1];
-  return (segundoItem+', '+primeiroItem+'.');
+  return (segundoItem + ', ' + primeiroItem + '.');
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let pontos = (wins*3)+(ties*1);
+  let pontos = (wins * 3) + (ties * 1);
   return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maiorNumero = 0;
+  let contador = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > array[index+1] && array[index] >= maiorNumero) {
+      maiorNumero = array[index];
+    }
+  }
+  for (let index2 = 0; index2 < array.length; index2 += 1) {
+    if (maiorNumero === array[index2]) {
+      contador += 1;
+    }
+  }
+  return contador;
 }
+
+let x = [0, 4, 4, 4, 9, 2, 1];
+console.log(highestCount(x));
+
 
 // Desafio 7
 function catAndMouse() {
