188e03b53fa308c9b9d6b8d806503cbca63ec30b
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-28 18:19:12 -0300

    Pretende resolver requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -19,43 +19,42 @@ function splitSentence(string) {
 function concatName(array) {
   return ("'" + array[(array.length - 1)] + ", " + array[0] + "'");
 }
-  // Desafio 5
-  function footballPoints() {
-    // seu código aqui
-  }
-  
-  // Desafio 6
-  function highestCount() {
-    // seu código aqui
-  }
-  
-  // Desafio 7
-  function catAndMouse() {
-    // seu código aqui
-  }
-  
-  // Desafio 8
-  function fizzBuzz() {
-    // seu código aqui
-  }
-  
-  // Desafio 9
-  function encode() {
-    // seu código aqui
-  }
-  function decode() {
-    // seu código aqui
-  }
-  
-  module.exports = {
-    calcArea,
-    catAndMouse,
-    compareTrue,
-    concatName,
-    decode,
-    encode,
-    fizzBuzz,
-    footballPoints,
-    highestCount,
-    splitSentence,
-  };
+// Desafio 5
+function footballPoints(wins, ties) {
+  return (3 * wins) + ties;
+}
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
+
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
+
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
+
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  highestCount,
+  splitSentence,
+};
