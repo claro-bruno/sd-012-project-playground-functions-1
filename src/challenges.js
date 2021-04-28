331f145387f473b0cccc0939a732094532cf2260
rachidb5 <jordan.rachid@gmail.com>
2021-04-28 20:49:41 -0300

    desafio7

--- src/challenges.js
+++ src/challenges.js
@@ -51,12 +51,12 @@ function highestCount(numbers) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if (cat1 - mouse < cat2 - mouse){
-      return "cat1";
-  }else if(cat1 - mouse > cat2 - mouse) {
-      return "cat2";
-  }else{
-      return "os gatos trombam e o rato foge"
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+    return 'cat1';
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
   }
 }
 
