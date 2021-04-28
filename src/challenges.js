e9180a43998b6d9bb30df0d82a8ed501bed0fc74
Emerson Filho <emerson.dev.machado@gmail.com>
2021-04-28 16:34:42 -0400

    eslint

--- src/challenges.js
+++ src/challenges.js
@@ -4,28 +4,28 @@ function compareTrue(param1, param2) {
     return true;
   } else {
     return false;
-  };
-};
+  }
+}
 
 // Desafio 2
 function calcArea(base, height) {
   return (base * height) / 2;
-};
+}
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
-};
+}
 
 // Desafio 4
 function concatName(array) {
   return `${array[array.length - 1]}, ${array[0]}`;
-};
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3) + (ties * 1);
-};
+}
 
 // Desafio 6
 function highestCount(array) {
@@ -35,16 +35,16 @@ function highestCount(array) {
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] > maiorNumero) {
       maiorNumero = array[index];
-    };
-  };
+    }
+  }
 
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] === maiorNumero) {
       contador += 1;
-    };
-  };
+    }
+  }
   return contador;
-};
+}
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -56,23 +56,23 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat1';
   } else {
     return 'os gatos trombam e o rato foge';
-  };
-};
+  }
+}
 
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
-};
+}
 
 // Desafio 9
 function encode() {
   // seu código aqui
-};
+}
 
 // Desafio 10
 function decode() {
   // seu código aqui
-};
+}
 
 module.exports = {
   calcArea,
@@ -85,4 +85,4 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
-};
+}
