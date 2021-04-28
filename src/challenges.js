eba003bf7b0d3e8b585c04007e19c443513e62a0
Stefano <stefanooliveira1@gmail.com>
2021-04-28 14:56:54 -0300

    resolve ex1

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,11 @@
 // Desafio 1
-function compareTrue() {
-a
-
-  // seu código aqui
+function compareTrue(valor1, valor2) {
+if (valor1 === true && valor2 === true) {
+  return true;
+} else {
+  return false;
 }
+};
 
 // Desafio 2
 function calcArea() {
