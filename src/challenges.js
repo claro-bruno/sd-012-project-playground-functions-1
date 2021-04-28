39fd3a24df267bcc90c70136dc4cffea7af2baa9
Josimar Souza <contact.josimarsouza@gmail.com>
2021-04-28 15:49:52 -0300

    Resolvido o requisito 4 do projeto playground functions

--- src/challenges.js
+++ src/challenges.js
@@ -37,8 +37,9 @@ function splitSentence(frase) {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(nameArray) {
   // seu código aqui
+  return `${nameArray[nameArray.length - 1]}, ${nameArray[0]}`
 }
 
 // Desafio 5
