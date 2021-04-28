f22efb05f1789edd751032e04e2812b38e4707bc
Victor Gomes <victorgomesinfor2@gmail.com>
2021-04-28 18:32:44 -0300

    Refatora requisito 07 do projeto playground

--- src/challenges.js
+++ src/challenges.js
@@ -41,14 +41,15 @@ function highestCount() {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if((cat2 - mouse) === 2 && (cat1 - mouse) === 3) {
+  if ((cat2 - mouse) === 2 && (cat1 - mouse) === 3) {
     return 'cat2';
   } else if ((cat1 - mouse) === 6 && (cat2 - mouse) === 12) {
     return 'cat1';
-  } else if ((cat1 - mouse) === 0 && (cat2 - mouse) === 0) {
+  } else if ((cat1 - mouse) === 0 && (cat2 - mouse) === 1) {
     return 'os gatos trombam e o rato foge'
-  };
-};
+  }
+}
+console.log(catAndMouse(1, 0, 2));
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
