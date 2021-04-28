0d4d172e66a88a8443ef731aded858b6c7c73871
Thalles Carneiro <thallescarneiro@gmail.com>
2021-04-28 19:07:48 -0300

    Requisito 10
    Cria uma função de Lista de Tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,31 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(technologies, name) {
+  let techsAndNameArray = []
+  for (let index = 0; index < technologies.length; index += 1) {
+    techsAndNameArray[index] = {
+      tech: technologies[index],
+      name: name
+    };
+  };
+  for (let iteration = 1; iteration < techsAndNameArray.length; iteration += 1) {
+    for (let indexObject = 0; indexObject < iteration; indexObject += 1) {
+      let currentObject = techsAndNameArray[iteration]['tech'];
+      let previusObject = techsAndNameArray[indexObject]['tech'];
+      if (currentObject < previusObject) {
+        let auxObject = currentObject;
+        techsAndNameArray[iteration]['tech'] = previusObject;
+        techsAndNameArray[indexObject]['tech'] = auxObject;
+      };
+    };
+  };
+  let result;
+  if (technologies.length > 0) {
+    result = techsAndNameArray;
+  } else {
+    result = 'Vazio!';
+  }
+  return result;
+};
 
 // Desafio 11
 function generatePhoneNumber() {
