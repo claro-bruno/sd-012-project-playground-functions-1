dbdbe93e058540d5466691f62c4c3ea28f3a64d6
Teika <shimworks.tk@gmail.com>
2021-04-28 18:33:51 -0300

    resolve desafio 09

--- src/challenges.js
+++ src/challenges.js
@@ -88,59 +88,59 @@ function fizzBuzz(arrayBuzz) {
 
 // Desafio 9
 function encode(toEncode) {
-  let codificado = ''
+  let codificado = '';
   for (let key in toEncode) {
-    if (toEncode[key] === 'a' || toEncode[key] === 'e' || toEncode[key] === 'i' || toEncode[key] === 'o' || toEncode[key] === 'u' ) {
+    if (toEncode[key] === 'a' || toEncode[key] === 'e' || toEncode[key] === 'i' || toEncode[key] === 'o' || toEncode[key] === 'u') {
       switch (toEncode[key]) {
-        case 'a':
-          codificado += '1'
-          break;
-        case 'e':
-          codificado += '2'
-          break;
-        case 'i':
-          codificado += '3'
-          break;
-        case 'o':
-          codificado += '4'
-          break;
-        case 'u':
-          codificado += '5'
-          break; 
-      };
+      case 'a':
+        codificado += '1';
+        break;
+      case 'e':
+        codificado += '2';
+        break;
+      case 'i':
+        codificado += '3';
+        break;
+      case 'o':
+        codificado += '4';
+        break;
+      case 'u':
+        codificado += '5';
+        break;
+      }
     } else {
-      codificado += toEncode[key]
+      codificado += toEncode[key];
     }
   }
-  return codificado
+  return codificado;
 }
 
 function decode(toDecode) {
-  let decodificado = ''
-  for (let key in toEncode) {
-    if (toEncode[key] === 'a' || toEncode[key] === 'e' || toEncode[key] === 'i' || toEncode[key] === 'o' || toEncode[key] === 'u' ) {
-      switch (toEncode[key]) {
-        case 'a':
-          codificado += '1'
-          break;
-        case 'e':
-          codificado += '2'
-          break;
-        case 'i':
-          codificado += '3'
-          break;
-        case 'o':
-          codificado += '4'
-          break;
-        case 'u':
-          codificado += '5'
-          break; 
-      };
+  let decodificado = '';
+  for (let key in toDecode) {
+    if (toDecode[key] === '1' || toDecode[key] === '2' || toDecode[key] === '3' || toDecode[key] === '4' || toDecode[key] === '5') {
+      switch (toDecode[key]) {
+      case '1':
+        decodificado += 'a';
+        break;
+      case '2':
+        decodificado += 'e';
+        break;
+      case '3':
+        decodificado += 'i';
+        break;
+      case '4':
+        decodificado += 'o';
+        break;
+      case '5':
+        decodificado += 'u';
+        break;
+      }
     } else {
-      codificado += toEncode[key]
+      decodificado += toDecode[key];
     }
   }
-  return decodificado
+  return decodificado;
 }
 
 module.exports = {
