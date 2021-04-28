456ce33c1790764bcde434e2d08d08e922140ffc
Roberval Filho <rslfilho@gmail.com>
2021-04-28 14:50:13 -0300

    R1 - Compare True

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  xablau
-}
+function compareTrue(bool1, bool2) {
+  if (bool1 === true && bool2 === true) {
+    return true;
+  }
+  else {
+    return false;
+  };
+};
 
 // Desafio 2
 function calcArea() {
