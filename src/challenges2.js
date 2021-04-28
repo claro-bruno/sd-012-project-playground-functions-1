1111a358939a5ca133d389cea219a185f98c42f8
Paulo Michael <paulo.michael@gmail.com>
2021-04-28 18:13:40 -0300

    desafio 10 - techList() - ordena lista de tecnologias e insere objetos em array

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,35 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function bubbleSort(array){
+  for(let bubble = 0; bubble < array.length - 1; bubble += 1){
+    for(let index = 0; index < array.length - bubble - 1; index += 1){
+        if(array[index] > array[index + 1]){
+            [array[index], array[index + 1]] = [array[index + 1], array[index]];
+        }
+    }
+  }
+  return array;
 }
+// let array = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// console.log(bubbleSort(array));
+
+function techList(technologies, name) {
+  let sortedTechnologies = [];
+  let objectList = [];
+  if(array.length === 0){
+    return 'Vazio!';
+  }
+  else{
+    sortedTechnologies = bubbleSort(technologies);
+    for(item of sortedTechnologies){
+      let objectInsertion = {"tech": item, "name": name};
+      objectList.push(objectInsertion);
+    }
+  }
+  return objectList;
+}
+// let array = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// let name = 'Lucas';
+// console.log(techList(array,name));
 
 // Desafio 11
 function generatePhoneNumber() {
