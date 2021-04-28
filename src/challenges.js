2a6844f1f12de01be3bc4155c6eb7e7ddc5dfe30
David Azevedo <zevid.dev@outlook.com>
2021-04-28 14:57:45 -0300

    Remove return unnecessary of the requirement 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 // Desafio 1
 function compareTrue(param1, param2) {
+  let confirm = false;
   if (param1 === true && param2 === true) {
-    return true;
-  } else {
-    return false;
+    confirm = true;
   }
+  return confirm;
 }
 
 // Desafio 2
