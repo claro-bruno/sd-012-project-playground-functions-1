0254e454092875e96c42896f558ab17e9183abac
Matheus Proenca <mproenca.marketing@gmail.com>
2021-04-28 18:51:44 -0300

    descomentando

--- src/challenges.js
+++ src/challenges.js
@@ -35,48 +35,47 @@ function concatName(array) {
   // seu código aqui
   let inicio = array[0];
   let final = array[array.length - 1];
-  return final + ' ' + inicio;
+  return final + ',' + inicio;
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
-// // Desafio 5
-// function footballPoints(wins, ties) {
-//   // seu código aqui
-// let wins = win * 3;
-// let ties = ties * 1;
-// let soma = wins + ties;
-//   return soma
+// Desafio 5
+function footballPoints(wins, ties) {
+  // seu código aqui
+  let wins = win * 3;
+  let ties = ties * 1;
+  let soma = wins + ties;
+  return soma;
+}
+console.log(footballPoints());
 
-// }
-// console.log(footballPoints());
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
 
-// // Desafio 6
-// function highestCount() {
-//   // seu código aqui
-// }
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
 
-// // Desafio 7
-// function catAndMouse() {
-//   // seu código aqui
-// }
+// Desafio 8
+function fizzBuzz(array) {
+  // seu código aqui
+  if (array % 3 === 0) return 'fizz';
+  if (array % 3 === 0) return 'buzz';
+  if (array % 3 === 0 && array % 3 === 0) return 'fizzbuzz';
+  return 'bug!';
+}
+console.log();
 
-// // Desafio 8
-// function fizzBuzz(array) {
-//   // seu código aqui
-//   if (array % 3 === 0) return 'fizz';
-//   if (array % 3 === 0) return 'buzz';
-//   if (array % 3 === 0 && array % 3 === 0) return 'fizzbuzz';
-//   return 'bug!';
-// }
-// console.log();
-
-// // Desafio 9
-// function encode() {
-//   // seu código aqui
-// }
-// function decode() {
-//   // seu código aqui
-// }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
 module.exports = {
   calcArea,
