babde558282d6f027dc19e602d200cec2f723782
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-04-28 15:26:52 -0300

    Requisito 5 - Cria funçao que calcula a quantidade de pontos no football

--- src/challenges.js
+++ src/challenges.js
@@ -34,8 +34,9 @@ function concatName(arrayPalavras) {
 }
 
 // Desafio 5
-function footballPoints() {
-    // seu código aqui
+function footballPoints(wins, ties) {
+    let points = wins * 3 + ties * 1;
+    return points;
 }
 
 // Desafio 6
