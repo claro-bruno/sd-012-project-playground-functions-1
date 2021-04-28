91b2c8b8036d6135b3034c681db6fd457fe3d62e
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-04-28 19:36:05 -0300

    Desafio 11 - Cria funçao que gera numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,48 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-    // seu código aqui
+function generatePhoneNumber(numbers) {
+    let numbersArray = numbers;
+    numbersArray.sort();
+    let maior = numbersArray[numbersArray.length - 1];
+    let menor = numbersArray[0];
+    let repete = 0;
+    let cont = 0;
+    for (let number of numbers) {
+        for (let num of numbersArray) {
+            if (number === num && repete < 3) {
+                repete += 1;
+            } else if (repete >= 3) {
+                cont = repete
+            }
+            repete = 0;
+        }
+    }
+    switch (true) {
+        case numbers.length != 11:
+            return 'Array com tamanho incorreto.';
+            break;
+        case (maior > 9 || menor < 0 || cont >= 3):
+            return 'não é possível gerar um número de telefone com esses valores';
+            break;
+        default:
+            let phoneNumber = '(';
+            phoneNumber += numbers[0];
+            phoneNumber += numbers[1];
+            phoneNumber += ') ';
+            phoneNumber += numbers[2];
+            phoneNumber += numbers[3];
+            phoneNumber += numbers[4];
+            phoneNumber += numbers[5];
+            phoneNumber += numbers[6];
+            phoneNumber += '-';
+            phoneNumber += numbers[7];
+            phoneNumber += numbers[8];
+            phoneNumber += numbers[9];
+            phoneNumber += numbers[10];
+            return phoneNumber;
+            break;
+    }
 }
 
 // Desafio 12
