7b50721f8dc5216da556a2136b2dbc93b532eaea
Euller Braz <euller.braz99@gmail.com>
2021-04-28 19:08:25 -0300

    Resolvido requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,9 +19,42 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumbers) {
+  let result = "";
+  let counter = 0;
+  if (arrayNumbers.length === 11) {
+    for (let index in arrayNumbers) {
+      counter = 0;
+      for (let index2 in arrayNumbers) {
+        if (arrayNumbers[index] === arrayNumbers[index2]) {
+          counter += 1;
+        }
+        if (counter === 3) {
+          break;
+        }
+      }
+      if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9 || counter === 3) {
+        result = "não é possível gerar um número de telefone com esses valores";
+        break;
+      } else {
+        if (index === "0") {
+          result += `(${arrayNumbers[index]}`;
+        } else if (index === "2") {
+          result += `)${arrayNumbers[index]}`;
+        } else if (index === "7") {
+          result += `-${arrayNumbers[index]}`;
+        } else {
+          result += arrayNumbers[index];
+        }
+      }
+    }
+  } else {
+    result = "Array com tamanho incorreto.";
+  }
+  return result;
 }
+
+console.log(generatePhoneNumber([1, 4, 3, 4, 5, 6, 7, 4, 2, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
