a79ee278b7ebd7cb6a657583aefecb64ab2f7628
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-04-28 15:41:31 -0300

    Resolve o requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -22,9 +22,12 @@ function calcArea(base, height) {
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let resultado = frase.split(" ");
+  
+  return resultado;
 }
+
 
 // Desafio 4
 function concatName() {
