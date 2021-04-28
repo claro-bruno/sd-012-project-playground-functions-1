c1fb2939886b031a1208c4eef0f9807c7f808bd3
Victor Emmanuel <victorpassos2012@gmail.com>
2021-04-28 18:44:16 -0300

    Desafio 9 feito

--- src/challenges.js
+++ src/challenges.js
@@ -1,27 +1,27 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(cond1, cond2, cond3, cond4) {
   // seu código aqui
   let result = false;
-  
-  if ((cond1 === cond2) && (cond3 === cond4) ) {
+  if ((cond1 === cond2) && (cond3 === cond4)) {
     result = true;
   } else {
     result = false;
   }
-  
   return result;
 }
-
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  return (base * height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence() {
   // seu código aqui
+  let result = [];
+
+  return result;
 }
 
 // Desafio 4
@@ -50,11 +50,60 @@ function fizzBuzz() {
 }
 
 // Desafio 9
-function encode() {
+// eslint-disable-next-line no-unused-vars
+function encode(string) {
   // seu código aqui
+  let result = '';
+  for (let letra of string) {
+    switch (letra) {
+      case 'a':
+        letra = '1';
+        break; 
+      case 'e':
+        letra = '2';
+        break; 
+      case 'i':
+        letra = '3';
+        break; 
+      case 'o':
+        letra = '4';
+        break; 
+      case 'u':
+       letra = '5';
+       break; 
+      default:
+        break;
+    }
+    result = result + letra;
+  }
+
+  return result;
 }
-function decode() {
+
+function decode(string) {
   // seu código aqui
+  let result = '';
+  for (let letra of string) {
+    switch (letra) {
+      case '1':
+        letra = 'a';
+        break; 
+      case '2':
+        letra = 'e';
+        break; 
+      case '3':
+        letra = 'i';
+        break; 
+      case '4':
+        letra = 'o';
+        break; 
+      case '5':
+       letra = 'u';
+       break; 
+      default:
+        break;
+    }
+    result = result + letra;
 }
 
 module.exports = {
