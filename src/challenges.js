632e09a2c06109809c36ad64a5cdcb0f7c5aa922
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-28 17:52:36 -0300

    add funçao concatName

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,15 @@
 // Desafio 1 - Verificação de números pares
 function compareTrue(valor1, valor2) {
-  if (valor1 % 2 == 0 && valor2 % 2 ==0) {
+  if (valor1 % 2 == 0 && valor2 % 2 == 0) {
     return('true');
   }
   else {
     return('false');
   }
 }
-console.log(compareTrue(89,56))
-console.log(compareTrue(73,11))
-console.log(compareTrue(98,42))
+console.log(compareTrue(89,56));
+console.log(compareTrue(73,11));
+console.log(compareTrue(98,42));
 
 
 
@@ -17,22 +17,42 @@ console.log(compareTrue(98,42))
 function calcArea(base, height) {
  return ((base * height) / 2);
 }
-console.log(calcArea(10,50))
-console.log(calcArea(5,2))
-console.log(calcArea(51,1))
+console.log(calcArea(10,50));
+console.log(calcArea(5,2));
+console.log(calcArea(51,1));
 
 
 
 // Desafio 3
-function splitSentence() {
-
-  // seu código aqui
+function splitSentence(frase) {
+  //let palavra = [];
+  for(let palavra in frase) {
+    console.log(frase[palavra])
+  }
+  //return frase[palavra];
+  // for (let index= 0; index < frase.length; index += 1) {
+    // array.push(frase[index])
+     //if (frase[index] == ' ') {
+     //  console.log()
+     //}
+     //console.log (frase[index]);
+   }  
+  return array;
+  }
 }
+  console.log(splitSentence('go Trybe'))
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+
+  // Desafio 4  
+let array = [ ];
+
+function concatName (array) {
+  return array[array.length - 1] + ', ' + array[0];  
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
+
 
 // Desafio 5
 function footballPoints() {
