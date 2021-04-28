4f5792fc9beaa5f2b1c8cfa159c627006dbce5eb
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-04-28 15:25:44 -0300

    Realiza quinto requisito

--- src/challenges.js
+++ src/challenges.js
@@ -32,9 +32,11 @@ function concatName(array) {
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontuacao = (wins * 3) + (ties * 1)
+  return pontuacao;
 }
+console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount() {
