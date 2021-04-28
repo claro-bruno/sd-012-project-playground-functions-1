8f7207b9f0127d33099a22a66be8dd18c87f4f16
martinsph <ph.mmartins@gmail.com>
2021-04-28 18:42:29 -0300

    adiciona requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,9 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
   let resultado = 0;
-  if (valor1 == false && valor2 == false) {
+  if (valor1 === false && valor2 === false) {
     resultado = false;
-    } if (valor2 == false || valor1 == false) {
+    } if (valor2 === false || valor1 === false) {
     resultado = false;
     } else {
     resultado = true;
@@ -11,7 +11,7 @@ function compareTrue(valor1, valor2) {
   return resultado;
 };
 
-console.log(compareTrue(false, false));
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, height) {
@@ -29,9 +29,9 @@ function splitSentence(str) {
   return aux;
 }
 
-console.log(splitSentence('go Trybe'))
-console.log(splitSentence('vamo que vamo'))
-console.log(splitSentence('foguete'))
+console.log(splitSentence('go Trybe'));
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
 
 // Desafio 4
 
@@ -45,14 +45,27 @@ console.log(concatName(['foguete','não', 'tem', 'ré']));
 console.log(concatName(['captian','my','captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+function footballPoints(wins, ties) {
+  let pontosTotais = (wins *3) + ties;
+  return pontosTotais;
+};
+
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(array) {
+  let maiorNumero = array[0];
+  for(index = 0; index < array.length; index += 1) {
+    if (array[index] > maiorNumero) {
+      maiorNumero = array[index];
+    }
+  }
+  return maiorNumero;
+};
+
+console.log(highestCount(9, 1, 2, 3, 9, 5, 7));
 
 // Desafio 7
 function catAndMouse() {
