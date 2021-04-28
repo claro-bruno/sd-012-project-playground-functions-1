3a182a08b271f8d387572bfb0e975a337e0df1c4
Emerson Filho <emerson.dev.machado@gmail.com>
2021-04-28 14:12:01 -0400

    remove comentarios

--- src/challenges.js
+++ src/challenges.js
@@ -9,13 +9,12 @@ function compareTrue(num1, num2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  // seu código aqui
   return (base * height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ')
 }
 
 // Desafio 4
