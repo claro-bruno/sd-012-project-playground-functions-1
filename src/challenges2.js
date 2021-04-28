f71650c530b5ad1c81101f254f42701cdce342ac
Renan Ferreira <renansantiago2013@hotmail.com>
2021-04-28 19:15:23 -0300

    Resolve o requisito 10(ajustes) - Tech list

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,14 +8,18 @@ function techList(tech, name) {
     }
     list.push(object);
   }
-  return list.sort(function(a, b) {
-    if (a.tech.toLowerCase() < b.tech.toLowerCase()) return -1;
-    else if (a.name.toLowerCase() > a.name.toLowerCase()) return 1;
-    return 0;
-  });
+  if (list = []) {
+    return "Vazio!"
+  } else {
+    return list.sort(function(a, b) {
+      if (a.tech.toLowerCase() < b.tech.toLowerCase()) return -1;
+      else if (a.name.toLowerCase() > a.name.toLowerCase()) return 1;
+      return 0;
+    });
+  }
 }
 
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Renan'));
+console.log(techList([], 'Renan'));
 
 // Desafio 11
 function generatePhoneNumber() {
