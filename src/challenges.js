b4828fc17708740672b580083f62023949d6d575
Victor Gomes <victorgomesinfor2@gmail.com>
2021-04-28 15:43:03 -0300

    Resolve requisito 02 do projeto playground

--- src/challenges.js
+++ src/challenges.js
@@ -9,10 +9,11 @@ function compareTrue(parametOne, parametTwo) {
   }
 }
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area = (base * height) / 2;
+  return area;
 }
-
 // Desafio 3
 function splitSentence() {
   // seu código aqui
