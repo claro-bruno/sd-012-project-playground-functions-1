0849eceb15f3db7f46015ffde1f0bb792fdf9ab7
Josimar Souza <contact.josimarsouza@gmail.com>
2021-04-28 17:32:05 -0300

    Adicionado algumas correçoes no requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -76,12 +76,12 @@ function highestCount(numberArray) {
 }
 
 // Desafio 7
-function catAndMouse(mouse, cat2, cat1) {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   if ((cat1 - mouse) < (cat2 - mouse)) {
     return 'cat1';
   }
-  if ((cat1 - mouse) > (cat2 - mouse)) {
+  if ((cat2 - mouse) < (cat1 - mouse)) {
     return 'cat2';
   }
   if ((cat1 - mouse) === (cat2 - mouse)) {
