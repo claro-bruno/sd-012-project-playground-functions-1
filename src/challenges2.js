fe05d4ae588f9d14242f9e8d6cd07741bd6c4c4f
Rodrigo <rodrigo.facury14@gmail.com>
2021-04-29 13:30:21 -0300

    Desafio 10 resolvido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
 // Desafio 10
-function techList() {
+function techList(arrayTech, name) {
   // seu código aqui
+  
+  let listAnswer = [];
+
+  arrayTech = arrayTech.sort();
+
+  for (let key in arrayTech){
+    let objects = {
+      tech: arrayTech[key],
+      name: name
+    };
+    listAnswer.push(objects);
+  };
+  if (arrayTech.length === 0) {
+    return 'Vazio!'
+  } else {  
+    return listAnswer;
+  }
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
