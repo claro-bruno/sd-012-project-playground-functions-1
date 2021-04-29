97c0d1a94133fbab3190288e73ca70f4c6f3cf2d
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-28 21:27:11 -0300

    (29) Corrige eslint requisito 10 e 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,43 +4,41 @@ function techList(tech, name) {
   let array = [];
   let alfabetica = tech.sort();
   if (tech.length === 0) {
-    return 'Vazio!'
+    return 'Vazio!';
   } else {
-    for (let language in alfabetica){
-      array.push ({
+    for (let language in alfabetica) {
+      array.push({
         tech: alfabetica[language],
-        name: name
-      });
-    };
+        name: name,
+      })
+    }
     return array;
-  }; 
-};
+  }
+}
 
 // Desafio 11
 function generatePhoneNumber(PhoneNumber) {
   // seu código aqui
-  if (PhoneNumber.length != 11){
-    return "Array com tamanho incorreto."
+  if (PhoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
   } else {
     for (let number in PhoneNumber) {
       if (PhoneNumber[number] < 0 || PhoneNumber[number] > 9) {
-        return "não é possível gerar um número de telefone com esses valores"
-      };
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
       let contador = 0;
-      for (let number2 in PhoneNumber){
-        if (PhoneNumber[number] === PhoneNumber[number2]){
-          contador += 1
+      for (let number2 in PhoneNumber) {
+        if (PhoneNumber[number] === PhoneNumber[number2]) {
+          contador += 1;
         }
-        if (contador > 2){
-          return "não é possível gerar um número de telefone com esses valores"
-        };
-      };
-    };
-    return '(' + PhoneNumber[0] + PhoneNumber[1] + ') ' + PhoneNumber[2] + PhoneNumber[3] + PhoneNumber[4] + PhoneNumber[5] + PhoneNumber[6] + '-' + PhoneNumber[7] + PhoneNumber[8] + PhoneNumber[9] + PhoneNumber[10]
-  };
-};
-
-
+        if (contador > 2) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+    return '(' + PhoneNumber[0] + PhoneNumber[1] + ') ' + PhoneNumber[2] + PhoneNumber[3] + PhoneNumber[4] + PhoneNumber[5] + PhoneNumber[6] + '-' + PhoneNumber[7] + PhoneNumber[8] + PhoneNumber[9] + PhoneNumber[10];
+  }
+}
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
