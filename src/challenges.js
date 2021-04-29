229b4cdbcfff06d91f1b3f3f78f368ac819effd6
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-29 19:04:45 -0300

    Adiciona requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -47,9 +47,18 @@ function footballPoints(wins,ties) {
 console.log((wins * 3) + (ties *1))
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+let contar = [9, 1, 2, 3, 9, 5, 7];
+let quantTimes = 0;
+function highestCount(contar){
+  for(index = 0; index <= contar.length; index +=1){
+    if (contar[index] === Math.max(...contar)){
+     quantTimes +=1
+    }  
+  } 
+  return quantTimes;
 }
+console.log(highestCount(contar))
 
 // Desafio 7
 function catAndMouse() {
