a92d3cea01b09fd1d9f46ee2ec38715b3712cdfe
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-04-29 15:50:09 -0300

    Tentativa 1 - requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,5 @@
+const { compareTrue } = require("./challenges");
+
 // Desafio 10
 function techList(lista, name) {
   let listaSaida = [];
@@ -20,8 +22,37 @@ function techList(lista, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function repNumero (arrNum, arrNum2) {
+  let count = 0;
+  for (let index = 0; index < arrNum.length; index += 1) {
+    if (arrNum[index] === arrNum2) {
+      count += 1;
+    }
+  }
+  if (count >= 3) {
+    return true;
+  }
+  return false;
+}
+
+function gerNum (arrNum, n) {
+  let vezes = '';
+  for (let index = 0; index < n; index += 1) {
+    vezes += arrNum[Math.floor(Math.random() * 9)];
+  }
+  return vezes;
+}
+
+function generatePhoneNumber(arrNum) {
+  if (arrNum.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arrNum.length; index += 1) {
+    if (arrNum[index] < 0 || arrNum[index] > 9 || repNumero(arrNum, arrNum[index]) === true) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${gerNum(arrNum, 2)}) ${gerNum(arrNum, 5)}-${gerNum(arrNum, 4)}`;
 }
 
 // Desafio 12
