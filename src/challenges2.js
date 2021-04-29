c4212e747cf9b77a296395ef7c9a8693472c8458
rachidb5 <jordan.rachid@gmail.com>
2021-04-29 16:39:40 -0300

    eslint

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,18 +14,18 @@ function techList(tecnologia, nome) {
   }
   if (tecnologia.length > 0) {
     return objLista;
-  } 
+  }
   return 'Vazio!';
 }
 
 // Desafio 11
 function generatePhoneNumber(n) {
   // seu código aqui
-  if (n.length === 11){
+  if (n.length === 11) {
     for (let i = 0; i < 11; i += 1) {
       let count = 0;
       for (let j = 0; j < 11; j += 1) {
-        if(n[i] === n[j]) {
+        if (n[i] === n[j]) {
           count += 1;
           if (n[j] < 0 || n[j] > 9 || count > 2) {
             return 'não é possível gerar um número de telefone com esses valores';
@@ -34,15 +34,15 @@ function generatePhoneNumber(n) {
       }
     } return `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`;
   }
-    return 'Array com tamanho incorreto.';
+  return 'Array com tamanho incorreto.';
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  return lineA < lineB + lineC && lineC < lineA + lineB && lineB < lineA + lineC &&
-  lineA > Math.abs(lineC - lineB) && lineB > Math.abs(lineC - lineA) &&
-  lineC > Math.abs(lineB - lineA);
+  return lineA < lineB + lineC && lineC < lineA + lineB && lineB < lineA + lineC
+  && lineA > Math.abs(lineC - lineB) && lineB > Math.abs(lineC - lineA)
+  && lineC > Math.abs(lineB - lineA);
 }
 
 // Desafio 13
@@ -53,14 +53,14 @@ function hydrate(pedido) {
   let aguas = 0;
 
   for (let i = 0; i < n.length; i += 1) {
-    numbers.push(parseInt(n[i]));
+    numbers.push(parseInt(n[i], 10));
   }
   for (let i = 0; i < numbers.length; i += 1) {
     aguas += numbers[i];
   }
   if (aguas > 1) {
-    return `${aguas} +  copos de água`;
-  } return `${aguas} + ' copo de água`;
+    return `${aguas} copos de água`;
+  } return `${aguas} copo de água`;
 }
 
 module.exports = {
