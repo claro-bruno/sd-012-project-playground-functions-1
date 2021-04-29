c477270519b6502c0a69dd32161c9e1d07bacd71
Sarah <sarahgsb@hotmail.com>
2021-04-29 05:02:47 -0300

    6- highestcount #vqv 4

--- src/challenges.js
+++ src/challenges.js
@@ -39,11 +39,11 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(numbers) {
 
-let numbers = Math.max(...numbers);
+let max = Math.max(...numbers);
 let count = 0
 
 for (let index = 0; index < numbers.length; index+=1) {
-   if (index == numbers[i]) {
+   if (max == numbers[i]) {
      count += 1
    }
 } return count
