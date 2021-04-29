ab26b5d5e86076d979f0a0786c7be181b08b987d
David Azevedo <zevid.dev@outlook.com>
2021-04-29 07:04:33 -0300

    Done requirement 9

--- src/challenges.js
+++ src/challenges.js
@@ -99,11 +99,24 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let newPhrase = phrase
+    .replace(/a/g, '1')
+    .replace(/e/g, '2')
+    .replace(/i/g, '3')
+    .replace(/o/g, '4')
+    .replace(/u/g, '5');
+  return newPhrase;
 }
-function decode() {
-  // seu código aqui
+
+function decode(code) {
+  let newCode = code
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+  return newCode;
 }
 
 module.exports = {
