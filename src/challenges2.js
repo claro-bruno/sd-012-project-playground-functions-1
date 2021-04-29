3c08af483813d25d363b88f2351de5ce32a3ae07
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-29 13:10:20 -0300

    completa Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,39 @@ function techList(tech, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  let ddd = '';
+  let phoneNumberPart1 = '';
+  let phoneNumberPart2 = '';
+  for (let indRep = 0; indRep < arr.length; indRep += 1) {
+    let repeticoes = 0;
+    for (let indRep2 = 0; indRep2 < arr.length; indRep2 += 1) {
+      if (arr[indRep] === arr[indRep2]) {
+        repeticoes += 1;
+      }
+    }
+    if (repeticoes >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (arr.length !== 11) {
+    return 'Array com Tamnho incorrento.';
+  }
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] < 0 || arr[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    if (index === 0) {
+      ddd += `(${arr[index]}`;
+    } else if (index === 1) {
+      ddd += `${arr[index]})`;
+    } else if (index > 1 && index < 7) {
+      phoneNumberPart1 += arr[index];
+    } else {
+      phoneNumberPart2 += arr[index];
+    }
+  }
+  return `${ddd} ${phoneNumberPart1}-${phoneNumberPart2}`;
 }
 
 // Desafio 12
