41e4f5f553baa982f4e515149504fc5589b6f857
rachidb5 <jordan.rachid@gmail.com>
2021-04-28 21:03:33 -0300

    desafio8

--- src/challenges.js
+++ src/challenges.js
@@ -61,9 +61,23 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(numbers) {
   // seu código aqui
+  let resposta = [];
+  for (let i = 0; i<numbers.length; i+=1){
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 != 0) {
+      resposta.push('fizz')
+    } else if(numbers[i] % 5 === 0 && numbers[i] % 3 != 0) {
+      resposta.push('buzz')
+    } else if (numbers[i] % 5 != 0 && numbers[i] % 3 != 0){
+      resposta.push('bug!')
+    } else {
+      resposta.push('fizzBuzz') 
+    }
+  }
+  return resposta;
 }
+
 
 // Desafio 9
 function encode() {
