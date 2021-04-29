7df66e6ecbd4f288bc997a06dd98f6b60fc7784d
Gabriel Takaki <gabitj@icloud.com>
2021-04-29 17:10:36 -0300

    Corrige desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,7 @@
 // Desafio 10
-// function techList(tech, name) {
+function techList() {
   // seu código aqui
-
-//}
+}
 
 // Desafio 11
 function generatePhoneNumber(array) {
@@ -15,19 +14,19 @@ function generatePhoneNumber(array) {
       if(array[index] === array[index2]) {
         rep += 1;
         if(rep >= 3) {
-          return 'não é possível gerar um número de telefone com esses valores';
+          return "não é possível gerar um número de telefone com esses valores";
         }
       }
     }
     if (array.length !== 11) {
       phoneNum = 'Array com tamanho incorreto.' 
     } else if (array[index] < 0 || array[index] > 9) {
-      phoneNum = 'não é possível gerar um número de telefone com esses valores';
+      phoneNum = "não é possível gerar um número de telefone com esses valores";
     } 
   }
   return phoneNum;
 }
-console.log(generatePhoneNumber([4, 2, 2, 2, 7, 2, 6, 7, 8, 9, 0]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
@@ -35,11 +34,10 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate(stri) {
+function hydrate() {
   // seu código aqui
   
 }
-console.log(hydrate(2, 7));
 
 module.exports = {
   generatePhoneNumber,
