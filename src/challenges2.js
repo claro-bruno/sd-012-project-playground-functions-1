9ee0cfda0b9a4b0a7beba2bd47507fffa899a997
David Azevedo <zevid.dev@outlook.com>
2021-04-29 10:00:56 -0300

    Done requirement 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let error = 'Vazio!';
+  if (tech.length !== 0) {
+    tech = tech.sort();
+  } else {
+    return error;
+  }
+  let array = [];
+  for (let indice = 0; indice < tech.length; indice += 1) {
+    let obj = {};
+    obj.tech = tech[indice];
+    obj.name = name;
+    array.push(obj);
+  }
+  let orden = array;
+  return orden;
 }
 
 // Desafio 11
