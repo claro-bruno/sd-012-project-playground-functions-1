368b36028eeeb8984eb25bd0f979a411da617b0a
Stefano <stefanooliveira1@gmail.com>
2021-04-29 01:54:06 -0300

    tenta fazer desafio 10 e 11 mas falta alguns detalhes pra acertar

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,24 +5,46 @@ let nameTeste = "Lucas";
 function techList(tech, name) {
   var listaObjetos = {};
   for (let key in tech) {
-    if (tech[key] != null) {
-      listaObjetos = {
-        tech: tech[key],
-        name: name,
-      };
-      if (tech[key] === null) {
-        return 'Vazio!'
-      }
-    }
-    console.log(listaObjetos);
-  }
+    if (tech[key] === null) {
+      console.log('Vazio!')
+    } else {
+      listaObjetos.tech = tech[key];
+      listaObjetos.name = name;
+      console.log(listaObjetos);
+    };
+ }
 }
+
 console.log(techList(listaTeste, nameTeste));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+let parametroTeste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+
+function generatePhoneNumber(number) {
+  let numeroGerado = [];
+  if (number.length != 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let index = 0; index < 11; index += 1) {
+    if (number[index] < 0 || number[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+      break;
+    }
+    numeroGerado += number[index];
+  }
+  let numeroArray = numeroGerado.split('');
+  let countNumRep = 0;
+ //PARTE_AINDA_NAO_FUNCIONANDO for (let i = 0; i < numeroArray.length; i += 1){
+//   let num = numeroArray[i];
+ //   counts[]
+ // }
+  console.log(countNumRep);
+  if (countNumRep >= 3) {
+    return 'não é possível gerar um número de telefone com esses valores'
+  } else {return'(' + numeroArray[0] + numeroArray[1] + ') ' + numeroArray[2] + numeroArray[3] + numeroArray[4] + numeroArray [5] + numeroArray [6] + '-' + numeroArray[7] + numeroArray[8] + numeroArray[9] + numeroArray[10]}
 }
+console.log(generatePhoneNumber(parametroTeste))
 
 // Desafio 12
 function triangleCheck() {
