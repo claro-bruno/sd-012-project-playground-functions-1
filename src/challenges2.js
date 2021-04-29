9663ae5fd7472c5cf21e2ded4daf3ecc0476ac36
Patrick Dack <patrickdack89@gmail.com>
2021-04-29 12:57:08 -0300

    Resolve desafio 13.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,18 +1,33 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let object = {};
+  let newList = [];
+  for (let key in array) {
+    object['techName'] = array[key];
+    object['name'] = name;
+    newList.push(object);
+    
+  }
+  return newList;
 }
-
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let triangleLine = [lineA, lineB, lineC];
+  triangleLine.sort((a,b) => a - b);
+  if((triangleLine[2] < triangleLine[0] + triangleLine[1]) && (triangleLine[2] > Math.abs(triangleLine[0] - triangleLine[1]))) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
-
+console.log(triangleCheck(10,14,8));
 // Desafio 13
 function hydrate() {
   // seu código aqui
