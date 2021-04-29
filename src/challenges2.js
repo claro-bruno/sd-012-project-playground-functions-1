79c477555220c20bdca7dad3dd9b8d9eac9d82c8
alestori <alestori7@gmail.com>
2021-04-28 21:04:48 -0300

    Refatora Desafio 11: remove else return

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,7 +19,6 @@ function techList(techNames, nome) {
 }
 
 // Desafio 11
-// eslint-disable-next-line max-lines-per-function
 function generatePhoneNumber(numeros) {
   if (numeros.length !== 11) {
     return 'Array com tamanho incorreto.';
@@ -35,9 +34,10 @@ function generatePhoneNumber(numeros) {
       }
     }
   }
-    return `(${numeros[0]}${numeros[1]}) ${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
+  return `(${numeros[0]}${numeros[1]}) ${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
 }
 
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12
 function triangleCheck(a, b, c) {
   return !!(a + b > c && a + c > b && b + c > a && Math.abs(a - b) < c && Math.abs(a - c) < b && Math.abs(b - c) < a);
