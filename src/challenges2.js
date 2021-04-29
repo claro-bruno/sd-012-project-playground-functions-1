f2f380529e260393ba5ffef2cac4be586a36ec91
Jossany Moura <mjossany@gmail.com>
2021-04-29 12:09:35 -0300

    Resolve Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(arr, name) {
+  arr.sort();
+  let finalList = [];
+
+  for (let index = 0; index < arr.length; index += 1) {
+    let objeto = {
+      tech: arr[index],
+      name: name
+    }
+    finalList.push(objeto);
+  }
+  if (arr.length == 0) {
+    return 'Vazio!'
+  } else {
+    return finalList;
+  };
+};
+console.log(techList([], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
