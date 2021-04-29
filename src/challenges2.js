abae30c0376c42adf3cad743de98dee284eab3c3
Gabriel Takaki <gabitj@icloud.com>
2021-04-29 17:19:22 -0300

    Última tentativa de acerto, desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,11 +22,13 @@ function generatePhoneNumber(array) {
       phoneNum = 'Array com tamanho incorreto.' 
     } else if (array[index] < 0 || array[index] > 9) {
       phoneNum = "não é possível gerar um número de telefone com esses valores";
-    } 
+    } else {
+      return `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
+    }
   }
   return phoneNum;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
