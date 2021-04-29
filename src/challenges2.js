4a42471c285c68c5e656d7a1298b3cf9e810438d
Renan Ferreira <renansantiago2013@hotmail.com>
2021-04-29 11:13:55 -0300

    Resolve o requisito 11 - phone number

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,9 +19,35 @@ function techList(tech, name) {
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Renan'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let prefix = `(${numbers[0]}${numbers[1]})`;
+  let phoneNumber = `${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`
+  let newPhoneNumber = prefix + phoneNumber
+  let contRepeated = 0;
+  let contNumber = 0;
+  for (let num in numbers) {
+    let verifyNumber = numbers[num];
+    for (let num2 in numbers) {
+      if (verifyNumber === numbers[num2]) {
+        contNumber += 1;
+      }
+    }
+    if (contNumber > contRepeated) {
+      contRepeated = contNumber;
+    }
+    contNumber = 0;
+    if (numbers[num] < 0 || numbers[num] > 9 || contRepeated >= 3) {
+      return 'Não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto'
+  } else {
+    return newPhoneNumber
+  }
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
