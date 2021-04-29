3927d2a86f3a6e8f507857b99d48c84cb88e9623
rachidb5 <jordan.rachid@gmail.com>
2021-04-29 16:23:46 -0300

    o desafio final

--- src/challenges2.js
+++ src/challenges2.js
@@ -35,7 +35,7 @@ function generatePhoneNumber(n) {
     }
     } return `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`
   }
-    return 'Array com tamanho incorreto. '
+    return 'Array com tamanho incorreto.'
 }
 
 // Desafio 12
