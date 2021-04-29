ddc19c3739e7300b5be72fdabf59575d90aa5cdd
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-29 20:27:21 -0300

    Desafio 11 try 3

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,10 +13,11 @@ function techList(arrayTec, name) {
   return newList;
 }
 // Desafio 11.1
-function validatePhone(phoneNum){
+function validatePhone(phoneNum) {
   if (phoneNum.length !== 11) return 'Array com tamanho incorreto.';
-  for (let i in phoneNum){
-    if (phoneNum[i] < 0 || phoneNum[i] > 9) return 'não é possível gerar um número de telefone com esses valores';
+  for (let i = 0; i < phoneNum.length; i += 1) {
+    if (phoneNum[i] < 0 || phoneNum[i] > 9) 
+    return 'não é possível gerar um número de telefone com esses valores';
     let contRep = 0;
     let numCurrent = phoneNum[i];
     for (let i2 in phoneNum){
@@ -24,16 +25,16 @@ function validatePhone(phoneNum){
     }
     if (contRep > 2) return 'não é possível gerar um número de telefone com esses valores';
   }
-  return phoneNum;
+  return true;
 }
 // Desafio 11
 function generatePhoneNumber(phoneNum) {
-  if(validatePhone(phoneNum)) {
-    let string = validatePhone(phoneNum);
+  if(validatePhone(phoneNum) === true) {
+    let string = phoneNum;
     let newString = ['('];
     for (let i = 0; i < string.length; i += 1) {
       if (i === 2) newString.push(') ');
-      if (i === 6) newString.push('-');
+      if (i === 7) newString.push('-');
       newString.push(string[i]);
     }
     newString = newString.toString();
@@ -42,6 +43,7 @@ function generatePhoneNumber(phoneNum) {
   }
   return validatePhone(phoneNum);
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
