90cb1baad643d4941b4fd4363bad8a8c913c79aa
Kevin Oliveira <kevin.zero@hotmail.com>
2021-04-28 21:35:31 -0300

    (31) Corrige eslint requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -57,47 +57,46 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(string) {
   // seu código aqui
-  let soma = 0
-  for (let index in string){
-    switch (string[index]){
-      case '1':
-        soma += 1;
-        break;
-      case '2':
-        soma += 2;
-        break;
-      case '3':
-        soma += 3;
-        break;
-      case '4':
-        soma += 4;
-        break;
-      case '5':
-        soma += 5;
-        break;
-      case '6':
-        soma += 6;
-        break;
-      case '7':
-        soma += 7;
-        break;
-      case '8':
-        soma += 8;
-        break;
-      case '9':
-        soma += 9;
-        break;
-      default:
-        soma = soma; 
+  let soma = 0;
+  for (let index in string) {
+    switch (string[index]) {
+    case '1':
+      soma += 1;
+      break;
+    case '2':
+      soma += 2;
+      break;
+    case '3':
+      soma += 3;
+      break;
+    case '4':
+      soma += 4;
+      break;
+    case '5':
+      soma += 5;
+      break;
+    case '6':
+      soma += 6;
+      break;
+    case '7':
+      soma += 7;
+      break;
+    case '8':
+      soma += 8;
+      break;
+    case '9':
+      soma += 9;
+      break;
+    default:
+      soma += 0;
     }
   }
   if (soma === 1){
-    return soma + ' copo de água'
+    return soma + ' copo de água';
   } else {
-    return soma + ' copos de água'
-  };
-};
-
+    return soma + ' copos de água';
+  }
+}
 
 // let s = "1 banana + 1 pineapple + 3 oranges";
 // let result = (s.match(/\d+/g));
@@ -121,5 +120,3 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
-
-
