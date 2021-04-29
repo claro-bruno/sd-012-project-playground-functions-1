6a1088c089a379b198c0db76f8bd4819cbaa1cd3
alestori <alestori7@gmail.com>
2021-04-28 21:01:45 -0300

    Refatora Desafio 10: remove else return

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,5 @@
 /* eslint-disable max-len */
 /* eslint-disable complexity */
-/* eslint-disable no-else-return */
 // Desafio 10
 function techList(techNames, nome) {
   let result = [];
@@ -15,9 +14,8 @@ function techList(techNames, nome) {
       result.push(objeto);
     }
     return result;
-  } else {
-    return 'Vazio!';
   }
+  return 'Vazio!';
 }
 
 // Desafio 11
@@ -25,20 +23,19 @@ function techList(techNames, nome) {
 function generatePhoneNumber(numeros) {
   if (numeros.length !== 11) {
     return 'Array com tamanho incorreto.';
-  } else {
-    for (let i = 0; i < numeros.length; i += 1) {
-      let count = 0;
-      for (let j = 0; j < numeros.length; j += 1) {
-        if (numeros[i] === numeros[j]) {
-          count += 1;
-          if (count > 2 || numeros[i] > 9 || numeros[i] < 0) {
-            return 'não é possível gerar um número de telefone com esses valores';
-          }
+  }
+  for (let i = 0; i < numeros.length; i += 1) {
+    let count = 0;
+    for (let j = 0; j < numeros.length; j += 1) {
+      if (numeros[i] === numeros[j]) {
+        count += 1;
+        if (count > 2 || numeros[i] > 9 || numeros[i] < 0) {
+          return 'não é possível gerar um número de telefone com esses valores';
         }
       }
     }
-    return `(${numeros[0]}${numeros[1]}) ${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
   }
+    return `(${numeros[0]}${numeros[1]}) ${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
 }
 
 // Desafio 12
