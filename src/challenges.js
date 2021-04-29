49bf499bec270fef85019bd4bf1a9b021f79ab40
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-04-28 22:44:38 -0300

    resolve desafio 8 e faz referencia

--- src/challenges.js
+++ src/challenges.js
@@ -93,9 +93,26 @@ function catAndMouse(mouse, cat1, cat2) {
 // console.log(catAndMouse(1, 3, 3));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayNumber = [];
+  for(let index in array) {
+    if (array[index] % 3 === 0){
+      arrayNumber.push("fizz");
+    } else if (array[index] % 5 === 0){
+      arrayNumber.push("buzz");
+    } else if (array[index] % 3 === 0 && array[index] % 5 === 0){
+      arrayNumber.push("fizzBuzz");
+    } else {
+      arrayNumber.push("bug!");
+    }
+  }
+  return arrayNumber;
 }
+
+// Referencia - as linhas 97,100, 102, 104, 106 foram modificadas após sucessivas tentativas de alteração do código, a versão mais proxima, nao trazia uma variavel para armazenar o resultado das interações, e ao invés de realizar o push, utilizava o return com a mensagem correspondente, o que fazia o código retornar apenas o resultado do primeiro número, após consultar, novamente a resolução do colega Eric Kreis (https://github.com/tryber/sd-012-project-playground-functions/commit/d820b69cf5cd11a6a2b68f156616e9fb3f3a183b), fez todo o sentido e compreendi a necessidade de uma váriavel de um array vazio que seria preenchida conforme o resultado. O restante (percorrer todo o objeto com o for in, e fazer as interações de verificação de divisão pelo número determinado, consegui sozinho. Graças a God!)
+// console.log(fizzBuzz([2, 15, 7, 9, 45]));
+// console.log(fizzBuzz([7, 9]));
+// console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
 function encode() {
