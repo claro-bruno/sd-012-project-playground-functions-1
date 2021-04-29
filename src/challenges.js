d48a8e6a79e4a842a0fec6e360fb1feb7e84421b
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-04-29 18:55:06 -0300

    corrige exercicios 6 e 7 para testar codigo

--- src/challenges.js
+++ src/challenges.js
@@ -48,41 +48,58 @@ function footballPoints(wins, ties) {
 // console.log(footballPoints(0,0))
 
 // Desafio 6
-
-let duplicatedNumbers = [];
-function highestCount(array) {
-  for (index = 0; index < array.length ; index += 1){
-    let maxNumbers = Math.max(...array)
-        if (maxNumbers === array[index]){
-      duplicatedNumbers.push(array[index])
-      
+function highestCount(array){
+  let maxNumbers = Math.max(...array);
+  let counter = 0;
+    for (index = 0; index < array.length ; index += 1){
+            if (maxNumbers === array[index]){
+             counter += 1      
     } 
   }
-  let finalNumbers = duplicatedNumbers.length
-  return finalNumbers
+   return counter
 }
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
+// console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
 
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let result1 = (cat1 - mouse) 
-  let result2 = (cat2 - mouse) 
+  let result1 = Math.abs(cat1 - mouse) 
+  let result2 = Math.abs(cat2 - mouse) 
   if (result1 < result2){
-    return 'cat 1';}
-
-  else if (result2 < result1) {
-    return 'cat2';}
-
-    else return "os gatos trombam e o rato foge"
+    return 'cat 1';
+  } else if (result2 < result1) {
+      return 'cat2';
+  } else {
+    return "os gatos trombam e o rato foge";
   }
-  
-// console.log(catAndMouse(1,2,2))
+}
+ 
+// console.log(catAndMouse(2,3,4))
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+let arrayFinal = [];
+let divisibleBy3 = 'fizz';
+let divisibleBy5 = 'buzz';
+let divisibleByBoth = 'fizzBuzz';
+let notDivisible = 'bug !';
+
+function fizzBuzz(array) {
+  for (index = 0; index < array.length; index += 1) {
+    if ( (array[index] % 3) === 0 ){
+    return arrayFinal[index].push(divisibleBy3)
+    }
+    
+    else if (( array[index] % 5) === 0 ) {
+    return arrayFinal[index].push(divisibleBy5)
+    }  
+
+    else if ((array[index] % 3 && array[index] % 5) === 0 ) {
+    return arrayFinal[index].push(divisibleByBoth)
+    }
+    else return arrayFinal[index].push(notDivisible)
+  }  
+
+console.log(fizzBuzz([7, 9]))
 
 // Desafio 9
 function encode() {
@@ -103,4 +120,4 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
-};
+}
