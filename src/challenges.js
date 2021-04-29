6870538dda4e2aab534baaf9ba8ce89572b8d347
Gustavo-Mourao <gcmbh1@gmail.com>
2021-04-29 02:10:23 -0300

    Questao8

--- src/challenges.js
+++ src/challenges.js
@@ -7,59 +7,59 @@ function compareTrue(valor1, valor2) {
     return false;
   }
 }
-console.log(compareTrue(true, false))
+console.log(compareTrue(true, false));
 
 // Desafio 2
 function calcArea(base, altura) {
   // seu código aqui
-  let area = (base * altura)/2
+  let area = (base * altura) / 2;
   return area;
 }
-console.log(calcArea(2, 5))
+console.log(calcArea(2, 5));
 
 // Desafio 3
 function splitSentence(frase) {
   // seu código aqui
-let dividindoFrases = frase.split(" ");
-return dividindoFrases;
+  let dividindoFrases = frase.split(' ');
+  return dividindoFrases;
 }
-console.log(splitSentence('vamo que vamo'))
+console.log(splitSentence('vamo que vamo'));
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-let primeiro = array[0]
-let ultimo = array[array.length - 1]
-let concatenacao = ultimo + ',' + ' ' + primeiro 
-return concatenacao;
+  let primeiro = array[0];
+  let ultimo = array[array.length - 1];
+  let concatenacao = ultimo + ',' + ' ' + primeiro;
+  return concatenacao;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let total = (3 * wins) + ties
+  let total = 3 * wins + ties;
   return total;
 }
-console.log(footballPoints(0, 0))
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(array) {
   // seu código aqui
-  let contador = [0]
-  let soma = 0
-  for (let index = 0; index < array.length; index += 1){
-    if (array[index] > contador){
-    contador = array[index]
+  let contador = [0];
+  let soma = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > contador) {
+      contador = array[index];
     }
   }
-  for (let incremento = 0; incremento < array.length; incremento += 1){
-    if (array[incremento] == contador){
-    soma += 1
+  for (let incremento = 0; incremento < array.length; incremento += 1) {
+    if (array[incremento] == contador) {
+      soma += 1;
+    }
   }
-}
   return soma;
 }
-console.log(highestCount([0, 7, 7, 3, 9, 5, 4]))
+console.log(highestCount([0, 7, 7, 3, 9, 5, 4]));
 
 // Desafio 7
 function catAndMouse() {
@@ -67,9 +67,24 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(oi) {
   // seu código aqui
+  let memory = [];
+  for (let index = 0; index < oi.length; index += 1) {
+    if (oi[index] % 3 == 0 && oi[index] % 5 !== 0) {
+      memory.push('fizz');
+    } else if (oi[index] % 5 == 0 && oi[index] % 3 !== 0) {
+      memory.push('buzz');
+    } else if (oi[index] % 3 == 0 && oi[index] % 5 == 0) {
+      memory.push('fizzBuzz');
+    } else {
+      memory.push('bug!');
+    }
+  }
+  return memory;
 }
+
+console.log(fizzBuzz([3, 5, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
