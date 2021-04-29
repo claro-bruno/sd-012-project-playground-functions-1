ba0a558f5f2f4d82e34089daea49ef1f2d777549
tiagoornelas <advtiagoornelas@gmail.com>
2021-04-28 21:27:08 -0300

    Desiste do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,45 +4,10 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
-  let negativeNumbers = 0;
-  let doubleDigits = 0;
-  let timesRepeated = 0;
-
-  for (let char in numbers) {
-    if (numbers[char] < 0) {
-      negativeNumbers += 1;
-    }
-    if (numbers[char] > 9) {
-      doubleDigits += 1;
-    }
-  }
-  
-  for (let index in numbers) {
-    let repeatedNumbers = -1;
-    for (let char in numbers) {
-      if (numbers[char] === numbers[index]) {
-        repeatedNumbers += 1;        
-      }
-      if (repeatedNumbers > 0) {
-        timesRepeated += 1;
-      }
-    }
-  }
-
-  if (numbers.length != 11) {
-    return "Array com tamanho incorreto.";
-  } else if ( negativeNumbers > 0 || doubleDigits > 0 || timesRepeated >= 3 ) {
-    return "não é possível gerar um número de telefone com esses valores";
-  } else {
-    return '(' + numbers[0] + numbers[1] + ') '
-    + numbers[2] + numbers[3] + numbers[4]
-    + numbers[5] + numbers[6] + '-'
-    + numbers[7] + numbers[8]
-    + numbers[9] + numbers[10];
-  }
+function generatePhoneNumber() {
+  // seu código aqui
 }
-console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
