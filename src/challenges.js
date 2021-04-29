52547b8cf1ad17e6d4470ab9b294f0a2bd134492
Marcela Silva <marcela.ops@hotmail.br>
2021-04-29 14:09:32 -0300

    desafio 7 - concertando erros

--- src/challenges.js
+++ src/challenges.js
@@ -61,15 +61,15 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = cat1 - mouse;
   let distanceCat2 = cat2 - mouse;
 
-  if(distanceCat1 > distanceCat2){
-    return ("cat2");
+  if(distanceCat1 > distanceCat2) {
+    return ('cat2');
   } else if (distanceCat2 > distanceCat1) {
-    return ("cat1");
+    return ('cat1');
   } else {
-    return ('os gatos trombam e o rato foge')
+    return ('os gatos trombam e o rato foge');
   }
 }
-// console.log (catAndMouse(0, 12, 12));
+console.log (catAndMouse(0, 12, 12));
 
 // // Desafio 8
 // function fizzBuzz() {
