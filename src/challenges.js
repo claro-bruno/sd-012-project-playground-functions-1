4cbd3c83e08aa398c02f9beb9e02fcc2177ffe41
Gabriel Takaki <gabitj@icloud.com>
2021-04-28 22:50:33 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,26 @@
 // Desafio 1
 function compareTrue(boolean1, boolean2) {
   // seu código aqui
-  if(boolean1 === true && boolean2 === true) {
+  if (boolean1 === true && boolean2 === true) {
     return true;
   } else {
     return false;
-  };
-};
+  }
+}
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   let area = (base * height / 2);
   return area;
-};
+}
 
 // Desafio 3
 function splitSentence(str) {
   // seu código aqui
   str = str.split(' ');
   return str;
-};
+}
 let sentence = 'go Tybe';
 console.log(splitSentence(sentence));
 
@@ -31,7 +31,7 @@ function concatName(arr) {
   let first = arr[0];
   let together = last + ',' + ' ' + first;
   return together;
-};
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -71,9 +71,9 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat2';
   } else if(distCat2 === distCat1) {
     return 'os gatos trombam e o rato foge';
-  };
+  }
   return;
-};
+}
 
 
 // Desafio 8
@@ -92,18 +92,64 @@ function fizzBuzz(arr) {
     } else {
       str.push("bug!");
     };
-  };  
+  }
   return str;
-};
+}
 
 // Desafio 9
-function encode() {
+function encode(str) {
   // seu código aqui
+  let res = [];
+  let a = 1;
+  let e = 2;
+  let i = 3;
+  let o = 4;
+  let u = 5;
+  for (let index = 0; index < str.length; index+=1) {
+    if ('a' === str[index]) {
+      res += a;
+    } else if ('e' === str[index]) {
+      res += e;
+    } else if ('i' === str[index]) {
+      res += i;
+    } else if ('o' === str[index]) {
+      res += o;
+    }else if ('u' === str[index]) {
+      res += u;
+    } else {
+      res +=(str[index]);
+    }
+  }
+  return res;
 }
+console.log(encode('hi there!'))
 
-function decode() {
+function decode(str) {
   // seu código aqui
+  let res = [];
+  let a = 'a';
+  let e = 'e';
+  let i = 'i';
+  let o = 'o';
+  let u = 'u';
+  for (let index = 0; index < str.length; index+=1) {
+    if ('1' === str[index]) {
+      res += 'a';
+    } else if ('2' === str[index]) {
+      res += 'e';
+    } else if ('3' === str[index]) {
+      res += 'i';
+    } else if ('4' === str[index]) {
+      res += 'o';
+    }else if ('5' === str[index]) {
+      res += 'u';
+    }else {
+      res += str[index];
+    }
+  }
+  return res;
 }
+console.log(decode('h3 th2r2!'));
 
 module.exports = {
   calcArea,
