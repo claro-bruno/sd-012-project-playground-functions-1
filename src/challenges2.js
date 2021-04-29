dbce6c16ce066beaa917cd0d81ca9ca2b0917344
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-28 23:26:13 -0300

    completa Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let sortArray = tech.sort();
+  let arrayTech = [];
+  let techs = {}
+  if (tech == '') {
+    return 'Vazio!';
+  } else {
+    for (index = 0; index < sortArray.length; index += 1) {
+      techs = {
+        tech: sortArray[index],
+        name: name,
+      }
+      arrayTech.push(techs);
+    }
+    return arrayTech;
+  }
 }
 
 // Desafio 11
