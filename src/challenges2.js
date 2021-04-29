f1686a0f11749009d0a50fce5748edd96317b572
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-04-29 16:16:11 -0300

    primeira parte do desafio 10 (criar array com um objeto para cada iteracao)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,56 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  // let techs = new Object();
+  let outputArray = [];
+
+  for (let index = 0; index < techs.length; index += 1){
+    outputArray.push(new Object());
+    outputArray[index].tech = techs[index];
+    outputArray[index].name = name;
+  }
+  return outputArray;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+
+
+
+
+
+// let techs = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// let name = "Lucas";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 11
 function generatePhoneNumber() {
