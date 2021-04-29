71d0e52b25b081e9681de7a35d3250b42a0c2172
Milton Castro <castro.milton07@gmail.com>
2021-04-29 03:49:36 -0300

    06 - Adiciona ao arquivo challenges2.js alteraçoes que atende o REQUISITO 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,59 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(receivedTechnologies, name) {
+  let listOfTechnologies = [];
+  if (receivedTechnologies.length !== 0) {
+    let technologie1 = {
+      tech: 'NomeTech',
+      name,
+    };
+    let technologie2 = {
+      tech: 'NomeTech',
+      name,
+    };
+    let technologie3 = {
+      tech: 'NomeTech',
+      name,
+    };
+    let technologie4 = {
+      tech: 'NomeTech',
+      name,
+    };
+    let technologie5 = {
+      tech: 'NomeTech',
+      name,
+    };
+    receivedTechnologies = receivedTechnologies.sort();
+    for (let index in receivedTechnologies) {
+      switch (index) {
+      case '0':
+        technologie1.tech = receivedTechnologies[index];
+        listOfTechnologies.push(technologie1);
+        break;
+      case '1':
+        technologie2.tech = receivedTechnologies[index];
+        listOfTechnologies.push(technologie2);
+        break;
+      case '2':
+        technologie3.tech = receivedTechnologies[index];
+        listOfTechnologies.push(technologie3);
+        break;
+      case '3':
+        technologie4.tech = receivedTechnologies[index];
+        listOfTechnologies.push(technologie4);
+        break;
+      case '4':
+        technologie5.tech = receivedTechnologies[index];
+        listOfTechnologies.push(technologie5);
+        break;
+      default:
+        break;
+      }
+    }
+    return listOfTechnologies;
+  } return 'Vazio!';
 }
+
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
