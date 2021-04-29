63586bc862888a13ea417230da615d00158c35d9
Thiago Prado <thiagopc_dev@hotmail.com>
2021-04-29 20:14:08 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,11 +12,39 @@ function techList(arrayTec, name) {
   }
   return newList;
 }
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// Desafio 11.1
+function validatePhone(phoneNum){
+  if (phoneNum.length !== 11) return 'Array com tamanho incorreto.';
+  for (let i in phoneNum){
+    if (phoneNum[i] < 0 || phoneNum[i] > 9) return 'não é possível gerar um número de telefone com esses valores';
+    let contRep = 0;
+    let numCurrent = phoneNum[i];
+    for (let i2 in phoneNum){
+      if (numCurrent === phoneNum[i2]) contRep += 1;
+    }
+    if (contRep > 2) return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return phoneNum;
 }
-
+// Desafio 11
+function generatePhoneNumber(phoneNum) {
+  
+  if(validatePhone(phoneNum)){
+    let string = validatePhone(phoneNum);
+    let newString = ['('];
+    for (let i = 0; i < string.length; i += 1) {
+      if (i === 2) newString.push(')');
+      if (i === 6) newString.push('-');
+      newString.push(string[i]);
+    }
+    newString = newString.toString();
+    newString = newString.replace(/,/g, '');
+    return newString
+  }
+  console.log('Foi eu 2');
+  return validatePhone(phoneNum);
+  
+}
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
