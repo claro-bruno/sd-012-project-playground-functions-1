4a49f7e39cc9cadb9f6f61d0930477daaf823081
Eric Kreis <erickreis793@gmail.com>
2021-04-28 22:12:14 -0300

    requisitos bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,7 +18,7 @@ function techList(skills, name) {
 function generatePhoneNumber(phone) {
   let phoneResult = '';
   if (phone.length !== 11) {
-    return 'Array com tamanho incorreto';
+    return 'Array com tamanho incorreto.';
   }
   for (let number of phone) {
     let times = 0;
@@ -39,7 +39,7 @@ function generatePhoneNumber(phone) {
       phoneResult = `(${phone[0]}${phone[1]}) `;
     } else if (index2 <= 5) {
       phoneResult += `${phone[index2]}`;
-    } else if (index2 == 6) {
+    } else if (Number(index2) === 6) {
       phoneResult += `${phone[index2]}-`;
     } else if (index2 > 6 && index2 <= 11) {
       phoneResult += `${phone[index2]}`;
@@ -48,17 +48,31 @@ function generatePhoneNumber(phone) {
   return phoneResult;
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 10, 0, 1]));
-
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(line1, line2, line3) {
+  if (line1 < line2 + line3 && line1 > Math.abs(line2 - line3)) {
+    return true;
+  } else if (line2 < line1 + line3 && line2 > Math.abs(line1 - line3)) {
+    return true;
+  } else if (line3 < line1 + line2 && line3 > Math.abs(line1 - line2)) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let numbers = frase.replace(/\D/g, '');
+  let quantity = 0;
+  for (let str of numbers) {
+    quantity += Number(str);
+  }
+  if (quantity === 1) {
+    return '1 copo de água'
+  }
+  return `${quantity} copos de água`
 }
+console.log(hydrate('1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
