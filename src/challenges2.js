8af337e9267028dbbed3be51dc96deeb3a795e0e
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-04-28 22:48:00 -0300

    Tentando resolver o requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,7 +4,7 @@ function techList(tech, name) {
   let exit2 = 'Vazio!';
   tech.sort();
   if (tech.length !== 0) {
-    for(let key in tech){
+    for (let key in tech) {
       let newObject = {
         tech: tech[key],
         name: name,
@@ -17,11 +17,37 @@ function techList(tech, name) {
   }
   return exit;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let saida = '';
+  if (array.length - 1 !== 10){
+    saida = 'Array com tamanho incorreto';
+  }
+  else {
+    for (let veri = 0; veri < array.length; veri += 1) {
+      if (array[veri] < 0 || array[veri] > 9) {
+        saida = 'não é possível gerar um número de telefone com esses valores';
+        break;
+      }
+      for (let index = 0; index < array.length; index += 1) {
+        let numbersRepeats = 0;
+        for (let index2 = 0; index2 < array.length; index2 += 1) {
+          if (array[index2] === array [index]) {
+            numbersRepeats += 1
+          }
+        }
+        if (numbersRepeats >= 3) {
+          saida = 'não é possível gerar um npumero de telefone com esses valores';
+          break;
+        }
+        else {
+          saida = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];  
+        }
+      }
+    }
+  }
+  return saida;
 }
 
 // Desafio 12
