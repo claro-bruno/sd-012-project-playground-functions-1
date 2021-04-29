68fdf79ab90786053a3441d95057176b823c98b7
Gabriel Takaki <gabitj@icloud.com>
2021-04-29 17:26:01 -0300

    ÚLTIMA MESMO DO 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,7 +7,7 @@ function techList() {
 function generatePhoneNumber(array) {
   // seu código aqui
   // AJUDA EM: https://www.florin-pop.com/blog/2019/04/jcc-create-a-phone-number/ & https://www.w3resource.com/javascript/form/all-numbers.php
-  let phoneNum = `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
+  // let phoneNum = `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
   for (let index = 0; index < array.length; index +=1) {
     let rep = 0;
     for(let index2 = 0; index2 < array.length; index2 +=1) {
@@ -19,16 +19,13 @@ function generatePhoneNumber(array) {
       }
     }
     if (array.length !== 11) {
-      phoneNum = 'Array com tamanho incorreto.' 
+      return 'Array com tamanho incorreto.' 
     } else if (array[index] < 0 || array[index] > 9) {
-      phoneNum = "não é possível gerar um número de telefone com esses valores";
-    } else {
-      return `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
+      return "não é possível gerar um número de telefone com esses valores";
     }
   }
-  return phoneNum;
+  return `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
 }
-console.log(generatePhoneNumber([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
