c4f400301283db18aea34e69468730702315e471
Josimar Schneider <josimarschneider@gmail.com>
2021-04-29 11:28:46 -0400

    correção de 26 erros no projeto

--- src/challenges.js
+++ src/challenges.js
@@ -21,40 +21,40 @@ function concatName(nome) {
   }
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins *3) + (ties*1); 
+  return (wins * 3) + (ties * 1); 
 }
 // Desafio 6
 function highestCount(Array) {
-  let maior = 0, contador = 0;
-  for (let index = 0; index < Array.length; index +=1){
-    if (maior < Array[index]){
+  let maior = 0;
+  let contador = 0;
+  for (let index = 0; index < Array.length; index += 1) {
+    if (maior < Array[index]) {
       maior = Array[index];
     }
   }
-  for (indexCont in Array){
-    if (maior === Array[indexCont])
-      contador +=1;
+  for (let indexCont in Array){
+    if (maior === Array[indexCont]) {
+      contador += 1;
   }
   return contador;
 }
 
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  cat1 = cat1 - mouse;
-  cat2 = cat2 - mouse;
-  if(cat1 < cat2) {
-    return "cat2";
-  }else if (cat2 < cat1) {
-    return "cat1";
+  cat1 -= - mouse;
+  cat2 -= - mouse;
+  if (cat1 < cat2) {
+    return 'cat2';
+  } else if (cat2 < cat1) {
+    return 'cat1';
   }
-  return "os gatos trombam e o rato foge";
+  return 'os gatos trombam e o rato foge';
 }
 console.log(catAndMouse(3, 4, 5));
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  
 }
 
 // Desafio 9
