d8939b9c695b2c8619c98fba63235ac887c2b632
AndZoni <lorenzoniandre@outlook.com>
2021-04-29 22:01:07 +0100

    Adiciona Requisito 4 - Crie uma função que use concatenação de strings

--- src/challenges.js
+++ src/challenges.js
@@ -1,30 +1,34 @@
 // Desafio 1
-function compareTrue(valor1, valor2) {
-  let resultado1;
+function compareTrue(value1, value2) {
+  let result1;
 
-  if (valor1 === true && valor2 === true) {
-    resultado1 = true;
+  if (value1 === true && value2 === true) {
+    result1 = true;
   } else {
-    resultado1 = false;
+    result1 = false;
   }
-  return resultado1;
+  return result1;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let resultado2 = (base * height) / 2;
-  return resultado2;
+  let result2 = (base * height) / 2;
+  return result2;
 }
 
 // Desafio 3 (Fonte: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_split)
 function splitSentence(sentence) {
-  let resultado = sentence.split(' ');
-  return (resultado);
+  let result3 = sentence.split(' ');
+  return (result3);
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(firstLast) {
+  let firstArray = firstLast[0];
+  let secondArray = firstLast[firstLast.length - 1];
+  let result4 = [];
+  result4.push(firstArray, secondArray);
+  return (result4);
 }
 
 // Desafio 5
