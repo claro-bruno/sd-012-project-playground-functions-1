07f3e5966f1610ae8a87177a0ef4359394eb6ff9
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-29 13:34:25 -0300

    Adiciona regra de negocio do desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,8 +11,30 @@ function techList(techNames, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(foneNumbers) {
+  if (foneNumbers.length != 11){
+    return "Array com tamanho incorreto.";
+  };
+  let getErro = false;
+  for (let index = 0; index < foneNumbers.length; index += 1) {
+    let countNumber;
+    if(foneNumbers[index] > 9 || foneNumbers[index] < 0) {
+      countErro = true;
+    };
+    for (let number of foneNumbers){
+      if(foneNumbers[index] === number){
+        countNumber += 1;
+      };
+    };
+    if (countNumber > 2) {
+      countErro = true;
+    };
+  };
+  if (countErro === true){
+    return "não é possível gerar um número de telefone com esses valores";
+  } else {
+    return `(${foneNumbers[0]}${foneNumbers[1]})${foneNumbers[2]}${foneNumbers[3]}${foneNumbers[4]}${foneNumbers[5]}${foneNumbers[6]}-${foneNumbers[7]}${foneNumbers[8]}${foneNumbers[9]}${foneNumbers[10]}`
+  }
 }
 
 // Desafio 12
