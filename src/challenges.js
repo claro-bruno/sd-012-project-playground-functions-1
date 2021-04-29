a2e8277d34065ead6471586e8be7b1a14b2ae832
Ramon Paschoal <rpaschoal90@gmail.com>
2021-04-28 21:35:39 -0300

    Desafio 6 - tentativa todas

--- src/challenges.js
+++ src/challenges.js
@@ -34,20 +34,19 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-  let highestNumber = array[0];
   let repeat = 0;
+  highestNumber = array[0];
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] >= highestNumber) {
       highestNumber = array[index];
+    }
+  }
+    for (let index = 0; index < array.length; index += 1) {
+      if (array[index] >= highestNumber)
       repeat += 1;
     }
-  }  
-  return repeat
+    return repeat;
 }
-
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
-console.log(highestCount([0, 0, 0]))
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -61,17 +60,48 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
+  let div3 = 0;
+  let div5 = 0;
+  let div35 = 0;
+  let divn35 = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0) {
+    div3 = 'fizz';
+    return div3;
+    } else if (array[index] % 5 === 0) {
+      div5 = 'buzz';
+      return div5;
+      } else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      div35 = 'fizzBuzz'
+      return div35;
+      } else if (array[index] % 3 !== 0 && array[index] % 5 !== 0) {
+      divn35 = 'bug!'
+      return divn35;
+      }
+  }
+  return 
+}
+
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
+console.log(fizzBuzz([7, 9]))
+console.log(fizzBuzz([9, 25]))
+
+// Desafio 9
+function encode(string) {
+  let split = string.split('');
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index] === 'i') {
+      
+    }
+  }
+  return split;
+}
+function decode() {
   
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+console.log(encode('hi there'))
 
 module.exports = {
   calcArea,
