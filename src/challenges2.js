6531703db46768c924de9787117c34a48be7ba29
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-04-29 10:30:55 -0300

    Resolução do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,10 +21,49 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'],
   'Matheus'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+let validNumber = true;
+let count = 0;
+let number = [];
+let result = '';
+function mapNumbers(numbers, length2) {
+  for (let index = 0; index < length2; index += 1) {
+    if (number[numbers] === number[index]) {
+      count += 1;
+    }
+  }
 }
 
+function validateNumber(length) {
+  for (let index = 0; index < length; index += 1) {
+    mapNumbers(index, length);
+    if (number[index] < 0 || number[index] > 9 || count >= 3) {
+      validNumber = false;
+      result = 'não é possível gerar um número de telefone com esses valores';
+    }
+    count = 0;
+  }
+}
+
+function generatePhoneNumber(array) {
+  result = ''; validNumber = true; count = 0; number = array;
+  if (number.length !== 11) {
+    validNumber = false;
+    result = 'Array com tamanho incorreto.';
+  }
+  if (validNumber) {
+    validateNumber(number.length);
+  }
+  if (validNumber) {
+    let phoneNumber = ['(', number[0], number[1], ') ', number[2], number[3], number[4], number[5],
+      number[6], '-', number[7], number[8], number[9], number[10]];
+    for (let index = 0; index < phoneNumber.length; index += 1) {
+      result += phoneNumber[index];
+    }
+  }
+  return result;
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
