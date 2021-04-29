0182c0eba4ade6775de0541088ea8e151eef0d26
gscuri <gscuri6@hotmail.com>
2021-04-29 13:35:54 -0300

    desafio 2 completo

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,21 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
-   if (valor1 === true && valor2 === true) {
+  if (valor1 === true && valor2 === true) {
     return true;
   } else {
     return false;
   }
 }
-console.log( compareTrue(true, true))
+console.log(compareTrue(true, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, heigth) {
+  area = (base * heigth) / 2;
+  return area;
 }
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 25.5));
 
 // Desafio 3
 function splitSentence() {
