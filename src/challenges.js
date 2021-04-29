4c995b4fc4dd4def356a9f476de5a194f36ecf94
Raphael <raphael.ph2ll@gmail.com>
2021-04-29 20:37:50 -0300

    Desafio_7 mouse and cat

--- src/challenges.js
+++ src/challenges.js
@@ -62,11 +62,14 @@ function highestCount(numbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if(mouse - cat1 > mouse - cat2){
+  mouse = Math.abs(mouse)
+  cat1 = Math.abs(cat1)
+  cat2 = Math.abs(cat2)
+  if((mouse - cat1) > (mouse - cat2)){
     return 'cat1'
-  } else if (mouse - cat1 < mouse - cat2){
+  } else if ((mouse - cat1) < (mouse - cat2)){
     return 'cat2'
-  } else if ( cat1 - cat2 == 0){
+  } else if ((cat1 - cat2) == 0){
     return 'os gatos trombam e o rato foge'
   }
   
