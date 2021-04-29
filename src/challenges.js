99d14a587e5e3abbea74df8ad4c4790483774e19
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-04-29 14:35:08 -0300

    resolve desafio 9 (split, switch e JOIN)

--- src/challenges.js
+++ src/challenges.js
@@ -115,14 +115,48 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(code) {
-  let arrayCode = [code];
-  for(let index in code) {
-    if
-
-
-function decode(arrayDecode) {
-  // seu código aqui
+  code = code.split('');
+  for (let index = 0; index<code.length; index += 1){
+    switch (code[index]) {
+      case 'a': code[index] = '1';
+      break;
+      case 'e': code[index] = '2';
+      break;
+      case 'i': code[index] = '3';
+      break;
+      case 'o': code[index] = '4';
+      break;
+      case 'u': code[index] = '5';
+      break;
+      default: code[index] == code[index];
+    }
+  }
+  return code.join('');
 }
+
+function decode(decoded) {
+  decoded = decoded.split('')
+  for (let index = 0; index<decoded.length; index += 1){
+    switch (decoded[index]) {
+      case '1': decoded[index] = 'a';
+      break;
+      case '2': decoded[index] = 'e';
+      break;
+      case '3': decoded[index] = 'i';
+      break;
+      case '4': decoded[index] = 'o';
+      break;
+      case '5': decoded[index] = 'u';
+      break;
+      default: decoded[index] == decoded[index];
+    }
+  }
+  return decoded.join('');
+}
+
+// Nota de referencia - Em busca no google para verificar se havia algum metódo para juntar os elementos de um array em uma string e retornar a string, foi localizado o meotodo JOIN (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/join)
+// console.log(encode("hi there"));
+// console.log(decode("h3 th2r2!"));
 
 module.exports = {
   calcArea,
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,13 +1,42 @@
-function catAndMouse(mouse, cat1, cat2) {
-  let distancecat1 = (cat1 - mouse);
-  let distancecat2 = (cat2 - mouse);
-  if (distancecat1 < distancecat2) {
-    return "cat1";
-  } else if (distancecat2 < distancecat1) {
-    return "cat2";
-  } else if (distancecat1 === distancecat2) {
-    return 'os gatos trombaram e o rato foge';
+function encode(code) {
+  code = code.split('');
+  for (let index = 0; index<code.length; index += 1){
+    switch (code[index]) {
+      case 'a': code[index] = '1';
+      break;
+      case 'e': code[index] = '2';
+      break;
+      case 'i': code[index] = '3';
+      break;
+      case 'o': code[index] = '4';
+      break;
+      case 'u': code[index] = '5';
+      break;
+      default: code[index] == code[index];
+    }
   }
+  return code.join('');
 }
-// Nota explicativa, foi crucial inverter a ordem nas váriaves de distancia, pois manter mouse-cat, invertia o resultado;
- console.log(catAndMouse(1, 4, 3));
+
+function decode(decoded) {
+  decoded = decoded.split('')
+  for (let index = 0; index<decoded.length; index += 1){
+    switch (decoded[index]) {
+      case '1': decoded[index] = 'a';
+      break;
+      case '2': decoded[index] = 'e';
+      break;
+      case '3': decoded[index] = 'i';
+      break;
+      case '4': decoded[index] = 'o';
+      break;
+      case '5': decoded[index] = 'u';
+      break;
+      default: decoded[index] == decoded[index];
+    }
+  }
+  return decoded.join('');
+}
+
+console.log(encode("hi there"));
+console.log(decode("h3 th2r2!"));
