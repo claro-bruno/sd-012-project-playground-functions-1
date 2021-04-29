f5f730f3627c9bafae3fd6167e1391b95594d6d7
Marcela Silva <marcela.ops@hotmail.br>
2021-04-29 14:28:49 -0300

    desafio 7 - valores absolutos nas distâncias

--- src/challenges.js
+++ src/challenges.js
@@ -58,8 +58,8 @@ function highestCount(numbersArray) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanceCat1 = cat1 - mouse;
-  let distanceCat2 = cat2 - mouse;
+  let distanceCat1 = Math.abs(cat1 - mouse);
+  let distanceCat2 = Math.abs(cat2 - mouse);
 
   if(distanceCat1 > distanceCat2) {
     return ('cat2');
@@ -69,7 +69,7 @@ function catAndMouse(mouse, cat1, cat2) {
     return ('os gatos trombam e o rato foge');
   }
 }
-// console.log (catAndMouse(0, 12, 12));
+// console.log (catAndMouse(1, 0, 2));
 
 // // Desafio 8
 // function fizzBuzz() {
