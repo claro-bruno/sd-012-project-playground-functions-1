040edaf0149f0407b68817466919a814fd27f194
Wellington <junior.wel02@exemplo.br>
2021-04-29 12:35:34 -0300

    Inicio do 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,12 +13,34 @@ function techList(arrayTecnologias, name) {
   }
 }
 
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
-
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numeroTelefone) {
   // seu código aqui
+  let numeroArrumado = [];
+  let repetido = 0;
+  let numeroRepetido = [];
+  let maior = 0;
+
+  for (let i = 0; i < numeroTelefone.length; i += 1) {
+    if (numeroTelefone.length !== 11) {
+      return 'Array com tamanho incorreto.';
+    } else if (numeroTelefone[i] < 0 || numeroTelefone[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+      if (numeroTelefone[i] == numeroTelefone[i - 1]) {
+        repetido += 1;
+      }
+      if (repetido > 2) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+  }
+
+  for (let index in numeroTelefone) {
+    numeroArrumado = `(${numeroTelefone[0]}${numeroTelefone[1]})`;
+  }
+  return numeroArrumado;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 1, 7, 8, 0, 9, 1]));
 
 // Desafio 12
 function triangleCheck() {
