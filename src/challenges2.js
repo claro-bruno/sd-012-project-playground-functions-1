9e92eb766a1e768849ebc4741d5a435d475e9afe
rachidb5 <jordan.rachid@gmail.com>
2021-04-29 12:20:18 -0300

    hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -26,19 +26,19 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(pedido) {
   // seu código aqui
-  let numbers = []
+  let numbers = [];
   let n = pedido.match(/\d/g);
-  let aguas = 0
+  let aguas = 0;
 
-  for (let i = 0; i<n.length; i += 1){
-    numbers.push(parseInt(n[i]))
+  for (let i = 0; i < n.length; i += 1) {
+    numbers.push(parseInt(n[i]));
   }
-  for (let i = 0; i< numbers.length; i += 1){
-    aguas += numbers[i]
+  for (let i = 0; i < numbers.length; i += 1) {
+    aguas += numbers[i];
   }
   if ( aguas > 1){
-  return aguas + " copos de água"
-  } return aguas + " copo de água"
+  return aguas + ' copos de água';
+  } return aguas + ' copo de água';
 }
 
 
