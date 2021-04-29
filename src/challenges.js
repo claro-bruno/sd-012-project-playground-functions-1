4751b28695f932734b023f27adc56b261cc5bd37
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-29 12:05:05 -0300

    Adiciona requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 // Desafio 1
-  let a = true;
-  let b = false;
-  let answer = a && b;
-  function compareTrue(a,b) {
+let a = true;
+let b = false;
+let answer = a && b;
+function compareTrue(a,b) {
   if (a && b === true) {
     return true;
   } else {
@@ -13,35 +13,39 @@
 
 
  // Desafio 2
-  let base = 10;
-  let height = 50;
-  function calcArea (base, height) {
+let base = 10;
+let height = 50;
+function calcArea (base, height) {
   return (base * height) / 2;
- } console.log((base * height) / 2);
+} 
+console.log((base * height) / 2);
  
  
  // Desafio 3
   
-  function splitSentence(string){
-   return string.split(" ");
-  }
-  console.log(splitSentence("vamo que vamo"));
+function splitSentence(string){
+  return string.split(" ");
+}
+console.log(splitSentence("vamo que vamo"));
   
   
 
 // Desafio 4
-  let strings = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-  function concatName(strings){ 
+let strings = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+function concatName(strings){ 
   return strings[strings.length -1] + ' ' + strings[0];
   
- } 
-  console.log(strings[strings.length -1] + ', ' + strings[0]);
+} 
+console.log(strings[strings.length -1] + ', ' + strings[0]);
   
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+let wins = 1;
+let ties = 2;
+function footballPoints(wins,ties) {
+  return ((wins * 3) + (ties *1))
 }
+console.log((wins * 3) + (ties *1))
 
 // Desafio 6
 function highestCount() {
