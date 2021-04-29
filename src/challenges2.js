795836a5221223faaa14e2d6c042cf2291a4f03d
rachidb5 <jordan.rachid@gmail.com>
2021-04-29 11:12:51 -0300

    triangleCheck

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,16 +6,21 @@ function techList() {
 // Desafio 11
 function generatePhoneNumber(n) {
   // seu código aqui
+  let qtd = 0;
+  for (let i = 0; i < n.length; i++){
+    
+  }
   if (n.length === 11) {
     return '(' + n[0] + n[1] + ') ' + n[2] + n[3] + n[4] + n[5] + n[6] + '-' + n[7] + n[8] + n[9] + n[10];
-  } {
-    return 'não é possível gerar um número de telefone com esses valores';
   }
+  return 'não é possível gerar um número de telefone com esses valores';
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  return lineA < lineB + lineC && lineC < lineA + lineB && lineB < lineA + lineC 
+  && lineA > Math.abs(lineC - lineB) && lineB > Math.abs(lineC - lineA) && lineC > Math.abs(lineB - lineA)
 }
 
 // Desafio 13
--- src/testes.js
+++ src/testes.js
@@ -17,14 +17,9 @@
 
   console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Rachift"))*/
 
-  // Desafio 11
-function generatePhoneNumber(n) {
-    // seu código aqui
-    if ( n.length == 11) {
-      return '('+n[0]+n[1]+') '+n[2]+n[3]+n[4]+n[5]+n[6]+'-'+n[7]+n[8]+n[9]+n[10];
-    } {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-  }
-
-  console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+// Desafio 12
+function triangleCheck(lineA, lineB, lineC) {
+  // seu código aqui
+  return lineA < lineB + lineC && lineC < lineA + lineB && lineB < lineA + lineC 
+  && lineA > Math.abs(lineC - lineB) && lineB > Math.abs(lineC - lineA) && lineC > Math.abs(lineB - lineA)
+}
