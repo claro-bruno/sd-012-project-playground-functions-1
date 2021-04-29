b2122803b53e9adfac80c624690726ef428fe6b5
pedro21medeiros <pedro21medeiros@gmail.com>
2021-04-29 18:18:54 -0300

    Resolve desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -78,10 +78,54 @@ function fizzBuzz(arrayFizzBuzz) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
+  let splittedString = string.split("");
+  let vogais = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u'
+  };
+  for (let key in splittedString) {
+    if (splittedString[key] === vogais['1']) {
+      splittedString[key] = '1';
+    } else if (splittedString[key] === vogais['2']) {
+      splittedString[key] = '2';
+    } else if (splittedString[key] === vogais['3']) {
+      splittedString[key] = '3';
+    } else if (splittedString[key] === vogais['4']) {
+      splittedString[key] = '4';
+    } else if (splittedString[key] === vogais['5']) {
+      splittedString[key] = '5';
+    }
+  };
+  return splittedString.join("");
 }
 
-function decode() {
+function decode(string) {
+  let splittedString = string.split("");
+  let vogais = {
+    a: '1',
+    e: '2',
+    i: '3',
+    o: '4',
+    u: '5'
+  };
+  for (let key in splittedString) {
+    if (splittedString[key] === vogais['a']) {
+      splittedString[key] = 'a';
+    } else if (splittedString[key] === vogais['e']) {
+      splittedString[key] = 'e';
+    } else if (splittedString[key] === vogais['i']) {
+      splittedString[key] = 'i';
+    } else if (splittedString[key] === vogais['o']) {
+      splittedString[key] = 'o';
+    } else if (splittedString[key] === vogais['u']) {
+      splittedString[key] = 'u';
+    }
+  };
+  return splittedString.join("");
 }
 
 module.exports = {
