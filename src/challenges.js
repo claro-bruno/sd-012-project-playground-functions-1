78aa75fd6596b20dab7baa7dfa99bc0702d695f7
rachidb5 <jordan.rachid@gmail.com>
2021-04-28 22:24:13 -0300

    desafio9

--- src/challenges.js
+++ src/challenges.js
@@ -49,35 +49,67 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat1';
   } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     return 'cat2';
-  } {
-    return 'os gatos trombam e o rato foge';
-  }
+  } return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz(numbers) {
   // seu código aqui
   let resposta = [];
-  for (let i = 0; i<numbers.length; i+=1){
+  for (let i = 0; i < numbers.length; i += 1) {
     if (numbers[i] % 3 === 0 && numbers[i] % 5 !== 0) {
       resposta.push('fizz');
-    } else if(numbers[i] % 5 === 0 && numbers[i] % 3 !== 0) {
+    } else if (numbers[i] % 5 === 0 && numbers[i] % 3 !== 0) {
       resposta.push('buzz');
-    } else if (numbers[i] % 5 != 0 && numbers[i] % 3 !== 0){
+    } else if (numbers[i] % 5 !== 0 && numbers[i] % 3 !== 0) {
       resposta.push('bug!');
     } else {
-      resposta.push('fizzBuzz') ;
+      resposta.push('fizzBuzz');
     }
   }
   return resposta;
 }
 
 // Desafio 9
-function encode() {
+function encode(frase) {
   // seu código aqui
+  frase = frase.split('')
+  for (let i = 0; i<frase.length; i += 1){
+    switch (frase[i]) {
+      case 'a': frase[i] = '1'
+      break;
+      case 'e': frase[i] = '2'
+      break;
+      case 'i': frase[i] = '3'
+      break;
+      case 'o': frase[i] = '4'
+      break;
+      case 'u': frase[i] = '5'
+      break;
+      default: frase[i] == frase[i]
+    }
+  }
+  return frase.join('')
 }
-function decode() {
+function decode(frase) {
   // seu código aqui
+  frase = frase.split('')
+  for (let i = 0; i<frase.length; i += 1){
+    switch (frase[i]) {
+      case '1': frase[i] = 'a'
+      break;
+      case '2': frase[i] = 'e'
+      break;
+      case '3': frase[i] = 'i'
+      break;
+      case '4': frase[i] = 'o'
+      break;
+      case '5': frase[i] = 'u'
+      break;
+      default: frase[i] == frase[i]
+    }
+  }
+  return frase.join('')
 }
 
 module.exports = {
