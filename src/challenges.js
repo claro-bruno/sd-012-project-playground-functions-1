175b92809690b03e3d9597799d6f4e519a7257a6
Camila Damásio <camiladamasioef@hotmail.com>
2021-04-29 10:47:56 -0300

    Adiciona exercicio 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,14 @@
 // X Desafio 1
-  let x = true;
-  let y = false;
-  let resposta = x && y;
+let x = true;
+let y = false;
+let resposta = x && y;
 function compareTrue(x, y) {
   if (x && y) {
     return true;
   } else {
     return false;
-  };
-};
+  }
+}
 console.log(resposta);
 
 // Desafio 2
@@ -16,26 +16,30 @@ let base = 10;
 let height = 50;
 function calcArea(base, height) {  
   return (base * height) / 2;
-};
+}
 console.log((base * height) / 2);
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
-};
+}
 console.log(splitSentence('go Trybe'));
 
 // Desafio 4
 let strings = ['foguete', 'não', 'tem', 'ré'];
 function concatName(strings) {
-  return strings[strings.length-1] + ', ' + strings[0]
+  return strings[strings.length -1] + ', ' + strings[0];
 }
-console.log(strings[strings.length-1] + ', ' + strings[0]);
+console.log(strings[strings.length -1] + ', ' + strings[0]);
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+let wins = 14;
+let ties = 8;
+pontosMarcados = ((wins * 3) + (ties * 1));
+function footballPoints(wins, ties) {
+  return pontosMarcados
 }
+console.log(pontosMarcados);
 
 // Desafio 6
 function highestCount() {
