dde834cb6ff77dfda193f867f570af7e8d85843a
Wellington <junior.wel02@exemplo.br>
2021-04-28 21:27:21 -0300

    9

--- src/challenges.js
+++ src/challenges.js
@@ -101,13 +101,42 @@ function fizzBuzz(numeros) {
   }
   return resultado;
 }
-console.log(fizzBuzz([9, 25]));
+
 // Desafio 9
-function encode() {
+function encode(frase) {
   // seu código aqui
+  let letras = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  let array = frase.split('');
+  for (let index in frase) {
+    if (Object.hasOwnProperty.call(letras, array[index])) {
+      array[index] = letras[frase[index]];
+    }
+  }
+  return array.join('');
 }
-function decode() {
+
+function decode(fraseNumerica) {
   // seu código aqui
+  let numeros = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  let array = fraseNumerica.split('');
+  for (let index in fraseNumerica) {
+    if (Object.hasOwnProperty.call(numeros, array[index])) {
+      array[index] = numeros[fraseNumerica[index]];
+    }
+  }
+  return array.join('');
 }
 
 module.exports = {
