4334f1b853ce9608c36a1710ffbe8427975e1542
RyanLaiber <ryanlaiber@gmail.com>
2021-04-29 10:04:16 -0300

    adiciona requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
+function techList(list, name) {
   // seu código aqui
+  if (list.length < 1){
+    return "Vazio!"
+  }
+  list.sort();
+
+  let listObjects = [];
+  for (let hability of list){
+    let objHability = {
+      tech : hability ,
+      name : name ,
+    };
+    listObjects.push(objHability);
+  };
+  return listObjects;
 }
 
 // Desafio 11
@@ -24,3 +38,19 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+// let disc = {
+//   obj1 : "abc", obj2 : "acb", obj3 : "aabc"
+// };
+
+// // for(let obj in disc){
+// //   console.log(disc[obj]);
+// // };
+
+// let list = [];
+
+// for (let item in disc){
+//   list.push(disc[item]);
+// }
+// list.sort();
+// console.log(list);
