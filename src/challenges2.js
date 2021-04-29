f17c293c7a99592865ac72c605771930bc55d247
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-04-29 20:34:29 -0300

    Realiza requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let resultado = []
+  let lista = {};
+  let listaOrdenada = tech.sort();
+  if(tech.length === 0) {
+    return 'Vazio!'
+  } else {
+    for(let key in listaOrdenada) {
+      lista = {
+        tech: tech[key],
+        name: name,
+      }
+      resultado.push(lista);
+    }
+  }
+  return resultado;
 }
+console.log(techList([], 'Lucas'));
+
+
 
 // Desafio 11
 function generatePhoneNumber() {
