3a5b4d57dfb6706c67298e7f63628bed0755d64e
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-30 12:46:32 -0300

    Adiciona requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -61,9 +61,17 @@ function highestCount(contar){
 console.log(highestCount(contar))
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+ function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+  return 'cat1'
+ } else if (Math.abs(cat2 - mouse) < Math.abs(cat1-mouse)) {
+   return 'cat2'
+ } else {
+   return 'os gatos trombam e o rato foge'
+} 
 }
+console.log(catAndMouse(7,1,8))
+
 
 // Desafio 8
 function fizzBuzz() {
