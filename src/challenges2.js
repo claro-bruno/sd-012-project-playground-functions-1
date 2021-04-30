854c0af00f9001072908bbf471129ab4c298930c
José Carlos <kalarruda@hotmail.com>
2021-04-29 21:34:51 -0300

    11-desafio

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,38 +21,50 @@ console.log(techList([], 'jose'));
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'jose'));
 
 
+// // Desafio 11
+// function generatePhoneNumber(numeros) {
+//   let maximoNumeros=11;
+//   for (index = 0 ; index <numeros.length ; index +=1){
+//     if(numeros.length<maximoNumeros || numeros.length>maximoNumeros){
+//       return ("Array com tamanho incorreto.");
+//     }
+//     else if(numeros[index]<0 || numeros[index]>9) {
+//       return ("não é possível gerar um número de telefone com esses valores");
+//     }
+//   }
+//   return (`(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`);
+// }
+
+// console.log(generatePhoneNumber([2,1,9,9,0,3,5,3,2,1,9]));
+
+
 // Desafio 11
 function generatePhoneNumber(numeros) {
   let maximoNumeros=11;
+  let numeroRepetido=0;
+  
   for (index = 0 ; index <numeros.length ; index +=1){
+    let contadorNumeroRepetido=0;
+    for (index2 =1 ; index2 < numeros.length ; index2 +=1){
+      if(numeros[index]===numeros[index2]){
+        contadorNumeroRepetido +=1;
+      }
+    }
     if(numeros.length<maximoNumeros || numeros.length>maximoNumeros){
       return ("Array com tamanho incorreto.");
     }
-    else if(numeros[index]<0 || numeros[index]>9) {
+    else if(numeros[index]<0 || numeros[index]>9 || contadorNumeroRepetido>=3) {
       return ("não é possível gerar um número de telefone com esses valores");
     }
   }
   return (`(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`);
 }
 
-console.log(generatePhoneNumber([2,1,9,9,0,3,5,3,2,1,9]));
+console.log(generatePhoneNumber([2,1,9,9,0,3,5,3,2,1,8]));
 
 
-// Desafio 11
-function generatePhoneNumber(numeros) {
-  let maximoNumeros=11;
-  for (index = 0 ; index <numeros.length ; index +=1){
-    if(numeros.length<maximoNumeros || numeros.length>maximoNumeros){
-      return ("Array com tamanho incorreto.");
-    }
-    else if(numeros[index]<0 || numeros[index]>9) {
-      return ("não é possível gerar um número de telefone com esses valores");
-    }
-  }
-  return (`(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`);
-}
 
-console.log(generatePhoneNumber([2,1,9,9,0,3,5,3,2,1,9]));
+
 
 
 // Desafio 12
