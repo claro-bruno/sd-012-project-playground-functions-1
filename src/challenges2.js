a4cc739b715a049c39e9948100e0e923c9240da4
José Carlos <kalarruda@hotmail.com>
2021-04-30 19:11:51 -0300

    altera a posicao do Array com tamanho incorreto do desafio-11

--- src/challenges2.js
+++ src/challenges2.js
@@ -26,24 +26,25 @@ function generatePhoneNumber(numeros) {
   let maximoNumeros = 11;
   let numeroRepetido = 0;
   
+  if(numeros.length!=11){ //refatorar porque a gisele pediu
+    return "Array com tamanho incorreto.";
+  }
+
   for (index = 0 ; index <numeros.length ; index +=1) {
     let contadorNumeroRepetido = 0;
     for (index2 =1 ; index2 < numeros.length ; index2 +=1){
       if(numeros[index]===numeros[index2]){
         contadorNumeroRepetido +=1;
       }
-    }
-    if(numeros.length!=11){ //refatorar porque a gisele pediu
-      return "Array com tamanho incorreto.";
-    }
-    else if(numeros[index]<0 || numeros[index]>9 || contadorNumeroRepetido>=3) {
+    }    
+    if(numeros[index]<0 || numeros[index]>9 || contadorNumeroRepetido>=3) {
       return "não é possível gerar um número de telefone com esses valores.";
     }
-  }
-  return `(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
+  }  
+  return `(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;  
 }
 
-// console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,1]));
+// console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,1,2]));
 
 
 // Desafio 12
