249a185f40b41cbb9becaeca405d6b4f62e0a8b5
Marco Antonio <marcoantoniocdspy@gmail.com>
2021-04-30 14:44:42 -0300

    Adiciona Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -74,9 +74,22 @@ console.log(catAndMouse(7,1,8))
 
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+let resultado = [];
+for (index = 0; index < array.length; index ++) {
+  if ((array[index] % 3 === 0) && (array[index] % 5 === 0)) {
+     resultado.push('fizzBuzz');
+  } else if (array[index] % 3 === 0) {
+    resultado.push('fizz');
+  } else if (array[index] % 5 === 0) {
+    resultado.push('buzz');
+  } else {
+    resultado.push('bug!');
+  }
 }
+return resultado;
+}
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
