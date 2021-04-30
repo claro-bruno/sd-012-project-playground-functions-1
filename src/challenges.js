1a9acd94e1cbaaa3e35e2048295b984030fcfacb
Juliane Marques <mqs.juliane@gmail.com>
2021-04-30 14:58:40 -0300

    testando exercicios 1 e 2, zerei tudo part 2

--- src/challenges.js
+++ src/challenges.js
@@ -6,9 +6,10 @@ function compareTrue(nun1, nun2) {
 };
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
+} 
 
 // Desafio 3
 function splitSentence() {
