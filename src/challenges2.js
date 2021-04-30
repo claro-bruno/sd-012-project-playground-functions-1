538408654b7ad90a81ade92878d3b7112e0e92b5
José Carlos <kalarruda@hotmail.com>
2021-04-30 13:44:36 -0300

    Tira o console.log depois do desafio-13

--- src/challenges2.js
+++ src/challenges2.js
@@ -92,7 +92,7 @@ function hydrate(string) {
     }return `${somaNumeros} copos de água`;
   }
 
-console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
+// console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
 
 module.exports = {
   generatePhoneNumber,
