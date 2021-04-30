0a3e1281ec6dd67696b6d029d0209109b131c57f
martinsph <ph.mmartins@gmail.com>
2021-04-30 15:35:00 -0300

    adicona requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,4 @@
 // Desafio 10
-
 function techList(array, nome) {
   let objList = [];
   array.sort();
@@ -19,11 +18,40 @@ function techList(array, nome) {
 
 console.log(techList([], "Pedro"));
 
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(array) {
+  let result = 0;
+  let contador = 0;
+  let aux = 0;
+  if (array.length != 11) {
+    result = 'Array com tamanho incorreto. erro 1';
+  };
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      result = 'não é possível gerar um número de telefone com esses valores. erro 2';
+    };
+  };
+
+  for (let index = 0; index < array.length; index += 1) {
+      for (let index2 = 1; index2 < array.length; index2 += 1){
+        if (array[index2] === array[index]) {
+          contador = contador + 1;
+          if (contador >= 3) {
+            result = 'não é possível gerar um número de telefone com esses valores. erro 3';
+          } else {
+            contador = 0;
+          }
+        };
+      };
+  };
+
+  result = '(' + array[0]+array[1] + ') ' + array[2]+array[3]+array[4]+array[5]+array[6] + '-' + array[7]+array[8]+array[9]+array[10] + '.';
+
+  return result;
+};
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
