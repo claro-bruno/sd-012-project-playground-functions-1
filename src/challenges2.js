270ddbe6c55ae3ba95411b81ca2a63fc2cd73331
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-04-30 17:57:58 -0300

    Corrige a funçao push no requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,23 @@
 // Desafio 10
 function techList(techNames,personName) {
-  let secondList = {};
+  let secondList = [];
   if(techNames.length === 0){
     return "Vazio!";
   } else {
-    let firstlist = techNames.sort();
-    for (let index = 0; index < firstlist.length; index +=1) {
-      secondList.push = {
-        tech: firstlist[index],
-        name: personName
-      }
+    let firstList = techNames.sort();
+    for (let index = 0; index < firstList.length; index += 1) {
+      secondList.push(
+        {
+        tech: firstList[index],
+        name: personName,
+      },);
     }
     return secondList;
   }
 }
- .
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
