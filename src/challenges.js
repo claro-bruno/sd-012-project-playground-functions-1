f6b99538ff3b41265327729641205ea39ab144e8
Anderson B <andbmarques@gmail.com>
2021-04-30 09:18:21 -0300

    Requisito 7 adicionado

--- src/challenges.js
+++ src/challenges.js
@@ -62,7 +62,16 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(cat1, cat2, mouse) {
+  if(cat1 === mouse || cat2 === mouse){
+    return 'Os gatos trombam e o rato foge'
+  }
+  else if(Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+    return 'cat1'
+  }
+  else if(Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
+    return 'cat2'
+  }
   
 }
 
