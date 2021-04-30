d4cc5592181b346459a977b0971be386b3306251
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-04-29 21:17:36 -0300

    Começa realizaçao do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,9 +21,24 @@ console.log(techList([], 'Lucas'));
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayOnzeNumeros) {
+  let numeroTelefonico = '(';
+  for(let key in arrayOnzeNumeros) {
+    switch(arrayOnzeNumeros[key]) {
+      case (arrayOnzeNumeros[1]):
+        numeroTelefonico += arrayOnzeNumeros[key] + ')';
+        break;
+      case (arrayOnzeNumeros[6]):
+        numeroTelefonico += arrayOnzeNumeros[key] + '-';
+        break;
+      default:
+        numeroTelefonico += arrayOnzeNumeros[key];
+        break;
+      }
+  }
+  return numeroTelefonico;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
