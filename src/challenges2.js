580d97d973c0f8f06d9b1de8a1dd2336fd2d632a
AndZoni <lorenzoniandre@outlook.com>
2021-04-30 16:04:44 +0100

    Corrigindo Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,16 @@
 // Desafio 10
 function techList(theTech, theName) {
-  theTech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
-  theName = 'Lucas';
   let objectList = [];
+  let odTheTech = theTech.sort();
   if (theTech.length === 0) {
     return ('Vazio!');
   }
-  for (let index = 0; index < theTech.length; index += 1) {
+  for (let index = 0; index < odTheTech.length; index += 1) {
     let object = {
       tech: '',
       name: theName,
     };
-    object.tech = theTech[index];
+    object.tech = odTheTech[index];
     objectList.push(object);
   }
   return (objectList);
