952071fd77d3bd0df593d6081e9d8566b9117927
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-04-30 20:48:09 -0300

    Retira o console.log da questao 10 e resolve a questao 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,19 +16,49 @@ function techList(techNames,personName) {
   }
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+
 
 
 // Desafio 11
 function generatePhoneNumber(numbers) {
-
-  // seu código aqui
+  for (let counter = 0; counter < numbers.length; counter +=1) {    
+    if (numbers[counter] < 0) {
+      let counter2 = counter2 +1;
+      if (counter2 !== 0) {
+      return "não é possível gerar um número de telefone com esses valores";
+      }
+    } else if (numbers[counter] > 9) {
+      let counter3 = counter3 +1;
+      if (counter3 !== 0) {
+      return "não é possível gerar um número de telefone com esses valores";
+      return ""
+      }
+    } else if (numbers.length !== 11) {
+      return "Array com tamanho incorreto.";
+    } else {
+      return "Tudo ok";
+    }  
+  }
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 12]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let isTriangle = false;
+  if((lineA < lineB + lineC) && (lineA > Math.abs(lineB - lineC))) {
+    isTriangle = true;
+  } else if ((lineB < lineA + lineC ) && (lineB > Math.abs(lineB - lineA))){
+    isTriangle = true;
+  } else if ((lineC < lineB + lineA ) && (lineC > Math.abs(lineA - lineB))){
+    isTriangle = true;
+  } else {
+    isTriangle = false;
+  }
+  return isTriangle;
 }
+
+
 
 // Desafio 13
 function hydrate() {
