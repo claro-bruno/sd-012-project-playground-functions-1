d28f76953d1017b74c99023703a71e7e6f0a9d76
martinsph <ph.mmartins@gmail.com>
2021-04-30 18:23:35 -0300

    teste3 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -51,7 +51,7 @@ function generatePhoneNumber(array2) {
   return result;
 };
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+//console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
