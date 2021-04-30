438973c88e8eb894e903f40a90d6282da1731887
José Carlos <kalarruda@hotmail.com>
2021-04-30 18:56:13 -0300

    adiciona 'copos e copo' no desafio-13 e simplifica o if de array tamanho incorreto

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,20 @@
 // Desafio 10
 function techList(tecnology, name) {
-  //o resultado tem que ser uma LISTA gigante com os itens do objeto
+  // o resultado tem que ser uma LISTA gigante com os itens do objeto
   // as tecnologias precisam ser ordenadas
 
-  let lista = [];//criar lista vazia para colocar os itens
-  let ordenada = tecnology.sort();//para ordenar a lista logo no início
-  for (let index=0 ; index<ordenada.length ; index +=1){
-    tecnologia = {
-      tech: tecnology [index],//passar de 1 em 1 conteúdo da tecnology
-      name: name,
-    }    
-    lista.push(tecnologia);//empurra itens dentro da lista vazia
-  }  
-  if(ordenada.length==0){
+  let lista = [];// criar lista vazia para colocar os itens
+  let ordenada = tecnology.sort();// para ordenar a lista logo no início
+  for(let index = 0 ; index < ordenada.length ; index += 1) {
+    let tecnologia = {
+      tech: tecnology[index], //passar de 1 em 1 conteúdo da tecnology
+      name: name
+    };
+    lista.push(tecnologia); //empurra itens dentro da lista vazia
+  }
+  if (ordenada.length == 0) {
     return 'Vazio!';
-   }
+  }
   return lista;
 }
 // console.log(techList([], 'jose'));
@@ -23,21 +23,21 @@ function techList(tecnology, name) {
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
-  let maximoNumeros=11;
-  let numeroRepetido=0;
+  let maximoNumeros = 11;
+  let numeroRepetido = 0;
   
-  for (index = 0 ; index <numeros.length ; index +=1){
-    let contadorNumeroRepetido=0;
+  for (index = 0 ; index <numeros.length ; index +=1) {
+    let contadorNumeroRepetido = 0;
     for (index2 =1 ; index2 < numeros.length ; index2 +=1){
       if(numeros[index]===numeros[index2]){
         contadorNumeroRepetido +=1;
       }
     }
-    if(numeros.length<maximoNumeros || numeros.length>maximoNumeros){
+    if(numeros.length!=maximoNumeros){ //refatorar porque a gisele pediu
       return "Array com tamanho incorreto.";
     }
     else if(numeros[index]<0 || numeros[index]>9 || contadorNumeroRepetido>=3) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return "não é possível gerar um número de telefone com esses valores.";
     }
   }
   return `(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
@@ -70,10 +70,15 @@ function hydrate(string) {
     {
       // numeros.push(parseInt(stringSeparada[index]));  
       somaNumeros += parseInt(stringSeparada[index]);
+    }  
   }
-  
-    }return `${somaNumeros} copos de água`;
+  if(somaNumeros>1){
+    return `${somaNumeros} copos de água`;
   }
+  else if(somaNumeros==1){
+    return `${somaNumeros} copo de água`;
+  }
+}
 
 // console.log(hydrate("1 cachaça, 5 cervejas e 2 copo de vinho"));
 
