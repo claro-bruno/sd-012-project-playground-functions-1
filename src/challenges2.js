6c62a925f60022eed20c57add637f4f82c52644a
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-04-30 00:23:00 -0300

    Satisfaz requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,8 +14,54 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function isValidNegative(arrayPhoneNumber) {
+  for (let index = 0; index < arrayPhoneNumber.length; index += 1) {
+    if (arrayPhoneNumber[index] < 0 || arrayPhoneNumber[index] > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isValidRep(arrayPhoneNumber) {
+  for (let index = 0; index < arrayPhoneNumber.length; index += 1) {
+    let count = 0;
+    for (
+      let indexComp = 0;
+      indexComp < arrayPhoneNumber.length;
+      indexComp += 1
+    ) {
+      if (arrayPhoneNumber[indexComp] === arrayPhoneNumber[index]) {
+        count += 1;
+      }
+      if (count > 2) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(arrayPhoneNumber) {
+  let validNegative = isValidNegative(arrayPhoneNumber);
+  let validRep = isValidRep(arrayPhoneNumber);
+  if (arrayPhoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto';
+  }
+  if (validNegative === true || validRep === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  let ddd = arrayPhoneNumber.slice(0, 2).join('');
+  let firstHalf = arrayPhoneNumber.slice(2, 7).join('');
+  let secondHalf = arrayPhoneNumber.slice(7, 11).join('');
+  ddd = ddd.padStart(3, '(');
+  ddd = ddd.padEnd(4, ')');
+  let number = [];
+  number[0] = firstHalf;
+  number[1] = secondHalf;
+  number = number.join('-');
+  let phoneNumber = `${ddd} ${number}`;
+  return phoneNumber;
 }
 
 // Desafio 12
