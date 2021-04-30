5b474c0a5b5eaffc050eaeacde498acffd346c03
José Carlos <kalarruda@hotmail.com>
2021-04-30 19:22:43 -0300

    retira o ponto final da frase nao e possivel gerar um numero...

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,22 +23,22 @@ function techList(tecnology, name) {
 
 // Desafio 11
 function generatePhoneNumber(numeros) {
-  let maximoNumeros = 11;
-  let numeroRepetido = 0;
+
+
   
-  if(numeros.length!=11){ //refatorar porque a gisele pediu
-    return "Array com tamanho incorreto.";
+  if (numeros.length !== 11) { // refatorar porque a gisele pediu
+    return 'Array com tamanho incorreto.';
   }
 
-  for (index = 0 ; index <numeros.length ; index +=1) {
+  for (let index = 0; index < numeros.length; index += 1) {
     let contadorNumeroRepetido = 0;
-    for (index2 =1 ; index2 < numeros.length ; index2 +=1){
-      if(numeros[index]===numeros[index2]){
-        contadorNumeroRepetido +=1;
+    for (index2 = 1; index2 < numeros.length; index2 += 1) {
+      if (numeros[index] === numeros[index2]) {
+        contadorNumeroRepetido += 1;
       }
-    }    
-    if(numeros[index]<0 || numeros[index]>9 || contadorNumeroRepetido>=3) {
-      return "não é possível gerar um número de telefone com esses valores.";
+    }
+    if (numeros[index] < 0 || numeros[index] > 9 || contadorNumeroRepetido >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }  
   return `(${numeros[0]}${numeros[1]})${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;  
