8ab398fca47bdce7ed94e80d1bb90197f54cc627
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-04-30 08:58:56 -0300

    realizado o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,14 @@
+//função auxiliar para o desafio 11:
+
+const quantidadeDeRepeticao = (arr) => {
+  arr.forEach(element => {
+    const repetition = 0;
+    arr.forEach(item => {
+      repetition++
+    })
+  })
+}
+
 // Desafio 10
 function techList(arr, name) {
 
@@ -24,9 +35,31 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numberArr) {
+
+  if (numberArr.length != 11) {
+    return 'Array com tamanho incorreto';
+  };
+
+  numberArr.forEach(element => {
+    let repetition = 0;
+    numberArr.forEach(number => {
+      if (element === number) {
+        repetition++;
+      } if (repetition === 3 || ((element || number) < 0) || ((element || number) > 9))
+        return 'não é possível gerar um número de telefone com esses valores';
+    });
+  });
+
+  numberArr.unshift('(');
+  numberArr.splice(3, 0, ')');
+  numberArr.splice(4, 0, ' ')
+  numberArr.splice(9, 0, '-');
+  return numberArr.join('');
+
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
