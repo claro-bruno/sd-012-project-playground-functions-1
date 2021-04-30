a3b708954771a020cb875b202ad7bb2410b462a9
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-04-30 13:03:50 -0300

    desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  começando
-}
+function compareTrue(valor1,valor2) {
+  if(valor1 == true && valor2 == true) {
+    return true;
+  } else {
+    return false
+  };
 
 // Desafio 2
 function calcArea() {
