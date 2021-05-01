e57ab91ef8cd13aa49ca113ce25172e8af67ade1
Bruno Augusto <brunaugusto@gmail.com>
2021-05-01 04:04:05 -0300

    Correçao Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,11 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let checklineA = lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) ? true : false;
+  let checklineB = lineA < (lineB + lineC) && lineA > Math.abs(lineA - lineC) ? true : false;
+  let checklineC = lineA < (lineB + lineC) && lineA > Math.abs(lineA - lineB) ?  true : false;
+  return checklineA === true || checklineB === true || checklineC === true ? true : false;
 }
 
 // Desafio 13
