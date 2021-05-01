b203864d81f9e88e5b44f41f0559e8cfb95db008
Nuwanda <morato_dev@protonmail.com>
2021-05-01 20:49:48 -0300

    acho que acabei tudo kkk vamo ve

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,24 +12,23 @@ function techList(technologies, person) {
     }
 }
 
-
 // Desafio 11
 function generatePhoneNumber(candidateNumbers) {
-    let phone = `(${candidateNumbers[0]}${candidateNumbers[1]}) ${candidateNumbers[2]}${candidateNumbers[3]}${candidateNumbers[4]}${candidateNumbers[5]}${candidateNumbers[6]}-${candidateNumbers[7]}${candidateNumbers[8]}${candidateNumbers[9]}${candidateNumbers[10]}`
-    return phone
+    let phone = `(${candidateNumbers[0]}${candidateNumbers[1]}) ${candidateNumbers[2]}${candidateNumbers[3]}${candidateNumbers[4]}${candidateNumbers[5]}${candidateNumbers[6]}-${candidateNumbers[7]}${candidateNumbers[8]}${candidateNumbers[9]}${candidateNumbers[10]}`;
+    return phone;
 }
 
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
-    let possibleA = lineA > Math.abs(lineB - lineC) && lineA < (lineB + lineC);
-    let possibleB = lineB > Math.abs(lineA - lineC) && lineB < (lineA + lineC);
-    let possibleC = lineC > Math.abs(lineA - lineB) && lineC < (lineA + lineA);
+    let possibleA = lineA > Math.abs(lineB - lineC) && lineA < lineB + lineC;
+    let possibleB = lineB > Math.abs(lineA - lineC) && lineB < lineA + lineC;
+    let possibleC = lineC > Math.abs(lineA - lineB) && lineC < lineA + lineA;
     if ((possibleA && possibleB && possibleC) === true) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
 
@@ -37,12 +36,12 @@ function triangleCheck() {
 function hydrate(manyDrinks) {
     let regex = /\d+/g;
     let matches = manyDrinks.match(regex).map(Number);
-    sum = 0
+    sum = 0;
     for (let index = 0; index < matches.length; index += 1) {
-        sum = sum + matches[index]
+        sum = sum + matches[index];
     }
-    return `${sum} copos de água`
-        //code idea by https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994
+    return `${sum} copos de água`;
+    //code idea by https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994
 }
 
 module.exports = {
