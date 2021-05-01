1ab55c027ab4ea2eed9ecdc4b862544d57053fcc
Bruno Augusto <brunaugusto@gmail.com>
2021-04-30 22:05:44 -0300

    Terceiro Desafio Correçoes

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,13 @@
 // Desafio 1
 function compareTrue(first, second) {
   // seu código aqui
-  return (first === true && second === true) ? true : false;
+  return first === true && second === true ? true : false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  return (!Number(base) && !Number(height)) ? ((base * height) /2 ) : 'Not a Number';
+  return (Number(base) && Number(height)) ? ((base * height) / 2) : 'Not a Number';
 }
 
 // Desafio 3
