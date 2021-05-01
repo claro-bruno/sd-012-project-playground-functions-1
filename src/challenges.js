0b943de96d891a300a30803bbdbaf8add66c06e3
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-04-30 23:35:42 -0300

    Adiciona exerc 9

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,11 @@
+/* eslint-disable sonarjs/cognitive-complexity */
+/* eslint-disable complexity */
+/* eslint-disable max-lines-per-function */
 // Desafio 1
 function compareTrue(word1, word2) {
-  if (word1 === true && word2 === true){
+  if (word1 === true && word2 === true) {
     return true
-  }
-  else {
+  } else {
     return false
   }
 }
@@ -24,17 +26,17 @@ calcArea(base, height)
 function splitSentence(phrase) {
   let result = phrase.split(' ');
   return result;
-  }
- 
+}
+
 
 (splitSentence('vamo que vamo'))
 
 // Desafio 4
 function concatName(nomeCompleto) {
-for (contador = 0; contador < nomeCompleto.length; contador+=1);{
-  nomeCompleto =  (nomeCompleto[nomeCompleto.length - 1] + ',') + ' ' + (nomeCompleto[0]);
-}
-return nomeCompleto
+  for (contador = 0; contador < nomeCompleto.length; contador += 1); {
+    nomeCompleto = (nomeCompleto[nomeCompleto.length - 1] + ',') + ' ' + (nomeCompleto[0]);
+  }
+  return nomeCompleto
 }
 concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
 
@@ -50,69 +52,111 @@ footballPoints(wins, ties)
 
 // Desafio 6
 function highestCount(array) {
-let repeat = 0;
-let highNumber = 0;
+  let repeat = 0;
+  let highNumber = 0;
 
-  for (accountant = 0; accountant<array.length; accountant+=1){
-    if (array[accountant] > highNumber){
+  for (accountant = 0; accountant < array.length; accountant += 1) {
+    if (array[accountant] > highNumber) {
       highNumber = array[accountant]
     }
   }
-  for (accountant = 0; accountant<array.length; accountant+=1){
-    if (highNumber === array[accountant]){
-      repeat +=1
-    }  
+  for (accountant = 0; accountant < array.length; accountant += 1) {
+    if (highNumber === array[accountant]) {
+      repeat += 1
+    }
   }
-return repeat
+  return repeat
 }
 highestCount([9, 1, 2, 3, 9, 5, 7])
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-let cat1position = Math.abs(mouse - cat1)
-let cat2position = Math.abs(mouse - cat2)
+  let cat1position = Math.abs(mouse - cat1)
+  let cat2position = Math.abs(mouse - cat2)
 
-if (cat1position === cat2position) {
-  return "os gatos trombam e o rato foge"
+  if (cat1position === cat2position) {
+    return "os gatos trombam e o rato foge"
+  }
+  if (cat1position < cat2position) {
+    return "cat1"
+  }
+  if (cat2position < cat1position) {
+    return "cat2"
+  }
 }
-if (cat1position<cat2position) {
-  return "cat1"
-}
-if (cat2position<cat1position) {
-  return "cat2"
-}
-}
-catAndMouse(1,0,2)
+catAndMouse(1, 0, 2)
 
 // Desafio 8
 function fizzBuzz(array) {
-  let finalvalue = [];   
-  for (let accountant = 0; accountant < array.length; accountant+=1){
+  let finalvalue = [];
+  for (let accountant = 0; accountant < array.length; accountant += 1) {
 
-    if(array[accountant] % 3 === 0 && array[accountant] % 5 === 0){
+    if (array[accountant] % 3 === 0 && array[accountant] % 5 === 0) {
       finalvalue.push("fizzBuzz")
-    }
-    else if(array [accountant] % 3 === 0){
-      finalvalue.push("fizz") 
-    }
-    else if(array[accountant] % 5 === 0){
+    } else if (array[accountant] % 3 === 0) {
+      finalvalue.push("fizz")
+    } else if (array[accountant] % 5 === 0) {
       finalvalue.push("buzz")
-    }
-    else {
+    } else {
       finalvalue.push("bug!");
     }
   }
-return finalvalue
+  return finalvalue
 }
 fizzBuzz([9, 25])
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(word) {
+  let encriptword = '';
+  let yesno = true;
+  let encript = {
+    a: '1',
+    e: '2',
+    i: '3',
+    o: '4',
+    u: '5',
+  };
+  for (let accountant = 0; accountant < word.length; accountant += 1) {
+    yesno = true;
+    for (let key in encript) {
+      if (word[accountant] === key) {
+        encriptword += encript[key];
+        yesno = false;
+      }
+    }
+    if (yesno) {
+      encriptword += word[accountant];
+    }
+  }
+return encriptword;
 }
-function decode() {
-  // seu código aqui
+encode('hi there!');
+
+function decode(word) {
+  let encriptword = ''
+  let yesno = true;
+  let encript = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  for (let accountant = 0; accountant < word.length; accountant += 1) {
+    yesno = true;
+    for (let key in encript) {
+      if (word[accountant] === key) {
+        encriptword += encript[key];
+        yesno = false;
+      }
+    }
+    if (yesno) {
+      encriptword += word[accountant];
+    }
+  }
+  return encriptword;
 }
+decode('h3 th2r2!');
 
 module.exports = {
   calcArea,
