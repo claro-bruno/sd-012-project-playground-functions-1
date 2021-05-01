0de92c0842643dceec58a632db375cf75ece1249
Gabriel Viana <g.viana87@hotmail.com>
2021-05-01 20:22:12 -0300

    desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,7 @@
 // Desafio 10
 function techList(tech, name) {
   // seu código aqui
-  for (let index = 0; index <= tech.lenght; index += 1) {
-    if (tech[index] === undefined) (
-      
-  };
-  }
+  
 }
 
 // Desafio 11
@@ -16,14 +12,17 @@ function generatePhoneNumber() {
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  let check = false
   if (lineA > (lineB + lineC) || lineB > (lineA + lineC) || lineC > (lineA + lineB)
   || lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineA - lineB)) {
-  return false;
+  check = false;
+} else{
+  check = true;
 }
 
-return true;
+return check;
 };
-  console.log (triangleCheck(-3, 14, 8))
+//console.log (triangleCheck(-3, 14, 8))
 
 
 // Desafio 13
