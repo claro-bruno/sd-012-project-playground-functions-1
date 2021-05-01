2cebc2b6ff60a7b6989dd782de974e2843548ddc
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-01 12:42:54 -0300

    Alteracao de sintaxe e nos desafios de 1 a 7 e refatoracao do desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -16,29 +16,30 @@ function splitSentence(string) {
   for (let letter of string) {
     if (letter !== ' ') {
       word += letter;
-    }
-    else {
+    } else {
       splitWords.push(word);
       word = '';
-    };
-  };
+    }
+  }
 
   if (word !== '') {
     splitWords.push(word);
-  };
+  }
 
   return splitWords;
 }
 
 // Desafio 4
 function concatName(arr) {
-  return arr[arr.length - 1] + ', ' + arr[0];
-};
+  let stringConcatenada = arr[arr.length - 1] + ', ' + arr[0];
+
+  return stringConcatenada;
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return wins * 3 + ties;
-};
+}
 
 // Desafio 6
 function highestCount(arrayNumeros) {
@@ -49,40 +50,37 @@ function highestCount(arrayNumeros) {
     if (numero > maiorNumero) {
       maiorNumero = numero;
       repeticoesMaiorNumero = 1;
-    }
-    else if (numero == maiorNumero) {
+    } else if (numero === maiorNumero) {
       repeticoesMaiorNumero += 1;
-    };
-  };
+    }
+  }
 
   return repeticoesMaiorNumero;
-};
+}
 
 // Desafio 7
 function catAndMouse(cat1, cat2) {
   if (cat1 === cat2) {
     return 'os gatos trombam e o rato foge';
-  }
-  else if (cat1 < cat2) {
+  } else if (cat1 < cat2) {
     return 'cat1';
   }
-  else {
-    return 'cat2';
-  };
-};
+
+  return 'cat2';
+}
 
 // Desafio 8
 function fizzBuzz(arrayNumeros) {
   let resultadosFizzBuzz = [];
 
   for (let numero of arrayNumeros) {
-    if (numero % 3 === 0 && numero % 5 === 0) {
+    if (!(numero % 15)) {
       resultadosFizzBuzz.push('fizzBuzz');
     }
-    else if (numero % 3 === 0) {
+    else if (!(numero % 3)) {
       resultadosFizzBuzz.push('fizz');
     }
-    else if (numero % 5 === 0) {
+    else if (!(numero % 5)) {
       resultadosFizzBuzz.push('buzz');
     }
     else {
@@ -119,8 +117,6 @@ function encode(mensagem) {
 
   return mensagemCodificada;;
 };
-
-console.log(encode('Retorne uma string codificada'));
 
 function decode(mensagemCodificada) {
   let transformacao = {
