d2b7a265b48f07bd53117e1e29e82e7d09e55c5e
Tulio <tuliotrindade99@gmail.com>
2021-05-01 12:39:47 -0300

    desafio 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,14 +11,38 @@ let arrayOrdenado=array.sort()
     return 'Vazio!';
   }
 }
-console.log(techList([],'lucas'))
+
 
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let saida='';
+      for (let n of array) {
+        let contador = 0;
+          for (let c of array) {
+            if (n === c) {
+              contador += 1;
+            }
+          }
+          if(array.length!=11){
+            saida='Array com tamanho incorreto.';
+          }
+          else if(array[n]>9 || array[n]<0 || contador>=3){
+            saida='não é possível gerar um número de telefone com esses valores';
+          }
+          else{
+            saida='('+array[0]+array[1]+') '+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];
+          }
+      }
+      
+      
+
+  return saida; 
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
 
 // Desafio 12
 function triangleCheck() {
