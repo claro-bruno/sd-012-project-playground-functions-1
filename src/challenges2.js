a75a6007ddec735cd5519eb591a5dc91b99898dc
Nuwanda <morato_dev@protonmail.com>
2021-05-01 18:47:17 -0300

    desafio 12 e 13 feitos

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,18 +14,35 @@ function techList(technologies, person) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-    // seu código aqui
+function generatePhoneNumber(candidateNumbers) {
+    let phone = `(${candidateNumbers[0]}${candidateNumbers[1]}) ${candidateNumbers[2]}${candidateNumbers[3]}${candidateNumbers[4]}${candidateNumbers[5]}${candidateNumbers[6]}-${candidateNumbers[7]}${candidateNumbers[8]}${candidateNumbers[9]}${candidateNumbers[10]}`
+    return phone
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
-    // seu código aqui
+    let possibleA = lineA > Math.abs(lineB - lineC) && lineA < (lineB + lineC);
+    let possibleB = lineB > Math.abs(lineA - lineC) && lineB < (lineA + lineC);
+    let possibleC = lineC > Math.abs(lineA - lineB) && lineC < (lineA + lineA);
+    if ((possibleA && possibleB && possibleC) === true) {
+        return true
+    } else {
+        return false
+    }
 }
 
 // Desafio 13
-function hydrate() {
-    // seu código aqui
+function hydrate(manyDrinks) {
+    let regex = /\d+/g;
+    let matches = manyDrinks.match(regex).map(Number);
+    sum = 0
+    for (let index = 0; index < matches.length; index += 1) {
+        sum = sum + matches[index]
+    }
+    return `${sum} copos de água`
+        //code idea by https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994
 }
 
 module.exports = {
