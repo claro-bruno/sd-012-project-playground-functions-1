bb94a4f19eb2a9ca2c5c19d096e31174cd0e0dfb
Bruno Augusto <brunaugusto@gmail.com>
2021-05-01 04:22:11 -0300

    Correçao Desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,16 +19,19 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC) {
-  let checklineA = lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) ? true : false;
-  let checklineB = lineA < (lineB + lineC) && lineA > Math.abs(lineA - lineC) ? true : false;
-  let checklineC = lineA < (lineB + lineC) && lineA > Math.abs(lineA - lineB) ?  true : false;
-  return checklineA === true || checklineB === true || checklineC === true ? true : false;
+function triangleCheck() {
+
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(stringRecebida) {
+  let counter = 0;
+  let patt1 = /[1-9]/g;
+  let result = stringRecebida.match(patt1);
+  for (let indexSecond = 0; indexSecond < result.length; indexSecond += 1) {
+    counter += Number(result[indexSecond]);
+  }
+  return counter > 0 && counter > 1 && `${counter} copos de água`;
 }
 
 module.exports = {
