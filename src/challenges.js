590641c532dc310a986ac40110ce17cdc28db54e
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-01 19:58:41 -0300

    Implementa requisitos de 1 ao 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,78 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(boolean1, boolean2) {
+  
+  if (boolean1 === true && boolean2 === true){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+
+let area = (base * height)/2;
+
+return area;
+
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string){
+  string = string.split(' ')
+
+  return string;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayString) {
+
+  let ultimoArray = arrayString[arrayString.length - 1];
+  let primeiroArray = arrayString[0];
+  let resultadoArray = `${ultimoArray}, ${primeiroArray}`;
+  
+  return resultadoArray;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+
+function footballPoints(wins, ties) {
+
+let pontos = wins * 3 + ties;    
+
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array){ 
+let maiorValor = 0;
+let repeticao = 0; 
+
+for(let contador = 0; contador < array.length; contador+=1){
+  if(array[contador] > maiorValor) {
+    maiorValor = array[contador];
+  }
+}
+
+for(let value of array){
+  if(maiorValor === value){
+    repeticao += 1;
+  } 
+}
+  return repeticao;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+ let cat1distancia =  Math.abs(cat1 - mouse);
+ let cat2distancia =  Math.abs(cat2 - mouse);
+ 
+ if(cat1distancia > cat2distancia){
+   return 'cat2';
+ } else if (cat2distancia > cat1distancia){
+   return 'cat1';
+ }
+   return 'os gatos trobam e o rato foge';
 }
 
 // Desafio 8
