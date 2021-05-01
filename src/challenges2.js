5cf844c6af0bf148302d5f756dfecf0d07020ce0
Tulio <tuliotrindade99@gmail.com>
2021-05-01 13:01:36 -0300

    desafio 11 corrigido

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,30 +18,29 @@ let arrayOrdenado=array.sort()
 // Desafio 11
 function generatePhoneNumber(array) {
   let saida='';
+  if(array.length!=11 || array==[]){
+    saida='Array com tamanho incorreto.';
+  }
       for (let n of array) {
         let contador = 0;
           for (let c of array) {
             if (n === c) {
               contador += 1;
             }
-          }
-          if(array.length!=11){
-            saida='Array com tamanho incorreto.';
-          }
-          else if(array[n]>9 || array[n]<0 || contador>=3){
+          }          
+          if(n>9 || n<0 || contador>=3){
             saida='não é possível gerar um número de telefone com esses valores';
           }
           else{
             saida='('+array[0]+array[1]+') '+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];
           }
+     
       }
-      
-      
-
   return saida; 
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
+console.log(generatePhoneNumber([]))
 
 
 // Desafio 12
