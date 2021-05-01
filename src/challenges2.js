e8968675402defbe013d88573035531c33c269e7
Bruno Augusto <brunaugusto@gmail.com>
2021-05-01 20:14:22 -0300

    Desafio 11 Correcao2

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,41 +15,36 @@ function techList(tech, nome) {
 
 // Desafio 11
 console.log(generatePhoneNumber([1, 5, 9, 9, 1, 6, 3, 1, 4, 0, 8]));
-function countNumber(arrayTelefone,value) {
-    let counter = 0;
-    for(let index = 0; index< arrayTelefone.length; index += 1) {
-        counter += arrayTelefone[index] === value ? 1 : 0;
-    }
-    return counter;
+function countNumber(arrayTelefone , value) {
+  let counter = 0;
+  for(let index = 0; index < arrayTelefone.length; index += 1) {
+    counter += arrayTelefone[index] === value ? 1 : 0;
+  }
+  return counter;
 }
 function generatePhoneNumber (arrayTelefone) {
 
-    let phoneFormated = '';
-    let errorMessage = '';
-    let counter = 0;
+  let phoneFormated = '';
+  let errorMessage = '';
+  let counter = 0;
 
-    if(arrayTelefone.length === 11)
+  if(arrayTelefone.length === 11)
+  {
+    for(let index = 0; index < arrayTelefone.length; index+= 1)
     {
-        for(let index = 0; index < arrayTelefone.length; index+= 1)
-        {
-                counter = countNumber(arrayTelefone,arrayTelefone[index]);
-                errorMessage = arrayTelefone[index] < 0 || arrayTelefone[index] > 9 || counter > 2 ? errorMessage = 'não é possível gerar um número de telefone com esses valores' : '';
-
-                if(errorMessage.length > 0) {
-                    break;
-                } else {
-                    phoneFormated += index === 0 ? `(${arrayTelefone[index]}` : index === 1 ? `${arrayTelefone[index]}) ` : index === 6 ? `${arrayTelefone[index]}-` : `${arrayTelefone[index]}`;
-
-                }
-        }
+      counter = countNumber(arrayTelefone,arrayTelefone[index]);
+      errorMessage = arrayTelefone[index] < 0 || arrayTelefone[index] > 9 || counter > 2 ? errorMessage = 'não é possível gerar um número de telefone com esses valores' : '';
+      if(errorMessage.length > 0) {
+        break;
+      } else {
+        phoneFormated += index === 0 ? `(${arrayTelefone[index]}` : index === 1 ? `${arrayTelefone[index]}) ` : index === 6 ? `${arrayTelefone[index]}-` : `${arrayTelefone[index]}`;
+      }
     }
-    else
-    {
-        errorMessage = 'Array com tamanho incorreto.';
-    }
-
-
-    return errorMessage.length === 0 ? phoneFormated : errorMessage ;
+  }
+  else {
+    errorMessage = 'Array com tamanho incorreto.';
+  }
+  return errorMessage.length === 0 ? phoneFormated : errorMessage ;
 }
 
 // Desafio 12
