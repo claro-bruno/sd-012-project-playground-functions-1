2ebaace01fcd71406c27b51d5ecf7dbd9f95a19a
Martin <escorpmartin97@gmail.com>
2021-05-01 13:31:24 -0300

    exercise 9 e 10

--- src/challenges.js
+++ src/challenges.js
@@ -72,12 +72,34 @@ return arr;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(frase) {
+  let frasecode = '';
+  let arm1 = '';
+
+  for (let index = 0; index < frase.length; index += 1) {
+  arm1 += frase[index];
+    frasecode = arm1.replace(/a/gi,'1')
+    .replace(/e/gi,'2')
+    .replace(/i/gi,'3')
+    .replace(/o/gi,'4')
+    .replace(/u/gi,'5');
+  }
+  return frasecode;
 }
-function decode() {
-  // seu código aqui
-}
+function decode(frase) {
+    let frasedecode = '';
+    let arm = '';
+  
+    for (let index1 = 0; index1 < frase.length; index1 += 1) {
+    arm += frase[index1];
+      frasedecode = arm.replace(/1/gi,'a')
+      .replace(/2/gi,'e')
+      .replace(/3/gi,'i')
+      .replace(/4/gi,'o')
+      .replace(/5/gi,'u');
+    }
+    return frasedecode;
+  }
 
 module.exports = {
   calcArea,
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,13 +1,31 @@
-function catAndMouse(mause, cat1, cat2) {
-  let distCat1Mause = Math.abs(cat1 - mause);
-  let distCat2Mause = Math.abs(cat2 - mause);
+function encode(frase) {
+  let frasecode = '';
+  let arm1 = '';
 
-  if ((distCat2Mause) < (distCat1Mause)) {
-    return 'cat2';
-  } else if (distCat1Mause < distCat2Mause) {
-    return 'cat1';
-  } else if (distCat1Mause === distCat2Mause) {
-  return 'os gatos trombam e o rato foge' ;
+  for (let index = 0; index < frase.length; index += 1) {
+  arm1 += frase[index];
+    frasecode = arm1.replace(/a/gi,'1')
+    .replace(/e/gi,'2')
+    .replace(/i/gi,'3')
+    .replace(/o/gi,'4')
+    .replace(/u/gi,'5');
   }
- }
- console.log(catAndMouse(1, 0, 5));
+  return frasecode;
+}
+console.log(encode("hi there!"))
+
+function decode(frase) {
+    let frasedecode = '';
+    let arm = '';
+  
+    for (let index1 = 0; index1 < frase.length; index1 += 1) {
+    arm += frase[index1];
+      frasedecode = arm.replace(/1/gi,'a')
+      .replace(/2/gi,'e')
+      .replace(/3/gi,'i')
+      .replace(/4/gi,'o')
+      .replace(/5/gi,'u');
+    }
+    return frasedecode;
+  }
+  console.log(decode("h3 th2r2!"))
