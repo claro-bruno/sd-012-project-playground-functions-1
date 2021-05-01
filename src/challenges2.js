abdb317e1f7c7d6ac346d31f73a028f63b02fb54
AndZoni <lorenzoniandre@outlook.com>
2021-05-01 13:19:33 +0100

    Adiciona Requisito 12 - Crie uma função de Condição de existência de um triângulo (Depois de quebrar a cabeca)

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,9 +21,37 @@ function generatePhoneNumber() {
   // seu código aqui
 }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
+// Desafio 12 (Part1)
+function isTriangle1(lineA, lineB, lineC) {
+  let lineAB = lineA + lineB;
+  let lineAC = lineA + lineC;
+  let lineBC = lineB + lineC;
+  let result;
+  if ((lineA < lineBC) && (lineB < lineAC) && (lineC < lineAB)) {
+    result = true;
+  } if (result !== true) {
+    result = false;
+  }
+  return (result);
+}
+
+// Desafio 12 (Part2)
+function isTriangle2(lineA, lineB, lineC) {
+  let lineAB = Math.abs(lineA - lineB);
+  let lineAC = Math.abs(lineA - lineC);
+  let lineBC = Math.abs(lineB - lineC);
+  let result;
+  if ((lineA > lineBC) && (lineB > lineAC) && (lineC > lineAB)) {
+    result = true;
+  } if (result !== true) {
+    result = false;
+  }
+  return (result);
+}
+
+// Desafio 12 Final
+function triangleCheck(lineA, lineB, lineC) {
+  return isTriangle1(lineA, lineB, lineC) && isTriangle2(lineA, lineB, lineC);
 }
 
 // Desafio 13
