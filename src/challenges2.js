1625a5379a9b690f412034793788fdae9a8751e2
AndZoni <lorenzoniandre@outlook.com>
2021-05-01 18:25:04 +0100

    Adiciona Requisito 11 - Crie uma função de Número de Telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,9 +16,56 @@ function techList(theTech, theName) {
   return (objectList);
 }
 
+// Desafio 11 (Part1)
+function noNumber(pNumber) {
+  if (pNumber.length > 11) {
+    return (true);
+  }
+}
+
+function wrongNumber(pNumber) {
+  for (let index = 0; index < pNumber.length; index += 1) {
+    if ((pNumber[index] > 9) || (pNumber[index] < 0)) {
+      return (true);
+    }
+  }
+}
+
+function countTriplicate1(pNumber) {
+// https://dev.to/huyddo/find-duplicate-or-repeat-elements-in-js-array-3cl3
+  let countNumber = {};
+  for (let index = 0; index < pNumber.length; index += 1) {
+    if (countNumber[pNumber[index]]) {
+      countNumber[pNumber[index]] += 1;
+    } else {
+      countNumber[pNumber[index]] = 1;
+    } for (let xTimes in countNumber) {
+      if (countNumber[xTimes] >= 3) {
+        return (true);
+      }
+    }
+  }
+}
+
+function formatNumber(pNumber) {
+  // Quebrei a cabeca tentand achar um comando que desse certo no lint.
+  // Olhei como o colega fez: https://github.com/tryber/sd-012-project-playground-functions/blob/eric-kreis-playground-functions/src/challenges2.js
+  // Entendi a logica e apliquei no meu requisito.
+  let firstPart = `${pNumber[0]}${pNumber[1]}`;
+  let secondPart = `${pNumber[2]}${pNumber[3]}${pNumber[4]}${pNumber[5]}${pNumber[6]}`;
+  let tirdPart = `${pNumber[7]}${pNumber[8]}${pNumber[9]}${pNumber[10]}`;
+  let finalNumber = `(${firstPart}) ${secondPart}-${tirdPart}`;
+  return (finalNumber);
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(pNumber) {
+  if (noNumber(pNumber) === true) {
+    return ('Array com tamanho incorreto.');
+  } if ((wrongNumber(pNumber)) || countTriplicate1(pNumber) === true) {
+    return ('não é possível gerar um número de telefone com esses valores');
+  }
+  return (formatNumber(pNumber));
 }
 
 // Desafio 12 (Part1)
