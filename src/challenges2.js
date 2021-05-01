ec08faf1f0f0c0f01d4a0af338f15ae734a2e2c7
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-01 14:30:23 -0300

    Req 11 function generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,12 +17,30 @@ function techList(tech, name) {
   return objLista;
   };
 };
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Artur"));
+// console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Artur"));
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phoneNumber) {
   // seu código aqui
-}
+  let result = 0;
+  let stringFinal = '';
+  for (key1 in phoneNumber) {
+      result = 0;
+      stringFinal = stringFinal.concat(phoneNumber[key1]);
+      if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
+      if (phoneNumber[key1] < 0 || phoneNumber[key1] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+      }
+      for (key2 in phoneNumber) {
+      if (phoneNumber[key1] === phoneNumber[key2]) {
+          result += 1;
+      }
+      if (result >= 3) return 'não é possível gerar um número de telefone com esses valores';
+      };
+    };
+    return stringFinal.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+};
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
