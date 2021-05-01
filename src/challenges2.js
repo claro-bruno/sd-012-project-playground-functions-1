5e84ddb433bf13923c8ec22fee903490b9eddbc7
Bruno Augusto <brunaugusto@gmail.com>
2021-05-01 03:57:27 -0300

    Correçao Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,8 +3,11 @@ function techList(tech, nome) {
   let objTech = [];
   if (tech.length > 0) {
     tech.sort();
-    for (let index in tech) {
-      objTech.push({ tech: tech[index], name: nome });
+    for (let key of tech) {
+      objTech.push({
+        tech: key,
+        name: nome,
+      });
     }
   }
   return objTech.length > 0 ? objTech : 'Vazio!';
