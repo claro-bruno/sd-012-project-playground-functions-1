d2cce99f00ba267c4cdf8c5f9877bc0748153a8c
Eric Faria <ericdeofaria@gmail.com>
2021-05-01 19:39:03 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,35 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let count = 0;
+  let number = '(';
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > 9 || array[i] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    count = 0;
+    for (let j = 1; j < array.length; j += 1) {
+      if (array[j] === array[i]) {
+        count += 1;
+        if (count >= 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+  }
+  for (let k = 0; k < array.length; k += 1) {
+    if (k === 2) {
+      number += ') ';
+    } else if (k === 7) {
+      number += '-';
+    }
+    number += array[k];
+  }
+  return number;
 }
 
 // Desafio 12
