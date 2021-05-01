1136baec49dd75763f32e9d7fda14a71ebafc743
Bruno Augusto <brunaugusto@gmail.com>
2021-05-01 00:07:16 -0300

    Correcao Desafio 7_5

--- src/challenges.js
+++ src/challenges.js
@@ -54,15 +54,13 @@ function catAndMouse(mouse, cat1, cat2) {
   if (rgC1M > rgC2M) {
     ret = 'cat2';
   }
-  else
-  {
-    if (rgC2M > rgC1M) {
+  else if (rgC2M > rgC1M) {
       ret = 'cat1';
     } else
     {
       ret = 'os gatos trombam e o rato foge';
     }
-  }
+
   return ret;
 }
 
