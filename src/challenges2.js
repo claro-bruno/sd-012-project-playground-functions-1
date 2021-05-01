218a44a6512bab5fda93891d42851cabbdebc633
David Azevedo <zevid.dev@outlook.com>
2021-05-01 07:34:49 -0300

    Done requirement 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,9 +18,29 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  let repeatNumber = 0;
+  if(phoneNumber.length > 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let verifiedNumber = [];
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    verifiedNumber = phoneNumber[index];
+    for (let index2 = 0; index2 < phoneNumber.length; index2 += 1) {
+      if(phoneNumber[index2] === verifiedNumber) {
+        repeatNumber += 1
+      } 
+      if (repeatNumber >= 3 || verifiedNumber < 0 || verifiedNumber > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    repeatNumber = 0;
+  }
+  let formatedNumber = phoneNumber.join('')
+  formatedNumber = formatedNumber.replace(/(\d{2})?(\d{5})?(\d{4})/, "($1) $2-$3")
+  return formatedNumber;
 }
+
 
 // Desafio 12
 function triangleCheck() {
