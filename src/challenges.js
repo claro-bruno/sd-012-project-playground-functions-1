b05fcb4b2352d81c0692985e1dfcf50cc425b2f2
Abner <gedeaoabner@gmail.com>
2021-05-02 20:24:12 -0300

    adiciona algumas correções sugeridas pelo eslint

--- src/challenges.js
+++ src/challenges.js
@@ -13,7 +13,6 @@ console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, altura) {
-  let resposta = 0;
   let area = (base * altura) / 2;
   return area;
 }
@@ -22,19 +21,19 @@ console.log(calcArea(5, 2));
 // Desafio 3
 function splitSentence(string) {
   let array = [];
-  array = string.split(" "); // separa a string de acordo com o caracter entre "aspas"
+  array = string.split(' '); // separa a string de acordo com o caracter entre "aspas"
   return array;
 }
-console.log(splitSentence("Vamo que Vamo"));
+console.log(splitSentence('Vamo que Vamo'));
 
 // Desafio 4
 function concatName(array) {
   let primeiroNome = array[0];
   let ultimoNome = array[array.length - 1];
-  let primeiroUltimo = ultimoNome + ", " + primeiroNome;
+  let primeiroUltimo = (ultimoNome + ', ' + primeiroNome);
   return primeiroUltimo;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -69,11 +68,11 @@ function catAndMouse(mouse, cat1, cat2) {
   console.log(cat1, cat2);
 
   if (cat1 < cat2) {
-    return "cat1";
+    return 'cat1';
   } else if (cat2 < cat1) {
-    return "cat2";
+    return 'cat2';
   } else {
-    return "os gatos trombam e o rato foge";
+    return 'os gatos trombam e o rato foge';
   }
 }
 console.log(catAndMouse(7, 5, 8));
@@ -83,13 +82,13 @@ function fizzBuzz(listaInteiros) {
   let arrayFizzBuzz = [];
   for (let index in listaInteiros) {
     if (((listaInteiros[index] % 3) === 0) && ((listaInteiros[index] % 5) === 0)) {
-      arrayFizzBuzz.push("fizzBuzz");
+      arrayFizzBuzz.push('fizzBuzz');
     } else if ((listaInteiros[index] % 3) === 0) {
-      arrayFizzBuzz.push("fizz");
+      arrayFizzBuzz.push('fizz');
     } else if ((listaInteiros[index] % 5) === 0) {
-      arrayFizzBuzz.push("buzz");
+      arrayFizzBuzz.push('buzz');
     } else {
-      arrayFizzBuzz.push("bug!");
+      arrayFizzBuzz.push('bug!');
     }
   };
   return arrayFizzBuzz;
@@ -98,47 +97,47 @@ console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode(string) {
-  let separado = string.split("");
-  //console.log(separado);
+  let separado = string.split('');
+  // console.log(separado);
   for (let index in string) {
-    if (string[index] === "a") {
-      separado[index] = "1";
-    } else if (string[index] === "e") {
-      separado[index] = "2";
-    } else if (string[index] === "i") {
-      separado[index] = "3";
-    } else if (string[index] === "o") {
-      separado[index] = "4";
-    } else if (string[index] === "u") {
-      separado[index] = "5";
+    if (string[index] === 'a') {
+      separado[index] = '1';
+    } else if (string[index] === 'e') {
+      separado[index] = '2';
+    } else if (string[index] === 'i') {
+      separado[index] = '3';
+    } else if (string[index] === 'o') {
+      separado[index] = '4';
+    } else if (string[index] === 'u') {
+      separado[index] = '5';
     };
   };
   separado = separado.join("");
-  //console.log(separado);
+  // console.log(separado);
   return separado;
 }
 
 function decode(string) {
-  let separado = string.split("");
+  let separado = string.split('');
   for (let index in string) {
-    if (string[index] === "1") {
-      separado[index] = "a";
-    } else if (string[index] === "2") {
-      separado[index] = "e";
-    } else if (string[index] === "3") {
-      separado[index] = "i";
-    } else if (string[index] === "4") {
-      separado[index] = "o";
-    } else if (string[index] === "5") {
-      separado[index] = "u";
+    if (string[index] === '1') {
+      separado[index] = 'a';
+    } else if (string[index] === '2') {
+      separado[index] = 'e';
+    } else if (string[index] === '3') {
+      separado[index] = 'i'
+    } else if (string[index] === '4') {
+      separado[index] = 'o';
+    } else if (string[index] === '5') {
+      separado[index] = 'u';
     };
   };
-  separado = separado.join("");
+  separado = separado.join('');
   return separado;
-} 
+}
 
-console.log(encode("hi there!"));
-console.log(decode("h3 th2r2!"));
+console.log(encode('hi there!'));
+console.log(decode('h3 th2r2!'));
 
 module.exports = {
   calcArea,
