af9626c27a4900f0fdb5f825da8530ee2590a99c
Eder Santos <3derms@gmail.com>
2021-05-02 19:14:53 -0300

    Requisito 10 - for nao trava maquina com essa funcao

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,54 +1,18 @@
 // Desafio 10
 function techList(name, array7) {
   novaArray = [];
-  // Todas as vezes que usei o for nessa função o computador travou
-  //for (let chave in array7){
-  //for (indiceDaArray7 = 0; indiceDaArray7 < array7.length; indiceDaArray7 =+ 1 ) {
+ 
+  for (indiceDaArray7 = 0; indiceDaArray7 < array7.length; indiceDaArray7 =+ 1 ) {
     saidaFuncao = {
-      tech: array7[0], 
+      tech: array7[indiceDaArray7], 
       nome: name
 
 
     };
     novaArray.push(saidaFuncao);
 
-    saidaFuncao = {
-      tech: array7[1], 
-      nome: name
 
-
-    };
-    novaArray.push(saidaFuncao);
-
-    saidaFuncao = {
-      tech: array7[2], 
-      nome: name
-
-
-    };
-    novaArray.push(saidaFuncao);
-
-    saidaFuncao = {
-      tech: array7[3], 
-      nome: name
-
-
-    };
-    novaArray.push(saidaFuncao);
-
-    novaArray.push(saidaFuncao);
-
-    saidaFuncao = {
-      tech: array7[4], 
-      nome: name
-
-
-    };
-    novaArray.push(saidaFuncao);
-  //}
-  
-  //for (let chave in novaArray){
-  //}
+  }
   return novaArray;
 }
 
