32dac96e9f5960d651bfb19ce9e6d5681fd757df
Eurides <euridesalves@yahoo.com.br>
2021-05-02 08:21:55 -0300

    retomada apos restauracao original e inclusao exec

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,118 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(par1, par2) {
+  return par1 && par2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return ((base * height) / 2);
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let resposta = [];
+  resposta = frase.split(' ');
+  return resposta;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(matriz) {
+  let resposta = matriz[matriz.length - 1];
+  resposta += ', ';
+  resposta += matriz[0];
+  return resposta;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties);
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 === cat2) {
+    return 'os gatos trombam e o rato foge';
+  }
+  if ((cat1 - mouse) < (cat2 - mouse)) {
+    return 'cat1';
+  }
+  return 'cat2';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(matriz) {
+  let resposta = [];
+  for (let index = 0; index < matriz.length; index += 1) {
+    if (matriz[index] % 3 === 0 && matriz[index] % 5 === 0) { 
+      resposta.push('fizzBuzz');
+    } else if (matriz[index] % 5 === 0) {
+      resposta.push('buzz');
+    } else if (matriz[index] % 3 === 0) {
+      resposta.push('fizz');
+    } else {      
+      resposta.push('bug!');
+    }
+  }
+  return resposta;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(variavel) {
+  let caracter = '';
+  let resposta = '';
+  for (let index = 0; index < variavel.length; index += 1) {
+    switch (variavel.substr(index,1)) {
+      case 'a':
+        caracter = '1';
+        break;
+      case 'e':
+        caracter = '2';
+        break;
+      case 'i':
+        caracter = '3';
+        break;
+      case 'o':
+        caracter = '4';
+        break;
+      case 'u':
+        caracter = '5';
+        break;
+      default:
+        caracter = variavel.substr(index,1);
+    }
+    resposta += caracter;
+  }
 }
-function decode() {
-  // seu código aqui
+
+function decode(variavel) {
+  let caracter = '';
+  let resposta = '';
+  for (let index = 0; index < variavel.length; index += 1) {
+    switch (variavel.substr(index,1)) {
+      case 'a':
+        caracter = '1';
+        break;
+      case 'e':
+        caracter = '2';
+        break;
+      case 'i':
+        caracter = '3';
+        break;
+      case 'o':
+        caracter = '4';
+        break;
+      case 'u':
+        caracter = '5';
+        break;
+      default:
+        caracter = variavel.substr(index,1);
+    }
+    resposta += caracter;
+  }
 }
 
 module.exports = {
