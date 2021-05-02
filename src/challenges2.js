494ff03e903fa22fbbd8f352ff3e560a3e754fcb
camila-mp <ca.malvp@gmail.com>
2021-05-02 14:56:34 -0300

    tentativa de corrigir desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,28 +4,28 @@ function techList(arrTech, nome1) {
     return 'Vazio!'}
 
   let arrTechSorted = arrTech.sort();
-  let arrObjTech = []; 
+  let arrObTech = []; 
   for (let index = 0; index < arrTechSorted.length; index += 1) { 
-    let objTech = {};
-    objTech = {
+    let objTech = {
       tech: arrTechSorted[index],
       nome: nome1,
       };
-    arrObjTech.push(objTech);  
-    }
-  return arrObjTech;
+    arrObTech.push(objTech);  
+  }
+  return arrObTech;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 // Desafio 11
 function generatePhoneNumber(numeros) {
-
-  let doisPrimeirosNum = '(' + `${numeros[0]}` + `${numeros[1] }`+ ')';
-  let noveNum = ''
+  let doisPrimeirosNum = '(' + `${numeros[0]}` + `${numeros[1] }`+ ')' + ' ';
+  let noveNum = '';
 
   for(let index = 2; index < numeros.length; index += 1){
-      noveNum += `${numeros[index]}`;
-    }
-  let numTelefone = doisPrimeirosNum + ' ' + noveNum;
-  return numTelefone;
+    noveNum += `${numeros[index]}`;
+  }
+  
+  let numTelefone = doisPrimeirosNum + noveNum.slice(0, 5) + '-' + noveNum.slice(5, 9);
+  return numTelefone
 }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12
