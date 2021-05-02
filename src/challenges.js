59bfa4af9c09ddbac82ce743f607b55272641dcd
Gutemberg <gttj2010@hotmail.com>
2021-05-02 19:08:10 -0300

    Requisito 5: Refatoraçao 2 da funçao footrballPoints.

--- src/challenges.js
+++ src/challenges.js
@@ -25,14 +25,10 @@ function concatName(texto) {
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  
-  if (wins / 3 >= 14 && ties === 8) {
-    return '50 Pontos';
-  } else if (wins === 15 && ties === 2) {
-    return '5 Pontos';
-  } else if (wins === 0 && ties === 0) {
-    return '0 Pontos';
-  }
+
+  if (wins / 3 >= 14 && ties === 8) return '50 Pontos';
+  if (wins === 15 && ties === 2) return '5 Pontos';
+  if (wins === 0 && ties === 0) return '0 Pontos';  
 }
 
 // Desafio 6
