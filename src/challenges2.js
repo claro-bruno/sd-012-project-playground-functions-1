b7a66393363a62fc42b23df1f6bcdedfab3b2f00
Eder Santos <3derms@gmail.com>
2021-05-02 18:37:57 -0300

    resolve requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,60 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(name, array7) {
+  novaArray = [];
+  // Todas as vezes que usei o for nessa função o computador travou
+  //for (let chave in array7){
+  //for (indiceDaArray7 = 0; indiceDaArray7 < array7.length; indiceDaArray7 =+ 1 ) {
+    saidaFuncao = {
+      tech: array7[0], 
+      nome: name
+
+
+    };
+    novaArray.push(saidaFuncao);
+
+    saidaFuncao = {
+      tech: array7[1], 
+      nome: name
+
+
+    };
+    novaArray.push(saidaFuncao);
+
+    saidaFuncao = {
+      tech: array7[2], 
+      nome: name
+
+
+    };
+    novaArray.push(saidaFuncao);
+
+    saidaFuncao = {
+      tech: array7[3], 
+      nome: name
+
+
+    };
+    novaArray.push(saidaFuncao);
+
+    novaArray.push(saidaFuncao);
+
+    saidaFuncao = {
+      tech: array7[4], 
+      nome: name
+
+
+    };
+    novaArray.push(saidaFuncao);
+  //}
+  
+  //for (let chave in novaArray){
+  //}
+  return novaArray;
 }
+
+//techList("Eder", ["React", "Jest", "HTML", "CSS", "JavaScript"]) 
+
+//console.log(novaArray);
 
 // Desafio 11
 function generatePhoneNumber() {
