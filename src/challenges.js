84c7ccef27f9396ea0265429320fa5268b4dbfde
Live session user <gabrielaguerra.arq@gmail.com>
2021-05-02 14:42:04 -0300

    adiciona desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -7,13 +7,10 @@ function compareTrue(a, b) {
 function calcArea(base, height){
   return base * height /2;
 };
-  console.log(calcArea(10, 50));
-  console.log(calcArea(5, 2));
-  console.log(calcArea (51, 1));
-
+ 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(foguete) {
+  return foguete.split(" ");
 }
 
 // Desafio 4
