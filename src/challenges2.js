823e4809f00ec9b5cdbbb904b9cda788090a532d
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-02 15:33:03 -0300

    Resolve desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,8 +5,8 @@ function techList(arrayOfTech, names) {
     for (let index = 0; index < arrayOfTech.length; index += 1) {
       newArrayTech.push({
         tech: arrayOfTech[index],
-        name: names
-      })
+        name: names,
+      });
     }
     newArrayTech.sort(function (a, b) {
       if (a.tech > b.tech) {
@@ -15,29 +15,46 @@ function techList(arrayOfTech, names) {
         return -1;
       } else {
         return 0;
-      };
+      }
     });
-    return newArrayTech
+    return newArrayTech;
   } else if (arrayOfTech === undefined) {
-    return 'Vazio!'
+    return 'Vazio!';
   }
 }
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
-// Desafio 11 FALTA arrumar < 0 || > 9 / se repita 3 vezes ou mais / arrumar identacao do telefone
+// Desafio 11 
 function generatePhoneNumber(arrayOfNumber) {
-  let phoneNumber = [];
+  let phoneNumber;
+  let isPossible = true;
   for (let index = 0; index < arrayOfNumber.length; index += 1) {
-    if (arrayOfNumber[index] < 0 || arrayOfNumber[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    } else if (arrayOfNumber.length > 11) {
-      return 'Array com tamanho incorreto.';
-    // } else if (arrayOfNumber.indexOf() === -1) {
-    //   return 'não é possível gerar um número de telefone com esses valores';
-    } else {
-      phoneNumber = arrayOfNumber;
+      if (arrayOfNumber[index] < 0 || arrayOfNumber[index] > 9) {
+          isPossible = false;
+      }
+  }
+  for (let number = 0; number < 10; number += 1) {
+      let contador = 0;
+      for (let index = 0; index < arrayOfNumber.length; index += 1) {
+          if (number === arrayOfNumber[index]) {
+              contador += 1;
+          }
+      }  
+      if (contador > 2) {
+          isPossible = false;
+      }
+  }
+  if (arrayOfNumber.length !== 11) {
+      console.log('Array com tamanho incorreto');
+  } else if (isPossible === false) {
+      console.log('não é possível gerar um número de telefone com esses valores');
+  } else {
+      let stringPhone = arrayOfNumber.join('');
+      let part1 = stringPhone.slice(0, 2);
+      let part2 = stringPhone.slice(2, 7);
+      let part3 = stringPhone.slice(7);
+      phoneNumber = `(${part1}) ${part2}-${part3}`;
       return phoneNumber;
-    }
   }
 }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
