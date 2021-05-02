4767266d95844832a8d9495d24c9cbae9fc1d75d
camila-mp <ca.malvp@gmail.com>
2021-05-01 21:12:52 -0300

    iniciando exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrTech, nome) {
+
+  if (arrTech === []) {
+    return 'Vazio!'
+  } else {
+    let arrTechSorted = arrTech.sort();
+    const objTech = {};
+
+    for (let tech of arrTechSorted){
+      objTech['tech'] = tech;
+      objTech['nome'] = nome;
+    }
+    return objTech  
+  }
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
