fc924608acbd965d5ba572241e9b24c89479d5d0
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-02 15:48:08 -0300

    Resolve desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,7 +22,7 @@ function techList(arrayOfTech, names) {
     return 'Vazio!';
   }
 }
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+// console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11 
 function generatePhoneNumber(arrayOfNumber) {
@@ -57,7 +57,7 @@ function generatePhoneNumber(arrayOfNumber) {
     return phoneNumber;
   }
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
