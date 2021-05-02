ae409520e0c5034db3cd0f9c1984618d26ec7bc9
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-02 18:58:28 -0300

    ajustes de erros desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,68 +1,71 @@
 // Desafio 1
 function compareTrue(booleano1, booleano2) {
-  if (booleano1 === true && booleano2 === true) {
-    return true;
-  } else {
-    return false;
-  }
+	let compare = true;
+	if (booleano1 === true && booleano2 === true) {
+		compare = true;
+  	} else {
+    	compare = false;
+  	}
+  	return compare;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height) / 2;
-  return area;
+	let area = (base * height) / 2;
+  	return area;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  let splitString = string.split(' ');
-  return splitString;
+	let splitString = string.split(' ');
+  	return splitString;
 }
 
 // Desafio 4
 function concatName(arrayString) {
-  let first = arrayString[arrayString.length - 1];
-  let second = arrayString[0];
-  let concatString = (first + ', ' + second);
-  return concatString;
+	let first = arrayString[arrayString.length - 1];
+	let second = arrayString[0];
+	let concatString = first + ', ' + second;
+	return concatString;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let pointsWins = 3 * wins;
-  let pointsTies = 1 * ties;
-  let points = pointsWins + pointsTies;
-  return points;
+	let pointsWins = 3 * wins;
+	let pointsTies = 1 * ties;
+	let points = pointsWins + pointsTies;
+	return points;
 }
 
 // Desafio 6
 function highestCount(arrayNumbers) {
-  let maior = 0;
-  let contaNum = 0;
-  for (let index = 0; index < arrayNumbers.length; index += 1) {
-    if (arrayNumbers[index] > maior) {
-      maior = arrayNumbers[index];
-    }
-  for (let numero in arrayNumbers) {
-    if (arrayNumbers[numero] === maior) {
-      contaNum += 1;
-    }
-  }
-  }
-  return contaNum;
+	let maior = 0;
+	let contaNum = 0;
+	for (let index = 0; index < arrayNumbers.length; index += 1) {
+    	if (arrayNumbers[index] > maior) {
+      	maior = arrayNumbers[index];
+    	}
+	}
+  	for (let numero in arrayNumbers) {
+    	if (arrayNumbers[numero] === maior) {
+      	contaNum += 1;
+    	}
+  	}
+  	return contaNum;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanciaCat1 = cat1 - mouse;
-  let distanciaCat2 = cat2 - mouse;
-  if(distanciaCat1 === distanciaCat2) {
-    return 'os gatos trombam e o rato foge';
-  } if(distanciaCat1 < distanciaCat2) {
-    return 'cat1';
-  } else {
-    return 'cat2';
-  }
+	let distanciaCat1 = Math.abs(cat1 - mouse);
+	let distanciaCat2 = Math.abs(cat2 - mouse);
+	if (distanciaCat1 === distanciaCat2) {
+		return 'os gatos trombam e o rato foge';
+	}
+	if (distanciaCat1 < distanciaCat2) {
+		return 'cat1';
+	} else {
+		return 'cat2';
+	}
 }
 
 // Desafio 8
@@ -74,6 +77,7 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
@@ -89,4 +93,4 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
-}
+};
