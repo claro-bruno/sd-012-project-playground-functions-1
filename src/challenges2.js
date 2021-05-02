df8907da96f2ffe0849efddc057428daa2644e07
Eurides <euridesalves@yahoo.com.br>
2021-05-02 12:00:00 -0300

    Adicionado desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,14 +4,51 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numero) {
+  let numerotel = '(';
+  if (numero.length != 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+     let matrizAux = numero.sort();
+     let repetiu = 0;
+     for (posicao = 0; posicao < matrizAux.length; posicao += 1) {
+       for (let index = 0; index < numero.length; index += 1) {
+         if (numero[index] < 0 || numero[index] > 9 ) {
+           return 'não é possível gerar um número de telefone com esses valores';
+         } else
+            if (matrizAux[posicao] === numero[index]) {
+               repetiu += 1
+            }
+           if (repetiu >= 3 ) {
+              return 'não é possível gerar um número de telefone com esses valores';
+           }
+        }
+      }
+      for (let index = 0; index < numero.length; index += 1) {
+        if (index = 2) {
+          numerotel += ')';
+        } 
+        numerotel += numero[index];
+        if (index = 7) {
+          numerotel += '-';
+        } 
+        numerotel += numero[index];
+      }
+    }
+  return numerotel;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let resposta = false;
+  if (linea > (lineb + linec)) {
+    resposta = true;
+  }
+
+   /* é necessário que a medida de qualquer um dos lados seja 
+   menor que a soma das medidas dos outros dois e 
+   maior que o valor absoluto da diferença entre essas medidas.
+} */
 
 // Desafio 13
 function hydrate() {
