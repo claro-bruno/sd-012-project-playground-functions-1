764143a055858e2f6ccb6a7cfcd051ff8694e3ad
Martin <escorpmartin97@gmail.com>
2021-05-02 11:33:56 -0300

    corrige retorno do else exercise 10

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -2,13 +2,14 @@ function techList(nomeTech, name) {
 let code = []; 
 nomeTech.sort();
 
-if (nomeTech.length > 4) {
-  for (let index in nomeTech){
-    code.push ({ tech: nomeTech[index], nome: name})
-  }
-  return code;
-} else {
-    return ('Vazio');
+  if (nomeTech.length > 4) {
+    for (let index in nomeTech){
+    code.push ({ tech: nomeTech[index],
+    name: name});
+    }
+      return code;
+  } else {
+      return ('Vazio!');
   }
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Martin'))
+console.log(techList([], "Lucas"))
