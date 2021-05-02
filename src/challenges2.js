ef76102b6f798525481f7a8060d8ee81b57abe44
Juliane Marques <mqs.juliane@gmail.com>
2021-05-02 15:42:14 -0300

    testando desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,55 +1,12 @@
 // Desafio 10
-function techList(arrayNomeTech, name) {
-  if (arrayNomeTech !== '[]') {
-    return 'Vazio!';
-  }
-
-  let lista = [];
-   for (let key in arrayNomeTech) {
-      lista.push(
-      {
-        tech: arrayNomeTech[key],
-        name: name
-      } 
-      )
-   }
-   
-  lista.sort(function(a,b) {
-    return a.tech < b.tech ? -1 : a.tech > b.tech ? 1 : 0;
-});
-
-  return lista;
+function techList() {
+  // seu código aqui
+}
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
-  let format = "(xx) xxxxx-xxxx";
-  
-  if (numbers.length !== 11 ) {
-    return "Array com tamanho incorreto.";
-  }  
-  for ( let key in numbers) {
-    if (numbers[key] < 0 || numbers[key] > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
-    }  
-  }
-  let count = 0;
-  for (let key in numbers) {
-    for (let key2 in numbers) {
-      if (numbers[key] === numbers[key2]) {
-        count += 1;
-      }
-    }
-    if (count > 2) {
-      return "não é possível gerar um número de telefone com esses valores";
-    }
-    count = 0;
-  }
-  for(let index = 0; index < numbers.length; index += 1) {
-    format = format.replace('x', numbers[index]);
-  }
-
-  return format;
-} 
+function generatePhoneNumber() {
+  // seu código aqui
+}
 
 // Desafio 12
 function triangleCheck() {
@@ -57,8 +14,16 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(bebida) {
+  let regex = /\d+/g; 
+  let result = bebida.match(regex);
+  let sum = 0;
+
+  for (let index = 0; index < result.length; index += 1 ) {
+    sum = sum + parseInt(result[index]);
+  }
+
+    return `${'Beba'} ${sum}` + ' copos de água';
 }
 
 module.exports = {
