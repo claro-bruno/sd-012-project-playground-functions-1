e22a80be3ccc539fb978c0009dc8f6e59cae738f
Juliane Marques <mqs.juliane@gmail.com>
2021-05-02 12:10:15 -0300

    testando ex13

--- src/challenges2.js
+++ src/challenges2.js
@@ -66,9 +66,19 @@ function triangleCheck(lineA, lineB, lineC) {
 } 
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+function hydrate(bebida) {
+  let regex = /\d+/g; 
+  let result = bebida.match(regex);
+  let sum = 0;
+
+  for (let index = 0; index < result.length; index += 1 ) {
+    sum = sum + parseInt(result[index]);
+  }
+
+    return [sum + " copos de água"];
+} 
+
+console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
 
 module.exports = {
   generatePhoneNumber,
