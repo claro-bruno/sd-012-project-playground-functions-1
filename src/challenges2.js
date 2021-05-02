74eb5fb8c8537faa36913062f02842c346ae2a43
Juliane Marques <mqs.juliane@gmail.com>
2021-05-02 10:35:57 -0300

    testando ex11

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,13 +25,34 @@ function techList(arrayNomeTech, name) {
 function generatePhoneNumber(numbers){
   let format = "(xx) xxxxx-xxxx";
   
-  for(let index = 0; index < numbers.length; index += 1)
-  {
+  if (numbers.length !== 11 ) {
+    return "Array com tamanho incorreto.";
+  }  
+  for ( let key in numbers) {
+    if (numbers[key] < 0 || numbers[key] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }  
+  }
+  let count = 0;
+  for (let key in numbers) {
+    for (let key2 in numbers) {
+      if (numbers[key] === numbers[key2]) {
+        count += 1;
+      }
+    }
+    if (count > 2) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    count = 0;
+  }
+  for(let index = 0; index < numbers.length; index += 1) {
     format = format.replace('x', numbers[index]);
   }
-  
+
   return format;
-}
+} 
+
+console.log(generatePhoneNumber([1, 2, 3, 3, 4, 3, 6, 7, 8, 0, 1]));
 
 
 // Desafio 12
