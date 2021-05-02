d5f15ef6033936307da978ed7342a3f7e06cc634
Live session user <gabrielaguerra.arq@gmail.com>
2021-05-02 15:16:38 -0300

    adiciona desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -19,10 +19,9 @@ function concatName(Array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties * 1;
 }
-
 // Desafio 6
 function highestCount() {
   // seu código aqui
