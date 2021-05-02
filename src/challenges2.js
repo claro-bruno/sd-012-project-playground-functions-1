1ce46b4d86aed4d0f8daa86f4fd9c5db7b1cd1db
Waltton Coelho <60485084+walttoncoelho@users.noreply.github.com>
2021-05-02 13:38:55 -0300

    Corrige  a questao 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,7 +15,7 @@ function techList(stack, person) {
   }
 }
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
-//console.log(techList([]));
+
 
 // Desafio 11
 function generatePhoneNumber(phone) {
