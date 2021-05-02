3843ace6a7f873c4169314dae320329ec4559109
cristianocsl <cslcristiano@gmail.com>
2021-05-02 14:00:53 -0300

    tentativa desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -66,9 +66,9 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(arrayOfNumber) {
   for (let index = 0; index < arrayOfNumber.length; index += 1) {
-  if (arrayOfNumber[index] % 3 === 0) {
+  if (arrayOfNumber[index] % 3 === 0 && arrayOfNumber[index] % 5 !== 0) {
     arrayOfNumber[index] = "fizz";
-  } else if (arrayOfNumber[index] % 5 === 0) {
+  } else if (arrayOfNumber[index] % 3 !== 0 && arrayOfNumber[index] % 5 === 0) {
     arrayOfNumber[index] = "buzz";
   } else if (arrayOfNumber[index] % 3 === 0 && arrayOfNumber[index] % 5 === 0) {
     arrayOfNumber[index] = "fizzBuzz";
