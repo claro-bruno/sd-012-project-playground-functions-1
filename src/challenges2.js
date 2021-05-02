cdf9e48a866e0def466a3e9fd383eeebd065a572
julio11lp <julio11lp@hotmail.com>
2021-05-02 19:36:29 -0300

    Criterio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,31 @@ function techList(tecnologia, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(telefone) {
   // seu código aqui
+  if (telefone.length > 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  let repeatNumber = telefone;
+
+  for (let index = 0; index < telefone.length; index += 1) {
+    let contador = 0;
+    for (let index2 = 0; index2 < repeatNumber.length; index2 += 1) {
+      if (telefone[index] === repeatNumber [index2]){
+        contador+= 1;
+      }
+      if (contador >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    if (telefone[index] < 0 || telefone[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let outputNumber = '(' + repeatNumber[0] + repeatNumber[1] + ')' + ' ' + repeatNumber [2] + repeatNumber [3] + repeatNumber [4] + repeatNumber [5] + '-' +  repeatNumber [6] + repeatNumber[7] + repeatNumber[8] + repeatNumber[9] + repeatNumber[10];
+
+  return outputNumber;
 }
 
 // Desafio 12
