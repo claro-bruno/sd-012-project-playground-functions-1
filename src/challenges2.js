6f416579b8e9a200fc45c8c125da10a15f8a2f0e
Martin <escorpmartin97@gmail.com>
2021-05-02 13:31:47 -0300

    exercise 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,9 +15,25 @@ nomeTech.sort();
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numero) {
+
+
+  if(numero.length < 11 || numero.length > 11){
+    return "Array com tamanho incorreto.";
+  }
+  for (let index = 0; index < numero.length; index += 1){
+    if (numero[index] < 0 || numero[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+  let pC = [];
+  for (let index1 = 0; index1 < numero.length ; index1 += 1){
+    pC.push(numero[index1]);
+  }
+    verifica = ( '(' + pC[0] + pC[1] + ')' + ' ' + pC[2] + pC[3] + pC[4] + pC[5] + pC[6] + '-' + pC[7] + pC[8] + pC[9] + pC[10]);
+    return verifica;
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
