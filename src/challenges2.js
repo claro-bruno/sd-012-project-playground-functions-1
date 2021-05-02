897eba3eb289419a4bd3b8c613cbd4abd5ed5314
Juliane Marques <mqs.juliane@gmail.com>
2021-05-02 15:25:42 -0300

    enviando desafio 10 pra revisao pela 3 vez pq esqueci de completar o desafio kkk

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,24 @@
 // Desafio 10
 function techList(arrayNomeTech, name) {
   if (arrayNomeTech.length === 0) {
-    return 'Vazio!';
+    return 'Vazio!;
   }
+
+  let lista = [];
+   for (let key in arrayNomeTech) {
+      lista.push(
+      {
+        tech: arrayNomeTech[key],
+        name: name
+      } 
+      )
+   }
+   
+  lista.sort(function(a,b) {
+    return a.tech < b.tech ? -1 : a.tech > b.tech ? 1 : 0;
+});
+
+  return lista;
 
 // Desafio 11
 function generatePhoneNumber() {
