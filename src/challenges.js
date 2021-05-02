2fa086e7e566730528b81c657c41bc32f909a689
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-02 18:12:29 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -23,7 +23,7 @@ function splitSentence(string) {
 function concatName(arrayString) {
   let first = arrayString[arrayString.length - 1];
   let second = arrayString[0];
-  let concatString = first + ', ' + second;
+  let concatString = first + ", " + second;
   return concatString;
 }
 
@@ -39,13 +39,13 @@ function footballPoints(wins, ties) {
 function highestCount(arrayNumbers) {
   let maior = 0;
   let contaNum = 0;
-  for (let index = 0; index < arrayNumbers.length; index++) {
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
     if (arrayNumbers[index] > maior) {
       maior = arrayNumbers[index];
     }
   }
   for (let numero in arrayNumbers) {
-    if (arrayNumbers[numero] == maior) {
+    if (arrayNumbers[numero] === maior) {
       contaNum += 1;
     }
   }
@@ -53,8 +53,16 @@ function highestCount(arrayNumbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = mouse - cat1;
+  let distanciaCat2 = mouse - cat2;
+  if (distanciaCat1 == distanciaCat2) {
+    return "os gatos trombam e o rato foge";
+  } else if (distanciaCat1 < distanciaCat2) {
+    return "cat1";
+  } else {
+    return "cat2";
+  }
 }
 
 // Desafio 8
