f7e31a15f9a688289e0297442031596e0af75ef6
camila-mp <ca.malvp@gmail.com>
2021-05-02 14:59:48 -0300

    tentativa de corrigir desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,7 +2,6 @@
 function techList(arrTech, nome1) {
   if (arrTech.length === 0) {
     return 'Vazio!'}
-
   let arrTechSorted = arrTech.sort();
   let arrObTech = []; 
   for (let index = 0; index < arrTechSorted.length; index += 1) { 
@@ -14,7 +13,6 @@ function techList(arrTech, nome1) {
   }
   return arrObTech;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 // Desafio 11
 function generatePhoneNumber(numeros) {
   let doisPrimeirosNum = '(' + `${numeros[0]}` + `${numeros[1] }`+ ')' + ' ';
@@ -27,7 +25,7 @@ function generatePhoneNumber(numeros) {
   let numTelefone = doisPrimeirosNum + noveNum.slice(0, 5) + '-' + noveNum.slice(5, 9);
   return numTelefone
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
