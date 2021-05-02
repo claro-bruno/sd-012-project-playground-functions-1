309d52bc7a01307b936a045aac2414d0f384468f
camila-mp <ca.malvp@gmail.com>
2021-05-02 16:38:07 -0300

    resolve desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,29 +15,33 @@ function techList(arrTech, nome1) {
 }
 // Desafio 11
 function generatePhoneNumber(numeros) {
-  
-  for (let index = 0; index < numeros.length; index += 1){
-    if (numeros[index] < 0 || numeros[index] > 9 ){
-      return "não é possível gerar um número de telefone com esses valores"
+  if ( numeros.length !== 11){
+    return "Array com tamanho incorreto."
+  }else{
+    for (let index = 0; index < numeros.length; index += 1){
+      if (numeros[index] < 0 || numeros[index] > 9 ){
+        return "não é possível gerar um número de telefone com esses valores"
+      }
     }
+    for (let index = 0; index < numeros.length; index += 1){
+      let contador = 0;
+      for (let index2 = 1; index2 <numeros.length; index2 += 1) {
+        if (numeros[index] === numeros[index2]){
+          contador += 1;
+          if (contador >= 3) {
+            return "não é possível gerar um número de telefone com esses valores" };
+        }
+      }
+    }
+    let doisPrimeirosNum = '(' + `${numeros[0]}` + `${numeros[1] }`+ ')' + ' ';
+    let noveNum = '';
+    for(let index = 2; index < numeros.length; index += 1){
+      noveNum += `${numeros[index]}`;
+    }
+    let numTelefone = doisPrimeirosNum + noveNum.slice(0, 5) + '-' + noveNum.slice(5, 9);
+    return numTelefone;
   }
-  
-  for (let index = 0; index < numeros.length; index += 1){
-    if ()
-  }
-  
-  
-  let doisPrimeirosNum = '(' + `${numeros[0]}` + `${numeros[1] }`+ ')' + ' ';
-  let noveNum = '';
-
-  for(let index = 2; index < numeros.length; index += 1){
-    noveNum += `${numeros[index]}`;
-  }
-  
-  let numTelefone = doisPrimeirosNum + noveNum.slice(0, 5) + '-' + noveNum.slice(5, 9);
-  return numTelefone
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
