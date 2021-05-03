e0b614a9a5beda3f9036969b915d511c5c004051
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-02 21:34:50 -0300

    exerc7 test

--- src/challenges.js
+++ src/challenges.js
@@ -70,13 +70,13 @@ for (let index = 0; index < array.length; index += 1){
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-if (cat1 === cat2 && cat1 === mouse || cat1 === cat2 ){
+if ((cat1 - cat2) === 0 && mouse ===0 || (cat1 - mouse) === (cat2 - mouse)){
   return ('os gatos trombam e o rato foge');
 }
-else if (cat1 < cat2 && cat1 >= mouse){
+else if ((cat1 - mouse) < (cat2 - mouse)){
   return ('cat1');
 }
-else if (cat1 > cat2 && cat2 >= mouse){
+else if ((cat2 - mouse) < (cat1 - mouse)){
   return ('cat2');
 }
 }
