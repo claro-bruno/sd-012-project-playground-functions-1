185ef7e7f6fb7babaa84fa33fe182c45e9f9a7b5
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-03 12:31:03 -0300

    resolucao do desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -60,8 +60,16 @@ function highestCount(arrayNum) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(cat1 > cat2){
+     return 'cat2';
+  }
+  if(cat2 > cat1){
+    return 'cat1';
+  }
+  if(cat1 === cat2){
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
