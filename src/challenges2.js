71a0bd4bc6a310d71b81446caba1abbf7c0fca3b
Eurides <euridesalves@yahoo.com.br>
2021-05-03 05:11:14 -0300

    corrigido desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,40 +5,38 @@ function techList() {
 
 // Desafio 11
 function generatePhoneNumber(numero) {
-  let numerotel = '(';
-  if (numero.length != 11) {
-    return 'Array com tamanho incorreto.';
-  } else {
-     let matrizAux = numero.sort();
-     let repetiu = 0;
-     for (posicao = 0; posicao < matrizAux.length; posicao += 1) {
-       for (let index = 0; index < numero.length; index += 1) {
-         if (numero[index] < 0 || numero[index] > 9 ) {
-           return 'não é possível gerar um número de telefone com esses valores';
-         } else
-            if (matrizAux[posicao] === numero[index]) {
-               repetiu += 1
-            }
-           if (repetiu >= 3 ) {
-              return 'não é possível gerar um número de telefone com esses valores';
-           }
+  function generatePhoneNumber(numero) {
+    if (numero.length != 11) {
+      return 'Array com tamanho incorreto.';
+    }
+    let numerotel = '(';
+    let caracter = '';
+    let repetiu = 0;
+    for (posicao = 0; posicao < numero.length; posicao += 1) {
+      if (numero[posicao] < 0 || numero[posicao] > 9 ) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      if (posicao === 2) {
+        numerotel += ')';
+      } 
+      if (posicao === 7) {
+        numerotel += '-';
+      }
+      numerotel += numero[posicao];
+      repetiu = 0;
+      for (let index = 0; index < numero.length; index += 1) {
+        if (numero[posicao] === numero[index]) {
+          repetiu += 1
+        }
+        if (repetiu >= 3 ) {
+          return 'não é possível gerar um número de telefone com esses valores';
         }
       }
-      for (let index = 0; index < numero.length; index += 1) {
-        if (index = 2) {
-          numerotel += ')';
-        } 
-        numerotel += numero[index];
-        if (index = 7) {
-          numerotel += '-';
-        } 
-        numerotel += numero[index];
-      }
     }
-  return numerotel;
-}
-
-// Desafio 12
+    return numerotel;
+  }
+  
+  // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let resposta = false;
   if ( (linea > (lineB + lineC)) && (lineA > Math.abs(lineB - lineC)) ) {
