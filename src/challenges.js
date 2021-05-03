b7066932d345ba2957b6dd3358918e7f6fb461da
cristianocsl <cslcristiano@gmail.com>
2021-05-03 12:06:14 -0300

    desafio 9 encode e decode

--- src/challenges.js
+++ src/challenges.js
@@ -100,20 +100,22 @@ function encode(string1) {
 }
 function decode(string2) {
   let string = string2.split('');
+  //let string = parseInt(string3);
   for (let index = 0; index < string.length; index += 1) {
-    if (string[index] == 1) {
+    if (string[index] === '1') {
       string[index] = 'a';
-    } else if (string[index] == 2) {
+    } else if (string[index] === '2') {
       string[index] = 'e';
-    } else if (string[index] == 3) {
+    } else if (string[index] === '3') {
       string[index] = 'i';
-    } else if (string[index] == 4) {
+    } else if (string[index] === '4') {
       string[index] = 'o';
-    } else if (string[index] == 5) {
+    } else if (string[index] === '5') {
       string[index] = 'u';
     }
   }
-  return string.join('');
+  let newString = string.join('');
+  return newString;
 }
 module.exports = {
   calcArea,
