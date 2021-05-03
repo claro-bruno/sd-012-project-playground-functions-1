0ba0cad454403946059f4320055ccb70bf0ac237
Eurides <euridesalves@yahoo.com.br>
2021-05-03 13:31:18 -0300

    corrigido exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,36 +5,35 @@ function techList() {
 
 // Desafio 11
 function generatePhoneNumber(numero) {
-  function generatePhoneNumber(numero) {
-    if (numero.length != 11) {
-      return 'Array com tamanho incorreto.';
+  if (numero.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let numerotel = '(';
+  let caracter = '';
+  let repetiu = 0;
+  for (posicao = 0; posicao < numero.length; posicao += 1) {
+    if (numero[posicao] < 0 || numero[posicao] > 9 ) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
-    let numerotel = '(';
-    let caracter = '';
-    let repetiu = 0;
-    for (posicao = 0; posicao < numero.length; posicao += 1) {
-      if (numero[posicao] < 0 || numero[posicao] > 9 ) {
+    if (posicao === 2) {
+      numerotel += ') ';
+    } 
+    if (posicao === 7) {
+      numerotel += '-';
+    }
+    numerotel += numero[posicao];
+    repetiu = 0;
+    for (let index = 0; index < numero.length; index += 1) {
+      if (numero[posicao] === numero[index]) {
+        repetiu += 1
+      }
+      if (repetiu >= 3 ) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
-      if (posicao === 2) {
-        numerotel += ') ';
-      } 
-      if (posicao === 7) {
-        numerotel += '-';
-      }
-      numerotel += numero[posicao];
-      repetiu = 0;
-      for (let index = 0; index < numero.length; index += 1) {
-        if (numero[posicao] === numero[index]) {
-          repetiu += 1
-        }
-        if (repetiu >= 3 ) {
-          return 'não é possível gerar um número de telefone com esses valores';
-        }
-      }
     }
-    return numerotel;
   }
+  return numerotel;
+}
   
   // Desafio 12
   function triangleCheck(lineA, lineB, lineC) {
