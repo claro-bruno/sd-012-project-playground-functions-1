0e0fee58064ef2540f38cda3956764b57e1d2a56
cristianocsl <cslcristiano@gmail.com>
2021-05-03 15:47:22 -0300

    tentativa desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,12 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(param1, param2) {
+  let param1 = ['react', 'jest', 'html'];
+  let param1obj = {};
+  for (let index = 0; index < param1.length; index += 1) {
+    param1.tech = param1[index];
+    param1obj.push(param1.tech);
+  }
+  console.log(param1obj)
 }
 
 // Desafio 11
