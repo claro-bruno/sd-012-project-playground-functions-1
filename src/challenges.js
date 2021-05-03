9694beccfbd76c0119cb11385c593a87a5f0c49e
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-03 14:37:01 -0300

    resolucao do desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -103,11 +103,47 @@ function fizzBuzz(arrayNum) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  string = string.split('');
+  for(let aux = 0; aux < string.length; aux ++){
+    if(string[aux] === 'a'){
+      string[aux] = '1';
+    }
+    if(string[aux] === 'e'){
+      string[aux] = '2';
+    }
+    if(string[aux] === 'i'){
+      string[aux] = '3';
+    }
+    if(string[aux] === 'o'){
+      string[aux] = '4';
+    }
+    if(string[aux] === 'u'){
+      string[aux] = '5';
+    }
+  }
+  return string.join('');
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  string = string.split('');
+  for(let aux = 0; aux < string.length; aux ++){
+    if(string[aux] === '1'){
+      string[aux] = 'a';
+    }
+    if(string[aux] === '2'){
+      string[aux] = 'e';
+    }
+    if(string[aux] === '3'){
+      string[aux] = 'i';
+    }
+    if(string[aux] === '4'){
+      string[aux] = 'o';
+    }
+    if(string[aux] === '5'){
+      string[aux] = 'u';
+    }
+  }
+  return string.join('');
 }
 
 module.exports = {
