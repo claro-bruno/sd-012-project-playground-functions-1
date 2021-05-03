fcaf2caea792dcdfb19696fd424df7652830e52e
cristianocsl <cslcristiano@gmail.com>
2021-05-03 18:41:55 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,7 +5,7 @@ function techList(arrTech, name) {
   if (arrTech.length === 0) {// se a lista estiver vazia, o tamanho é 0.
     return 'Vazio!';
   } else {// cado arrTech não seja vazio, execute:
-    for ( let index = 0; index < arrOrden.length; index += 1) {
+    for ( let index = 0; index < arrOrden.length; index += 1) {// 'for' para percorrer cada elemento da array.
       let obj_index = {
         tech: arrOrden[index],
         name: name,
@@ -15,7 +15,7 @@ function techList(arrTech, name) {
     return techObject;
   }
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'Cristiano'));
+// console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'],'Cristiano'));
 
 // Desafio 11
 function generatePhoneNumber() {
