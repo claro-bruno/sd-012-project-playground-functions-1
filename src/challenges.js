5cb6d61f6e4de7109efa8c4775d35f26ebb96b07
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-03 02:37:19 -0300

    7-Funcao caça ao rato

--- src/challenges.js
+++ src/challenges.js
@@ -5,27 +5,27 @@ function compareTrue(valor1, valor2) {
     } else {
       return false
     };
-};
+}
 
 // Desafio 2
 function calcArea(base, height) {
     return (base * height) / 2;
-};
+}
 
 // Desafio 3
 function splitSentence(frase) {
-    return frase.splint(' ');  
-};
+    return frase.split(' ');  
+}
 
 // Desafio 4
 function concatName(nome) { 
     return `${nome[nome.length - 1]}, ${nome[0]}`;
-};
+}
 
 // Desafio 5
 function footballPoints(win, ties) {
     return (win * 3) + (ties);
-}; 
+} 
 
 // Desafio 6
 function highestCount(numbers) {
@@ -43,11 +43,20 @@ function highestCount(numbers) {
       }
     }
     return repeticao;
-};
+}
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(cat1, cat2, mouse) {
+  cat1 = Math.abs(cat1 - mouse);
+  cat2 = Math.abs(cat2 - mouse);
+
+  if (cat1 < cat2) {
+    return 'cat1';
+  } else if (cat2 < cat1) {
+    return 'cat2';
+  } else {
+    return 'Os gatos trombam e o rato foge.';
+  };
 }
 
 // Desafio 8
