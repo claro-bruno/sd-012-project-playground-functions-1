9f762189ead5e1062e2eb1875a5a06dd16831079
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-05-03 10:41:42 -0400

    refatora desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -51,9 +51,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanciaCat1 = mouse - cat1;
   let distanciaCat2 = mouse - cat2;
   if (distanciaCat1 > 0 && distanciaCat2 > 0) {
-    if (distanciaCat2 === 2 && distanciaCat1 === 3) {
+    if (distanciaCat2 > 0 && distanciaCat2 < distanciaCat1) {
       return 'cat2';
-    } else if (distanciaCat1 === 6 && distanciaCat2 === 12) {
+    } else if (distanciaCat1 > 0 && distanciaCat1 < distanciaCat2) {
       return 'cat1';
     } else {
       return 'os gatos trombam e o rato foge';
