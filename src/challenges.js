068062f93e8214813af9d5ea0d96c487cab46613
Lucas Basques <lukasbasques@gmail.com>
2021-05-02 22:20:15 -0300

    Refatora desafio 1, organizaçao do codigo.

--- src/challenges.js
+++ src/challenges.js
@@ -1,45 +1,34 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  let result1;
-  if (value1 == true && value2 == true){
-    
-    return result1 = true;
+  if (value1 === true && value2 === true) {
+    return true;
   }
-  else {
-    return result1 = false;
-
-  }
+  return false;
 }
-
 
 // Desafio 2
 function calcArea(base, height) {
-  let result2 = (base * height) /2;
+  let result2 = (base * height) / 2;
   return result2;
-  
 }
-
 
 // Desafio 3
 function splitSentence(sentence) {
-  let result3 = sentence.split (" ");
+  let result3 = sentence.split(' ');
   return result3;
 }
-
 
 // Desafio 4
 function concatName(nameList) {
   let firstName = nameList[0];
-  let lastName = nameList [(nameList.length) -1];
-  let result4 = [lastName, firstName]; 
+  let lastName = nameList[(nameList.length) - 1];
+  let result4 = [lastName, firstName];
   return result4;
-
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-
-  let result5 = (wins *3) + (ties);
+  let result5 = (wins * 3) + (ties);
   return result5;
 }
 
@@ -49,82 +38,64 @@ function highestCount(numberList) {
   let timesNumber = 0;
 
   for (let index = 0; index < numberList.length; index += 1) {
-
-    if (highestNumber < numberList[index]){
+    if (highestNumber < numberList[index]) {
       highestNumber = numberList[index];
-    };
-  };
+    }
+  }
   for (let index = 0; index < numberList.length; index += 1) {
-
-    if (highestNumber === numberList[index]){
-      timesNumber = timesNumber + 1;
-    };
-  };
+    if (highestNumber === numberList[index]) {
+      timesNumber += 1;
+    }
+  }
   return timesNumber;
-};
+}
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1;
   let distanceCat2;
- 
-  if (mouse - cat1 >= 0){
+  if (mouse - cat1 >= 0) {
     distanceCat1 = mouse - cat1;
-  };
-
-  if(mouse - cat1 < 0){
-    distanceCat1 = (mouse - cat1) * -1;   
-  };
-
-  if (mouse - cat2 >= 0){
-    distanceCat2  = mouse - cat2;
-  };
-  if(mouse - cat2 < 0){
-    distanceCat2 = (mouse - cat2) * -1;   
-  };
-
-  if(distanceCat1 < distanceCat2){
-  return 'cat1';
-
   }
-
-  if (distanceCat1 > distanceCat2){
+  if (mouse - cat1 < 0) {
+    distanceCat1 = (mouse - cat1) * -1;
+  }
+  if (mouse - cat2 >= 0) {
+    distanceCat2 = mouse - cat2;
+  }
+  if (mouse - cat2 < 0) {
+    distanceCat2 = (mouse - cat2) * -1;
+  }
+  if (distanceCat1 < distanceCat2) {
+    return 'cat1';
+  }
+  if (distanceCat1 > distanceCat2) {
     return 'cat2';
-
   }
-
-  if (distanceCat1 === distanceCat2){
+  if (distanceCat1 === distanceCat2) {
     return 'os gatos trombam e o rato foge';
-    
-    }
-
+  }
 }
 
 // Desafio 8
 function fizzBuzz(numberArray) {
   let result8 = [];
   for (let index = 0; index < numberArray.length; index += 1) {
-    
     if (numberArray[index] % 3 === 0 && numberArray[index] % 5 === 0) {
       result8[index] = 'fizzBuzz';
-
-    } 
-    if (numberArray[index] % 5 === 0 && numberArray[index] % 3 != 0) {
-      result8[index] = 'buzz';
-
-    } 
-    if (numberArray[index] % 3 === 0 && numberArray[index] % 5 != 0) { 
-      result8[index] = 'fizz'; 
-
     }
-    if (numberArray[index] % 3 != 0 && numberArray[index] % 5 != 0) {
+    if (numberArray[index] % 5 === 0 && numberArray[index] % 3 !== 0) {
+      result8[index] = 'buzz';
+    }
+    if (numberArray[index] % 3 === 0 && numberArray[index] % 5 !== 0) {
+      result8[index] = 'fizz';
+    }
+    if (numberArray[index] % 3 !== 0 && numberArray[index] % 5 !== 0) {
       result8[index] = 'bug!';
-
     }
   }
-  return result8
+  return result8;
 }
-
 
 // Desafio 9
 function encode(letterNumber) {
@@ -146,9 +117,7 @@ function decode(numberLetter) {
   numberLetter = numberLetter.replace(/3/g, 'i');
   numberLetter = numberLetter.replace(/4/g, 'o');
   numberLetter = numberLetter.replace(/5/g, 'u');
-
   return numberLetter;
-
 }
 
 module.exports = {
