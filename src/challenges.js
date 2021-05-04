6072996dcc49704c297f88fbef3bf71f158cab27
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-04 18:41:58 -0300

    Requisito 4 - analisado

--- src/challenges.js
+++ src/challenges.js
@@ -8,24 +8,24 @@
   //return area;
  //}
  //Desafio 3
-  function splitSentence(newfrase) {
-  let array = [];
-  let text = " ";
-    for ( let index=0; index< newfrase.length; index +=1); {
-   if (newfrase[index] == text);
-      array.push(text + newfrase[index];)
-    } 
-   return array;
-    }
+  //function splitSentence(newfrase) {
+ // let array = [];
+  //let text = " ";
+    //for ( let index=0; index< newfrase.length; index +=1); {
+   //if (newfrase[index] == text);
+  //    array.push(text + newfrase[index];)
+  // } 
+  // return array;
+  //  }
 
  //Desafio 4
-  //function concatName(array) {
-  //let primeiroNome = array[array.length -1];         
-  //let ultimoNome =  array[0];
-  //let nome = primeiroNome + ', ' + ultimoNome;
-//
-  //return nome; 
-//}
+  function concatName(array) {
+  let primeiroNome = array[array.length -1];         
+  let ultimoNome =  array[0];
+  let nome = primeiroNome + ', ' + ultimoNome;
+
+  return nome; 
+  }
 
 //function footballPoints(wins,ties) {
   //  let match = wins *3 + ties *1;
