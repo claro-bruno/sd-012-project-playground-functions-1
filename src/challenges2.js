7ac27bc12ae56447bbb3c9ed9ed0c409cffa493b
Martin <escorpmartin97@gmail.com>
2021-05-04 18:42:12 -0300

    corrige exercise 11 e add 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,7 +17,7 @@ nomeTech.sort();
 // Desafio 11
 function generatePhoneNumber(numero) {
   let repetidos = 0;
-  let boolean = false
+  let valRepetidos = 0;
 
   for (let i = 0; i < numero.length; i += 1) {
     for (let j = 0; j < numero.length; j += 1){
@@ -25,15 +25,16 @@ function generatePhoneNumber(numero) {
         repetidos += 1;
       }
     }
-        if (repetidos >= 19) {
-         boolean = true;
-      }
+        if (repetidos > valRepetidos) {
+         valRepetidos = repetidos;
+         console.log(valRepetidos)
+      } repetidos = 0;
   }
   if (numero.length < 11 || numero.length > 11){
     return "Array com tamanho incorreto.";
   }
   for (let index = 0; index < numero.length; index += 1){
-    if (numero[index] < 0 || numero[index] > 9  || boolean === true) {
+    if (numero[index] < 0 || numero[index] > 9  || valRepetidos > 2) {
       return "não é possível gerar um número de telefone com esses valores";
     }
   }
@@ -44,6 +45,7 @@ function generatePhoneNumber(numero) {
     verifica = ( '(' + pC[0] + pC[1] + ')' + ' ' + pC[2] + pC[3] + pC[4] + pC[5] + pC[6] + '-' + pC[7] + pC[8] + pC[9] + pC[10]);
     return verifica;
  }
+ console.log(generatePhoneNumber([2, 6, 6, 5, 9, 9, 7, 7, 7, 1, 3]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -60,8 +62,20 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate(bebida) {
- // seu código aqui
-}
+  let cont = /\d+/g;
+  let contador = 0;
+  let agua = bebida.match(cont);
+ 
+   for (let index = 0; index < agua.length; index += 1){
+   contador += parseInt(agua[index]);
+   } if(contador > 2) {
+     return (contador + " copos de agua");
+     }
+      else if (contador === 1){
+       return (contador + " copo de agua");
+      }
+ }
+ console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"))
 
 module.exports = {
   generatePhoneNumber,
