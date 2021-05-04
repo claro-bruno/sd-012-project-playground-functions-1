f6bc94f4031c4e2de1af0a514c7aed29120ba1a5
Neto Silva <netosilv44445@gmail.com>
2021-05-04 18:49:54 -0300

    eai

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,14 +11,28 @@ function techList(array, name) {
   return 'Vazio!';
 }
 
-function verificaNumeros(array) {
-  if (array.length !== 11) { return 'Array com tamanho incorreto.'; }
+function repeticao(array) {
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] > 9 || array[i] < 0) {
-      return 'não é possível gerar um número de telefone com esses valores';
+    let vezes = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      if (array[i] === array[j]) {
+        vezes += 1;
+      }
+      if (vezes >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
     }
   }
   return true;
+}
+
+function verificaNumeros(array) {
+  const erro = 'não é possível gerar um número de telefone com esses valores';
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > 9) { return erro; }
+    if (array[i] < 0) { return erro; }
+  }
+  return repeticao(array);
 }
 
 function changePosition(arr, from, to) {
@@ -29,12 +43,13 @@ function changePosition(arr, from, to) {
 // Desafio 11
 
 function generatePhoneNumber(array) {
+  if (array.length !== 11) { return 'Array com tamanho incorreto.'; }
   let resposta = verificaNumeros(array);
   if (resposta === true) {
     array.unshift('(', ')', '-', ' ');
     array = changePosition(array, 1, 3);
     array = changePosition(array, 4, 1);
-    array = changePosition(array, 2, 9);
+    array = changePosition(array, 2, 10);
     array = changePosition(array, 4, 2);
     array = changePosition(array, 3, 4);
     array = array.join('');
@@ -43,7 +58,7 @@ function generatePhoneNumber(array) {
   return resposta;
 }
 
-console.log(generatePhoneNumber([4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 4]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
@@ -53,7 +68,6 @@ function triangleCheck() {
 function hydrate(string) {
   let dale = 0;
   let numeros = string.split('');
-  console.log(numeros);
   numeros = numeros.filter((valor) => {
     if (Number(valor)) {
       dale += Number(valor);
