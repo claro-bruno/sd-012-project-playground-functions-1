f9a4ea23d8c0a18b061c4409ddc690a4dfa616a0
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-04 19:02:41 -0300

    Corrigido espaçamento apos () no retorno da funçao do desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -51,7 +51,7 @@ function generatePhoneNumber(number) {
         
       } else {
         phone.push(number[index]);
-        phonenumber = `(${phone[0]}${phone[1]})${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
+        phonenumber = `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
       }
     }
   }
