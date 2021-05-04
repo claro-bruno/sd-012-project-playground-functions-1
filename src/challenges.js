a00211d3da790457a276de1a4450f4587d4c73ec
Gutemberg <gttj2010@hotmail.com>
2021-05-04 18:34:23 -0300

    Requisito 12: Refatoraçao 6 da funçao triangleCheck.

--- src/challenges.js
+++ src/challenges.js
@@ -134,41 +134,6 @@ function generatePhoneNumber() {
   } */
 }
 
-// Desafio 12 - Parte 1
-function isTriangulo1(lineA, lineB, lineC) {
-  return lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB;
-}
-
-// Desafio 12 - Parte 2
-function isTriangulo2(lineA1, lineB1, lineC1) {
-  // seu código aqui
-  let  vv = lineA1 > Math.abs(lineB1 - lineC1) && lineB1 > Math.abs(lineA1 - lineC1);
-  return vv && lineC1 > Math.abs(lineA1 - lineB1);
-}
-
-// Desafio 12 - Parte 3
-function triangleCheck(lineA, lineB, lineC) {
-  // seu código aqui
-  let lineA1 = lineA;
-  let lineB1 = lineB;
-  let lineC1 = lineC;
-  let resultado = (isTriangulo1(lineA, lineB, lineC) && isTriangulo2(lineA1, lineB1, lineC1));
-  return resultado;
-}
-
-// Desafio 13
-function hydrate(string) {
-  // seu código aqui
-  let con = 0;
-  let numeros = string.match(/\d+/g).map(Number);
-  for (let indice = 0; indice < numeros.length; indice += 1) {
-    con += numeros[indice];
-  } if (con === 1) {
-    return ('1 copo de água');
-  }
-  return (`${con} copos de água`);
-}
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -179,9 +144,5 @@ module.exports = {
   fizzBuzz,
   footballPoints,
   highestCount,
-  splitSentence,
-  arrayFunction,
-  generatePhoneNumber,
-  triangleCheck,
-  hydrate,
+  splitSentence, 
 };
