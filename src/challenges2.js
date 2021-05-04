e792c0cb314ea09838df2eb7260b45dde3c9dc65
Bruno <brunocostah@hotmail.com>
2021-05-04 13:02:20 -0300

    Requisito 10 completo segunda parte

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,34 @@
 // Desafio 10
+
 function techList(array, name) {
-  let object = {};
-}
+  
+  let objectArray=[];
+
+  if (array.length < 0) {
+    return 'Vazio!';
+  }
+  else {
+    for (let index = 0; index <array.length; index +=1){
+      let object = {};
+      object.tech = array[index];
+      object.name = name;
+      objectArray.push(object);
+    }
+  }
+  //Credito:https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  objectArray.sort(function (a, b) {
+    if (a.tech > b.tech) {
+      return 1;
+    }
+    if (a.tech < b.tech) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+  return objectArray;
+};
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
