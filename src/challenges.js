90d08b58217388596f7ce48844ac99a25142b2ee
Bruno <brunocostah@hotmail.com>
2021-05-04 00:57:54 -0300

    Requisito 9 completo

--- src/challenges.js
+++ src/challenges.js
@@ -96,15 +96,31 @@ function fizzBuzz(array) {
   };
   return arrayFizzBuzz;
 };
-console.log(fizzBuzz([9,25]))
+console.log(fizzBuzz([9,25]));
+
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+
+//creditos: Gisele Costa - Turma 11  eu pesquisei sobre replace porem ainda nao estava entendo como montar bem a sintaxe.
+function encode(string) {
+  string = string.replace(/a/g, '1');
+  string = string.replace(/e/g, '2');
+  string = string.replace(/i/g, '3');
+  string = string.replace(/o/g, '4');
+  string = string.replace(/u/g, '5');
+  return string;
 }
 
+function decode(string) {
+  string = string.replace(/1/g, 'a');
+  string = string.replace(/2/g, 'e');
+  string = string.replace(/3/g, 'i');
+  string = string.replace(/4/g, 'o');
+  string = string.replace(/5/g, 'u');
+  return string;
+}
+
+
+ 
 module.exports = {
   calcArea,
   catAndMouse,
@@ -117,3 +133,6 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+
+
