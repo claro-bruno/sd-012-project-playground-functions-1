292d5b522683547a6fe70bb6675fee7e7a52618c
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-05-04 14:35:20 -0300

    adiciona alteraçao desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -40,10 +40,10 @@ function catAndMouse(mouse, cat1, cat2) {
   let distancia1 = cat1 - mouse;
   let distancia2 = cat2 - mouse;
   let resultado = ' ';
-  if (distancia1 < distancia2) {
-    resultado = 'cat1';
-  } else if (distancia2 > distancia1) {
+  if (distancia2 < distancia1) {
     resultado = 'cat2';
+  } else if (distancia1 < distancia2) {
+    resultado = 'cat1';
   } else if (distancia1 === distancia2) {
     resultado = 'os gatos trombam e o rato foge';
   }
