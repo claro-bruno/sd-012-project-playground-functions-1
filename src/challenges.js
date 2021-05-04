8161575a7fbfcc50ec1be9c87f7b59ce670fe732
brennerr <marciobrennerbusiness@gmail.com>
2021-05-04 09:43:56 -0400

    C1 - Questões 1 a 4

--- src/challenges.js
+++ src/challenges.js
@@ -1,23 +1,49 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(number1,number2) {
   // seu código aqui
-  sdfg
-}
+ let number1 = 5;
+ let number2 = 3;
+ let isTrue = (number1 >= 3 && number2 >= 3  );
+//  let isFalse = (number1 <= 2 && number2 <= 2 );
+
+ if (number1 >= 3 && number2 >= 3){
+   return isTrue;
+ } else {
+   return isTrue = False;
+ }
+};
+// console.log(compareTrue(2,2));
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, altura) {
   // seu código aqui
-}
+  let area = 0;
+  if(base > 0 && altura > 0) {
+    area = (base * altura) /2;
+  }
+  return area;
+};
+// console.log(calcArea(2,4))
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(" ") {
   // seu código aqui
-}
+  let divisao = string.split(" ");
+  return divisao;
+};
+// console.log(splitSentence(" "));
 
 // Desafio 4
-function concatName() {
+function concatName(string) {
   // seu código aqui
-}
+  let concatenacaoDaString = [];
+
+  for (let index = 0; index < string.length; index++) {
+    concatenacaoDaString = string[index] + "," +
+  };
+  return concatenacaoDaString;
+
+};
 
 // Desafio 5
 function footballPoints() {
