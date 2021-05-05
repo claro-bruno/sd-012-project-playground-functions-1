9a023da777cf6a9eb38b64ef9ad2427d8fc93f1d
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-04 23:42:29 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,22 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbersArray) {
+  let menorZero = 0;
+  let maiorNove = 0;
+  let somaNoves = 0;
+  for (let index = 0; index < numbersArray.length; index += 1) {
+    if (numbersArray[index] < 0) menorZero += 1;
+    else if (numbersArray[index] > 9) maiorNove += 1;
+    else if (numbersArray[index] == 9) somaNoves += 1;
+  }
+  if (numbersArray.length > 11) {
+    return "Array com tamanho incorreto.";
+  } else if (menorZero > 0 || maiorNove > 0 || somaNoves > 3) {
+    return "não é possível gerar um número de telefone com esses valores";
+  } else {
+    return `(${numbersArray[0]}${numbersArray[1]})${numbersArray[2]}${numbersArray[3]}${numbersArray[4]}${numbersArray[5]}${numbersArray[6]}-${numbersArray[7]}${numbersArray[8]}${numbersArray[9]}${numbersArray[10]}`
+  }
 }
 
 // Desafio 12
