dd03ce6c77015f8c7839717a3907e48c4fd76a6c
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-04 21:58:37 -0300

    concluindo o projeto playground-functions Desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -38,16 +38,33 @@ function generatePhoneNumber(lista) {
   return `(${lista[0]}${lista[1]}) ${lista[2]}${lista[3]}${lista[4]}${lista[5]}${lista[6]}-${lista[7]}${lista[8]}${lista[9]}${lista[10]}`;  
 }
 
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
-
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+
+  let triangiloABC = (lineA < lineB + lineC) && (lineA > Math.abs(lineB - lineC));
+  let triangiloBAC = (lineB < lineA + lineC) && (lineB > Math.abs(lineA - lineC));
+  let triangiloCBA = (lineC < lineB + lineA) && (lineC > Math.abs(lineB - lineA));
+
+  if(triangiloABC && triangiloBAC && triangiloCBA){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let numerosEmString = frase.replace(/([^\d])+/gim, '');
+  let numerosSomados = 0;
+
+  for(let key in numerosEmString){
+    numerosSomados += parseInt(numerosEmString[key])
+  }
+  if(numerosSomados > 1){
+    return `${numerosSomados} copos de água`;
+  } else {
+    return `${numerosSomados} copo de água`;
+  }
 }
 
 module.exports = {
