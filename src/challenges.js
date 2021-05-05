329737d73515d5501f9a151fe8b328696283ab19
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-05 11:39:32 -0300

    Completa funcoes do desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -78,12 +78,56 @@ function fizzBuzz(numbersArray) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+function encode(string) {
+  let stringArray = string.split('');
+  for (let index = 0; index < stringArray.length; index += 1) {
+      if (index === stringArray.indexOf('a')) {
+        stringArray[stringArray.indexOf('a')] = 1;
+      };
+      if (index === stringArray.indexOf('e')) {
+        stringArray[stringArray.indexOf('e')] = 2;
+      };
+      if (index === stringArray.indexOf('i')) {
+        stringArray[stringArray.indexOf('i')] = 3;
+      };
+      if (index === stringArray.indexOf('o')) {
+        stringArray[stringArray.indexOf('o')] = 4;
+      };
+      if (index === stringArray.indexOf('u')) {
+        stringArray[stringArray.indexOf('u')] = 5;
+      };
+    };
+
+  let codedString = stringArray.join('');
+  return codedString;
+};
+console.log(encode("hi there!"));
+
+function decode(string) {
+  let stringArray = string.split('');
+  for (let index = 0; index < stringArray.length; index += 1) {
+      if (index === stringArray.indexOf('1')) {
+        stringArray[stringArray.indexOf('1')] = 'a';
+      };
+      if (index === stringArray.indexOf('2')) {
+        stringArray[stringArray.indexOf('2')] = 'e';
+      };
+      if (index === stringArray.indexOf('3')) {
+        stringArray[stringArray.indexOf('3')] = 'i';
+      };
+      if (index === stringArray.indexOf('4')) {
+        stringArray[stringArray.indexOf('4')] = 'o';
+      };
+      if (index === stringArray.indexOf('5')) {
+        stringArray[stringArray.indexOf('5')] = 'u';
+      };
+    };
+
+  let codedString = stringArray.join('');
+  return codedString;
+};
+console.log(decode("h3 th2r2!"));
+
 
 module.exports = {
   calcArea,
