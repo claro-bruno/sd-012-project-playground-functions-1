1db93164b164fe74298244598e7f3b1559fb7852
Gutemberg <gttj2010@hotmail.com>
2021-05-05 00:59:36 -0300

    Requisito 10: Refatoraçao 4 da funçao techList e a mençao, ou os devidos creditos, ao colega Caio Takeshi.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,7 @@
 // Desafio 10
 function techList(listaTec, nome) {
   // seu código aqui
+  // Algoritmo de Caio Takeshi - colega de turma
   let objeto = [];
   let lista = {};
   
@@ -15,10 +16,11 @@ function techList(listaTec, nome) {
       objeto.push(lista);
     }
   }
-  // https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/#:~:text=Como%20ordenar%20uma%20array%20de%20objetos%20com%20Javascript%20sort(),-Javascript&text=Caso%20voc%C3%AA%20tenha%20um%20array,usar%20o%20m%C3%A9todo%20sort().
-  return objeto.sort(function(a, b) {return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0);});
+// https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/#:~:text=Como%20ordenar%20uma%20array%20de%20objetos%20com%20Javascript%20sort(),-Javascript&text=Caso%20voc%C3%AA%20tenha%20um%20array,usar%20o%20m%C3%A9todo%20sort().
+  return objeto.sort(function(a, b) { 
+         return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0);
+  });
 }
-//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "guto"));
 
 // Desafio 11
 function generatePhoneNumber() {
