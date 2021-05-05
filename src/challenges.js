1b3479fad5bc2850d502bd0e759c8fb3fe845aab
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-05 12:08:43 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -3,15 +3,18 @@
 //  return bool1 && bool2;
 //} 
 // // Desafio 2
-function calcArea() {
-  let area = (base * height) / 2;
-  return area;
-}
+//function calcArea() {//let area = (base * height) / 2;//return area;
+//}
 // // Desafio 3
-// function splitSentence() {
-//   // seu código aqui
-// }
-
+ function splitSentence() {
+  let array = [];
+     let text = " ";
+       for ( let index=0; index< newfrase.length; index +=1); {
+      if (newfrase[index] == text);
+        array.push(text + newfrase[index];)
+     } 
+     return array;
+    }
 // // Desafio 4
 // function concatName() {
 //   // seu código aqui
