c30dac21713b46ff4386fe550d683c90fb3ec9f0
Victor Veloso <victorveloso13@gmail.com>
2021-05-05 02:16:14 -0300

    Faz requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,9 +9,18 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let lados = lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB;
+  
+  let medidas = lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineA - lineB);
+  
+  return lados && medidas;
+};
+console.log(triangleCheck(10, 14, 8));
+console.log(triangleCheck(5, 5, 20));
+
+
+
 
 // Desafio 13
 function hydrate() {
