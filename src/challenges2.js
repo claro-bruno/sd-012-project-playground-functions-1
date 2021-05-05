4ce3a6dd1a636d43906954f6e76455335836c975
Gutemberg <gttj2010@hotmail.com>
2021-05-05 01:05:29 -0300

    Requisito 10: Refatoraçao 5 da funçao techList.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,10 +1,8 @@
 // Desafio 10
-function techList(listaTec, nome) {
-  // seu código aqui
+function techList(listaTec, nome) {  
   // Algoritmo de Caio Takeshi - colega de turma
   let objeto = [];
   let lista = {};
-  
   if (listaTec.length === 0) {
     return 'Vazio!';
   } else {
@@ -16,9 +14,9 @@ function techList(listaTec, nome) {
       objeto.push(lista);
     }
   }
-// https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/#:~:text=Como%20ordenar%20uma%20array%20de%20objetos%20com%20Javascript%20sort(),-Javascript&text=Caso%20voc%C3%AA%20tenha%20um%20array,usar%20o%20m%C3%A9todo%20sort().
-  return objeto.sort(function(a, b) { 
-         return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0);
+  // https://www.edsonemiliano.com.br/blog/como-ordenar-uma-array-de-objetos-com-javascript-sort/#:~:text=Como%20ordenar%20uma%20array%20de%20objetos%20com%20Javascript%20sort(),-Javascript&text=Caso%20voc%C3%AA%20tenha%20um%20array,usar%20o%20m%C3%A9todo%20sort().
+  return objeto.sort(function(a, b) {
+    return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0);
   });
 }
 
