ce51ad5c023d071f711c95b08892dd029dab88a4
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-05 11:26:05 -0300

    Desafio One

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,26 +1,26 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
-module.exports = {
-  generatePhoneNumber,
-  techList,
-  hydrate,
-  triangleCheck,
-};
+// module.exports = {
+//   generatePhoneNumber,
+//   techList,
+//   hydrate,
+//   triangleCheck,
+// };
