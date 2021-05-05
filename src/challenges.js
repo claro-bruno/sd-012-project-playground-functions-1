a94e60838a0d310b918fa162e024f7c959210577
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-05 11:34:51 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,12 @@
 // // Desafio 1
-function compareTrue(bool1,bool2) {
-    return bool1 && bool2;
-  } 
-
-
+//function compareTrue(bool1,bool2) {
+//  return bool1 && bool2;
+//} 
 // // Desafio 2
-// function calcArea() {
-//   // seu código aqui
-// }
-
+function calcArea(base,height) {
+  let area = (base * height) / 2;
+  return area;
+}
 // // Desafio 3
 // function splitSentence() {
 //   // seu código aqui
