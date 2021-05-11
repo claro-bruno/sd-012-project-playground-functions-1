ead647277b2ecdead07faeae2af0050b80ef9870
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-11 18:59:08 -0300

    Revisao Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,65 +1,60 @@
-//Desafio 1
-   function compareTrue(bool1,bool2) {
-   return bool1 && bool2 ;
-   } 
- //Desafio 2
-  function calcArea(base,height) {
-  let area = (base * height) / 2;
-  return area;
- }
+// //Desafio 1
+//    function compareTrue(bool1,bool2) {
+//    return bool1 && bool2 ;
+//    } 
+//  //Desafio 2
+//   function calcArea(base,height) {
+//   let area = (base * height) / 2;
+//   return area;
+//  }
 // Desafio 3
   function splitSentence(newfrase) {
- let array = [];
-  let text = " ";
-    for ( let index=0; index< newfrase.length; index +=1); {
-   if (newfrase[index] == text);
-     array.push(text + newfrase[index]);
-  } 
-  return array;
-   }
+     return newfrase.split(' ');
+    }
+     return newfrase.split= ' ';
+ 
+//  //Desafio 4
+//  function concatName(array) {
+//  let primeiroNome = array[array.length -1];         
+//  let ultimoNome =  array[0];
+// let nome = primeiroNome + ', ' + ultimoNome;
 
- //Desafio 4
- function concatName(array) {
- let primeiroNome = array[array.length -1];         
- let ultimoNome =  array[0];
-let nome = primeiroNome + ', ' + ultimoNome;
+//  return nome; 
+// }
+// //Desafio 5
+// function footballPoints(wins,ties) {
+// let match = wins *3 + ties *1;
+//  return match;
+// }
+//  //Desafio 6
+//  function highestCount(array) {
+//    let highestNumber = 0;
+//    let counter = 0;
+//    for (let index = 0; index < array.length; index += 1) {
+//      if (array[index] >= highestNumber) {
+//        highestNumber = array[index];
+//        counter += 1;
+//      }
+//    }
+//    return counter;
+//   }
+// //Desafio 7
+// function catAndMouse() {
+  
+// }
 
- return nome; 
-}
-//Desafio 5
-function footballPoints(wins,ties) {
-let match = wins *3 + ties *1;
- return match;
-}
- //Desafio 6
- function highestCount(array) {
-   let highestNumber = 0;
-   let counter = 0;
-   for (let index = 0; index < array.length; index += 1) {
-     if (array[index] >= highestNumber) {
-       highestNumber = array[index];
-       counter += 1;
-     }
-   }
-   return counter;
-  }
-//Desafio 7
-function catAndMouse() {
+// //Desafio 8
+// function fizzBuzz() {
   
-}
+// }
 
-//Desafio 8
-function fizzBuzz() {
+// //Desafio 9
+// function encode() {
   
-}
-
-//Desafio 9
-function encode() {
+// }
+// function decode() {
   
-}
-function decode() {
-  
-}
+// }
  module.exports = {
    calcArea,
    catAndMouse,
