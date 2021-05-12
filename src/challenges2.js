d6403f0783bb548a36408bb618fb121abc6f6f41
Aldria Melo <aldriamelo.academico@gmail.com>
2021-05-12 14:09:46 -0300

    adiciona 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,9 +18,29 @@ function techList(tecnologias, name) {
 // console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Marcela"));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(telefone) {
+let numeros = telefone
+
+if (numeros.length < 11 || numeros.length > 11){
+  return "Array com tamanho incorreto."
 }
+
+for(let index = 0; index < numeros.length; index +=1){
+  if(numeros[index] < 0 || numeros[index] > 9){
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+  for(let index2 = inicio+1; index2 < numeros.length; index2 +=1){
+  // if (numeros[index] == index2[index2]){
+  //   cont +=1;
+  }
+}
+}
+if (cont > 3){
+  return "não é possível gerar um número de telefone com esses valores"
+}
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -41,7 +61,6 @@ function triangleCheck(lineA, lineB, lineC) {
 } else { return true}
 }
 
-// console.log(triangleCheck(10,14,8))
 
 // Desafio 13
 function hydrate() {
