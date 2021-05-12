d1c1903e37514564af99cf0f3cd3d68e1c7b2460
Aldria Melo <aldriamelo.academico@gmail.com>
2021-05-12 14:23:59 -0300

    altera 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -40,7 +40,7 @@ for(let index = 0; index < numeros.length; index +=1){
   return "não é possível gerar um número de telefone com esses valores"
     }
   }
-  return "("+numeros[0]+numeros[1]+")"+""+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+"-"+numeros[7]+numeros[8]+numeros[9]+numeros[10]
+  return "("+numeros[0]+numeros[1]+")"+" "+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+"-"+numeros[7]+numeros[8]+numeros[9]+numeros[10]
 }
 
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,]))
