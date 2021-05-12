94b38ab47e2b61c834afbc7d3b481b7b831ae26c
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-11 21:49:27 -0300

    Revisa changes

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,12 @@
 // //Desafio 1
-//    function compareTrue(bool1,bool2) {
-//    return bool1 && bool2 ;
-//    } 
+   function compareTrue(bool1,bool2) {
+   return bool1 && bool2 ;
+   } 
 //  //Desafio 2
-//   function calcArea(base,height) {
-//   let area = (base * height) / 2;
-//   return area;
-//  }
+  function calcArea(base,height) {
+  let area = (base * height) / 2;
+  return area;
+ }
 // Desafio 3
   function splitSentence(newfrase) {
      return newfrase.split(' ');
@@ -14,47 +14,47 @@
      return newfrase.split= ' ';
  
 //  //Desafio 4
-//  function concatName(array) {
-//  let primeiroNome = array[array.length -1];         
-//  let ultimoNome =  array[0];
-// let nome = primeiroNome + ', ' + ultimoNome;
+ function concatName(array) {
+ let primeiroNome = array[array.length -1];         
+ let ultimoNome =  array[0];
+let nome = primeiroNome + ', ' + ultimoNome;
 
-//  return nome; 
-// }
+ return nome; 
+}
 // //Desafio 5
-// function footballPoints(wins,ties) {
-// let match = wins *3 + ties *1;
-//  return match;
-// }
-//  //Desafio 6
-//  function highestCount(array) {
-//    let highestNumber = 0;
-//    let counter = 0;
-//    for (let index = 0; index < array.length; index += 1) {
-//      if (array[index] >= highestNumber) {
-//        highestNumber = array[index];
-//        counter += 1;
-//      }
-//    }
-//    return counter;
-//   }
+function footballPoints(wins,ties) {
+let match = wins *3 + ties *1;
+ return match;
+}
+ //Desafio 6
+ function highestCount(array) {
+   let highestNumber = 0;
+   let counter = 0;
+   for (let index = 0; index < array.length; index += 1) {
+     if (array[index] >= highestNumber) {
+       highestNumber = array[index];
+       counter += 1;
+     }
+   }
+   return counter;
+  }
 // //Desafio 7
-// function catAndMouse() {
+  function catAndMouse() {
   
-// }
+ }
 
 // //Desafio 8
-// function fizzBuzz() {
+  function fizzBuzz() {
   
-// }
+ }
 
 // //Desafio 9
-// function encode() {
+ function encode() {
   
-// }
-// function decode() {
+ }
+  function decode() {
   
-// }
+  }
  module.exports = {
    calcArea,
    catAndMouse,
