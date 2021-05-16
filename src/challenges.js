33a3c7dc84212d7dcb64a83be92cd8df86bc3283
Cassio Mascarenhas <csmlvx@gmail.com>
2021-05-16 07:28:47 -0300

    calcArea Function

--- src/challenges.js
+++ src/challenges.js
@@ -11,9 +11,12 @@ function compareTrue(a, b) {
 compareTrue();
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height) / 2;
 }
+
+calcArea();
 
 // Desafio 3
 function splitSentence() {
