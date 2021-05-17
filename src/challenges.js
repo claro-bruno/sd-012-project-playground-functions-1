f63d114ff66db462553ee82cd574962a72908df6
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-17 19:38:29 -0300

    novos desafios acrescentados.

--- src/challenges.js
+++ src/challenges.js
@@ -49,12 +49,26 @@ let match = wins *3 + ties *1;
  }
 
 // //Desafio 9
- function encode() {
-  
- }
-  function decode() {
-  
+   function encode(frase) {
+    let novaFrase = frase
+      .replace(/a/g, '1')
+      .replace(/e/g, '2')
+      .replace(/i/g, '3')
+      .replace(/o/g, '4')
+      .replace(/u/g, '5');
+    return novaFrase;
   }
+ 
+     function decode(codigo) {
+      let novoCodigo = codigo
+        .replace(/1/g, 'a')
+        .replace(/2/g, 'e')
+        .replace(/3/g, 'i')
+        .replace(/4/g, 'o')
+        .replace(/5/g, 'u');
+      return novoCodigo;
+   }
+  
  module.exports = {
    calcArea,
    catAndMouse,
