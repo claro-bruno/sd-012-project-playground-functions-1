c8b1928eac258f61c9ed1f2edfec405fcef67cd8
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-18 11:42:16 -0300

    ajustes finais

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,9 +21,17 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+    if (lineA > (lineB + lineC) || lineB > (lineA + lineC) || lineC > (lineB + lineA)) {
+      return false;
+    }
+    if (lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineB - lineA)) {
+      return false;
+    }
+    if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) || lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC) || lineC < (lineB + lineA) && lineC > Math.abs(lineB - lineA)) {
+      return true;
+    }
+  }
 
 // Desafio 13
 function hydrate() {
