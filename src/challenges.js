2b97292b2fefa70289b1acc893ee99f0c5c21ea4
Ramon Paschoal <rpaschoal90@gmail.com>
2021-06-06 21:47:14 -0300

    Requisito  2

--- src/challenges.js
+++ src/challenges.js
@@ -1,67 +1,66 @@
 // Desafio 1
 function compareTrue(num1, num2) {
   if (num1 === true && num2 === true) {
-    return true
+    return true;
   }
   return false;
 }
 
 // // Desafio 2
-// function calcArea(base, height) {
+function calcArea(base, height) {
+  return (base * height) * 2;
+}
 
-// }
+// Desafio 3
+function splitSentence(string) {
 
+}
 
-// // Desafio 3
-// function splitSentence(string) {
+// Desafio 4
+function concatName(array) {
 
-// }
+}
 
-// // Desafio 4
-// function concatName(array) {
+// Desafio 5
+function footballPoints(wins, ties) {
 
-// }
+}
 
-// // Desafio 5
-// function footballPoints(wins, ties) {
+// Desafio 6
+function highestCount(array) {
 
-// }
+}
 
-// // Desafio 6
-// function highestCount(array) {
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
 
-// }
+}
 
-// // Desafio 7
-// function catAndMouse(mouse, cat1, cat2) {
+// Desafio 8
+function fizzBuzz(array) {
 
-// }
+}
 
-// // Desafio 8
-// function fizzBuzz(array) {
+// Desafio 9
+function encode(string) {
 
-// }
+}
 
-// // Desafio 9
-// function encode(string) {
-
-// }
-
-// function decode() {
+function decode() {
   
-// }
+}
 
-// console.log(encode('hi there'));
+console.log(encode('hi there'));
 
-// module.exports = {
-//   calcArea,
-//   catAndMouse,
-  compareTrue
-//   concatName,
-//   decode,
-//   encode,
-//   fizzBuzz,
-//   footballPoints,
-//   highestCount,
-//   splitSentence,
-// };
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  highestCount,
+  splitSentence,
+};
